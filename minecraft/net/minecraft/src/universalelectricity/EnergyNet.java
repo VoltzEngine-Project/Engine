17a15921f3fc11f53354420649b4021018cd9e72
AJ Fleming <atrain99@mchsi.com>
2012-06-25 11:57:52 -0500

    More Work

---
+++ minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
@@ -0,0 +1,12 @@
+package net.minecraft.src.universalelectricity;
+
+import java.util.*;
+import net.minecraft.src.*;
+import net.minecraft.src.forge.*;
+
+class EnergyNet{
+  ArrayList<UEBlockCondutor> conductors;
+  public EnergyNet(UEBlockConductor initial){
+    
+  }
+}
