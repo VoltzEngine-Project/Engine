2bf5e5346571269c19dbbb1d32806d6413558379
AJ Fleming <atrain99@mchsi.com>
2012-06-25 11:59:36 -0500

    Update master

--- minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
+++ minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
@@ -6,6 +6,8 @@ import net.minecraft.src.forge.*;
 
 class EnergyNet{
   ArrayList<UEBlockCondutor> conductors;
+  ArrayList<EnergyNetTarget> targets;
+  ArrayList<UEIProducer> inputs;
   public EnergyNet(UEBlockConductor initial){
     
   }
