b65dcde72c7703c01e378a78b351b0a6fbc0d3cd
calclavia <henry@calclavia.com>
2012-06-26 11:46:18 -0700

    Code clean up and formatting changes.

--- minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
+++ minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
@@ -4,30 +4,45 @@ import java.util.*;
 import net.minecraft.src.*;
 import net.minecraft.src.forge.*;
 
-class EnergyNet{
-  ArrayList<UETileEntityConductor> conductors;
-  ArrayList<UEIConsumer> targets = new ArrayList<UEIConsumer>();
-  ArrayList<UEIProducer> inputs = new ArrayList<UEIProducer>();
-  ArrayList<UEIConsumer> runningTargets = new ArrayList<UEIConsumer>();
+public class EnergyNet
+{
+  private List<UETileEntityConductor> conductors = new ArrayList<UETileEntityConductor>();
+  private List<UEIConsumer> targets = new ArrayList<UEIConsumer>();
+  private List<UEIProducer> inputs = new ArrayList<UEIProducer>();
+  private List<UEIConsumer> runningTargets = new ArrayList<UEIConsumer>();
   public boolean canEnergyFlow = false;
-  public EnergyNet(UETileEntityConductor initial){
-      conductors = new ArrayList<UETileEntityConductor>();
+  
+  public EnergyNet(UETileEntityConductor initial)
+  {
+    conductors = new ArrayList<UETileEntityConductor>();
   	conductors.add(initial);
   }
-  public void addConsumer(UEIConsumer consumer){
+  
+  public void addConsumer(UEIConsumer consumer)
+  {
     targets.add(consumer);
   }
-  public void addProducer(UEIProducer producer){
+  
+  public void addProducer(UEIProducer producer)
+  {
     inputs.add(producer);
   }
-  public void updateEnergyNet(){
-    for(UEIConsumer machine : targets){
-        if(machine.needsPower){
+  
+  public void updateEnergyNet()
+  {
+    for(UEIConsumer machine : targets)
+    {
+        if(machine.needsPower)
+        {
             runningTargets.add(machine);
-        }else{
-            try{
+        }
+        else
+        {
+            try
+            {
                 runningTargets.remove(machine);
             }catch(Exception e){}
+            
             if(runningTargets.length == 0) this.canEnergyFlow == false;
         }
   }         
