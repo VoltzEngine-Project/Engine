53f1b218fa547aa5f9e5f1acfa26743344ce7d11
atrain99 <atrain99@mchsi.com>
2012-06-26 09:18:50 -0500

    Major Update

--- minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
+++ minecraft/net/minecraft/src/universalelectricity/EnergyNet.java
@@ -5,10 +5,31 @@ import net.minecraft.src.*;
 import net.minecraft.src.forge.*;
 
 class EnergyNet{
-  ArrayList<UEBlockCondutor> conductors;
-  ArrayList<EnergyNetTarget> targets;
-  ArrayList<UEIProducer> inputs;
-  public EnergyNet(UEBlockConductor initial){
-    
+  ArrayList<UETileEntityConductor> conductors;
+  ArrayList<UEIConsumer> targets = new ArrayList<UEIConsumer>();
+  ArrayList<UEIProducer> inputs = new ArrayList<UEIProducer>();
+  ArrayList<UEIConsumer> runningTargets = new ArrayList<UEIConsumer>();
+  public boolean canEnergyFlow = false;
+  public EnergyNet(UETileEntityConductor initial){
+      conductors = new ArrayList<UETileEntityConductor>();
+  	conductors.add(initial);
   }
+  public void addConsumer(UEIConsumer consumer){
+    targets.add(consumer);
+  }
+  public void addProducer(UEIProducer producer){
+    inputs.add(producer);
+  }
+  public void updateEnergyNet(){
+    for(UEIConsumer machine : targets){
+        if(machine.needsPower){
+            this.canEnergyFlow == true;
+            runningTargets.add(machine);
+        }else if(!machine.needsPower){
+            try{
+                runningTargets.remove(machine);
+            }catch(Exception e){}
+            if(runningTargets.length == 0) this.canEnergyFlow == false;
+        }
+  }         
 }
