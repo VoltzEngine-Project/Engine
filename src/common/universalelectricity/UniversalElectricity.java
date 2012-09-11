3616395222a2adc41b5c6c8117d8045da980059c
Calclavia <henry@calclavia.com>
2012-09-11 20:17:00 +0800

    UE 0.8.0 Release!

--- src/common/universalelectricity/UniversalElectricity.java
+++ src/common/universalelectricity/UniversalElectricity.java
@@ -38,7 +38,7 @@ public class UniversalElectricity implements IConnectionHandler
 {
 	public static final Configuration CONFIGURATION = new Configuration(new File(Loader.instance().getConfigDir(), "config/UniversalElectricity/UniversalElectricity.cfg"));
 	public static final List<Object> MODS = new ArrayList<Object>();
-    public static final String VERSION = "0.7.0";
+    public static final String VERSION = "0.8.0";
         
     public static UniversalElectricity instance;
     
