5c86effea3ab3d4f2bc13e2f91f168cb5e888b45
Calclavia <henry@calclavia.com>
2012-09-11 20:20:40 +0800

    Config Derp

--- src/common/universalelectricity/UniversalElectricity.java
+++ src/common/universalelectricity/UniversalElectricity.java
@@ -36,7 +36,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class UniversalElectricity implements IConnectionHandler
 {
-	public static final Configuration CONFIGURATION = new Configuration(new File(Loader.instance().getConfigDir(), "config/UniversalElectricity/UniversalElectricity.cfg"));
+	public static final Configuration CONFIGURATION = new Configuration(new File(Loader.instance().getConfigDir(), "UniversalElectricity/UniversalElectricity.cfg"));
 	public static final List<Object> MODS = new ArrayList<Object>();
     public static final String VERSION = "0.8.0";
         
