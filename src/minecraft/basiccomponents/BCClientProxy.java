e7170dba3f513d9c2f5436c41cb987de79640feb
Henry Mao <admin@calclavia.com>
2012-11-15 18:20:49 +0800

    Formatting

--- src/minecraft/basiccomponents/BCClientProxy.java
+++ src/minecraft/basiccomponents/BCClientProxy.java
@@ -18,32 +18,35 @@ public class BCClientProxy extends BCCommonProxy
 	@Override
 	public void preInit()
 	{
-		//Preload textures
+		// Preload textures
 		MinecraftForgeClient.preloadTexture(BasicComponents.BLOCK_TEXTURE_FILE);
 		MinecraftForgeClient.preloadTexture(BasicComponents.ITEM_TEXTURE_FILE);
 	}
-	
+
 	@Override
 	public void init()
 	{
 		ClientRegistry.registerTileEntity(TileEntityCopperWire.class, "TileEntityCopperWire", new RenderCopperWire());
 	}
-	
+
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
 	{
 		TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		
+
 		if (tileEntity != null)
-        {
-			switch(ID)
+		{
+			switch (ID)
 			{
-				case 0: return new GUIBatteryBox(player.inventory, ((TileEntityBatteryBox)tileEntity));
-				case 1: return new GuiCoalGenerator(player.inventory, ((TileEntityCoalGenerator)tileEntity));
-				case 2: return new GUIElectricFurnace(player.inventory, ((TileEntityElectricFurnace)tileEntity));
+				case 0:
+					return new GUIBatteryBox(player.inventory, ((TileEntityBatteryBox) tileEntity));
+				case 1:
+					return new GuiCoalGenerator(player.inventory, ((TileEntityCoalGenerator) tileEntity));
+				case 2:
+					return new GUIElectricFurnace(player.inventory, ((TileEntityElectricFurnace) tileEntity));
 			}
-        }
-		
+		}
+
 		return null;
 	}
 }
