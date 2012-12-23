6e6c8130f68f52fba617ea29fc688df823699cbf
Henry Mao <admin@calclavia.com>
2012-12-24 01:26:00 +0800

    Fixed wrenching issue

--- src/minecraft/basiccomponents/common/item/ItemWrench.java
+++ src/minecraft/basiccomponents/common/item/ItemWrench.java
@@ -1,9 +1,13 @@
 package basiccomponents.common.item;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import railcraft.common.api.core.items.ICrowbar;
 import universalelectricity.prefab.UETab;
+import universalelectricity.prefab.implement.ISneakUseWrench;
 import universalelectricity.prefab.implement.IToolConfigurator;
 import basiccomponents.common.BasicComponents;
 
@@ -18,6 +22,31 @@ public class ItemWrench extends Item implements ICrowbar, IToolConfigurator
 		this.setItemName("wrench");
 		this.setIconIndex(texture);
 		this.setTextureFile(BasicComponents.ITEM_TEXTURE_FILE);
+	}
+
+	@Override
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
+	{
+		if (player.isSneaking())
+		{
+			int blockID = world.getBlockId(x, y, z);
+
+			if (blockID > 0 && blockID < Block.blocksList.length)
+			{
+				Block block = Block.blocksList[blockID];
+
+				if (block != null)
+				{
+					if (block instanceof ISneakUseWrench)
+					{
+						((ISneakUseWrench) block).onSneakUseWrench(world, x, y, z, player, side, hitX, hitY, hitZ);
+						return true;
+					}
+				}
+			}
+		}
+		
+		return false;
 	}
 
 	@Override
