6dc64a532ca662f46efb964edc2bb6d80797e6b0
Henry Mao <admin@calclavia.com>
2013-04-25 18:01:44 +0800

    Temporarily gave up on yaw rotation

--- calclavia/lib/CalculationHelper.java
+++ calclavia/lib/CalculationHelper.java
@@ -10,14 +10,48 @@ public class CalculationHelper
 	public static void rotateByAngle(Vector3 vector, double yaw, double pitch)
 	{
 		double yawRadians = Math.toRadians(yaw);
-		double pitchRadians = Math.toRadians(yaw);
+		double pitchRadians = Math.toRadians(pitch);
 
-		double originalX = vector.x;
-		double originalY = vector.y;
-		double originalZ = vector.z;
+		double x = vector.x;
+		double y = vector.y;
+		double z = vector.z;
 
-		vector.x = originalX * Math.cos(yawRadians) - originalZ * Math.sin(yawRadians);
-		vector.z = originalX * Math.sin(yawRadians) + originalZ * Math.cos(yawRadians);
-		vector.y = originalX * Math.cos(pitchRadians) - originalY * Math.sin(pitchRadians);
+		if (yaw != 0)
+		{
+			vector.x = x * Math.cos(yawRadians) - z * Math.sin(yawRadians);
+			vector.z = x * Math.sin(yawRadians) + z * Math.cos(yawRadians);
+		}
+
+		if (pitch != 0)
+		{
+			vector.y = x * Math.cos(pitchRadians) - y * Math.sin(pitchRadians);
+		}
+	}
+
+	public static void rotateByAngle(Vector3 vector, double yaw, double pitch, double roll)
+	{
+		double yawRadians = Math.toRadians(yaw);
+		double pitchRadians = Math.toRadians(pitch);
+		double rollRadians = Math.toRadians(roll);
+
+		double x = vector.x;
+		double y = vector.y;
+		double z = vector.z;
+
+		// Rotate around the x axis
+		double xy = Math.cos(pitchRadians) * y - Math.sin(pitchRadians) * z;
+		double xz = Math.sin(pitchRadians) * y + Math.cos(pitchRadians) * z;
+
+		// Rotation around the Y axis
+		double yz = Math.cos(yawRadians) * xz - Math.sin(yawRadians) * x;
+		double yx = Math.sin(yawRadians) * xz + Math.cos(yawRadians) * x;
+
+		// Rotation around the Z axis
+		double zx = Math.cos(rollRadians) * yx - Math.sin(rollRadians) * xy;
+		double zy = Math.sin(rollRadians) * yx + Math.cos(rollRadians) * xy;
+
+		vector.x = zx;
+		vector.y = zy;
+		vector.z = yz;
 	}
 }
