f94ea1df9eea72a92c7cda08f57f527664472d25
Henry Mao <admin@calclavia.com>
2013-04-25 17:18:16 +0800

    Improved rotation code

--- calclavia/lib/CalculationHelper.java
+++ calclavia/lib/CalculationHelper.java
@@ -5,24 +5,19 @@ import universalelectricity.core.vector.Vector3;
 public class CalculationHelper
 {
 	/**
-	 * Rotates a point horizontally around the anchor 0,0 by a specific angle.
+	 * Rotates a point by a yaw and pitch around the anchor 0,0 by a specific angle.
 	 */
-	public static void rotateXZByAngle(Vector3 vector, double angle)
+	public static void rotateByAngle(Vector3 vector, double yaw, double pitch)
 	{
-		double angleRadians = Math.toRadians(angle);
+		double yawRadians = Math.toRadians(yaw);
+		double pitchRadians = Math.toRadians(yaw);
 
 		double originalX = vector.x;
+		double originalY = vector.y;
 		double originalZ = vector.z;
-		vector.x = originalX * Math.cos(angleRadians) - originalZ * Math.sin(angleRadians);
-		vector.z = originalX * Math.sin(angleRadians) + originalZ * Math.cos(angleRadians);
-	}
 
-	/**
-	 * Rotates a point vertically around the anchor 0,0 by a specific angle.
-	 */
-	public static void rotateYByAngle(Vector3 vector, int angle)
-	{
-		double angleRadians = Math.toRadians(angle);
-		vector.y = Math.cos(angleRadians) * vector.y;
+		vector.x = originalX * Math.cos(yawRadians) - originalZ * Math.sin(yawRadians);
+		vector.z = originalX * Math.sin(yawRadians) + originalZ * Math.cos(yawRadians);
+		vector.y = originalX * Math.cos(pitchRadians) - originalY * Math.sin(pitchRadians);
 	}
 }
