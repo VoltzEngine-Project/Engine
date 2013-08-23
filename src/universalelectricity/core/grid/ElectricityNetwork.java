58e29857abf16d5851e8f1b5fbe9c6ceb380a518
Calclavia <admin@calclavia.com>
2013-08-23 20:20:16 +0800

    Updated modding library

--- src/universalelectricity/core/grid/ElectricityNetwork.java
+++ src/universalelectricity/core/grid/ElectricityNetwork.java
@@ -41,6 +41,8 @@ public class ElectricityNetwork implements IElectricityNetwork
 	public Map<TileEntity, ArrayList<ForgeDirection>> electricalTiles = new HashMap<TileEntity, ArrayList<ForgeDirection>>();
 
 	private final Set<IConductor> conductors = new HashSet<IConductor>();
+	
+	public float acceptorResistance = 500;
 
 	@Override
 	public float produce(ElectricityPack electricity, TileEntity... ignoreTiles)
@@ -48,8 +50,12 @@ public class ElectricityNetwork implements IElectricityNetwork
 		ElectricityProductionEvent evt = new ElectricityProductionEvent(electricity, ignoreTiles);
 		MinecraftForge.EVENT_BUS.post(evt);
 
-		float energy = electricity.getWatts();
-		float totalEnergy = energy;
+		float totalEnergy = electricity.getWatts();
+		float networkResistance = getTotalResistance();
+		float proportionWasted = getTotalResistance() / (getTotalResistance() + acceptorResistance);
+		float energyWasted = totalEnergy * proportionWasted;
+		float totalUsableEnergy = totalEnergy - energyWasted;
+		float remainingUsableEnergy = totalUsableEnergy;
 		float voltage = electricity.voltage;
 
 		if (!evt.isCanceled())
@@ -72,19 +78,13 @@ public class ElectricityNetwork implements IElectricityNetwork
 							{
 								if (electricalTile.canConnect(direction) && this.getConductors().contains(VectorHelper.getConnectorFromSide(tileEntity.worldObj, new Vector3(tileEntity), direction)))
 								{
-									float energyToSend = totalEnergy * (electricalTile.getRequest(direction) / totalEnergyRequest);
+									float energyToSend = totalUsableEnergy * (electricalTile.getRequest(direction) / totalEnergyRequest);
 
 									if (energyToSend > 0)
 									{
 										ElectricityPack electricityToSend = ElectricityPack.getFromWatts(energyToSend, voltage);
 
-										// Calculate energy loss caused by resistance.
-										float ampsReceived = electricityToSend.amperes - (electricityToSend.amperes * electricityToSend.amperes * this.getTotalResistance()) / electricityToSend.voltage;
-										float voltsReceived = electricityToSend.voltage - (electricityToSend.amperes * this.getTotalResistance());
-
-										electricityToSend = new ElectricityPack(ampsReceived, voltsReceived);
-
-										energy -= ((IElectrical) tileEntity).receiveElectricity(direction, electricityToSend, true);
+										remainingUsableEnergy -= ((IElectrical) tileEntity).receiveElectricity(direction, electricityToSend, true);
 									}
 								}
 							}
@@ -94,7 +94,7 @@ public class ElectricityNetwork implements IElectricityNetwork
 			}
 		}
 
-		return energy;
+		return remainingUsableEnergy;
 	}
 
 	/**
