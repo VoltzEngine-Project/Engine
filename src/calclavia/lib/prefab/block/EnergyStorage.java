015fabb450c50680f81bd25011799b78ab838605
Calclavia <admin@calclavia.com>
2013-12-20 23:29:25 +0800

    Added EnergyStorage

---
+++ src/calclavia/lib/prefab/block/EnergyStorage.java
@@ -0,0 +1,186 @@
+package calclavia.lib.prefab.block;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * Used internally for IEnergyInterface tiles.
+ * 
+ * @author Calclavia, King Lemming
+ * 
+ */
+public class EnergyStorage
+{
+	protected int energy;
+	protected int capacity;
+	protected int maxReceive;
+	protected int maxExtract;
+
+	public EnergyStorage(int capacity)
+	{
+
+		this(capacity, capacity, capacity);
+	}
+
+	public EnergyStorage(int capacity, int maxTransfer)
+	{
+
+		this(capacity, maxTransfer, maxTransfer);
+	}
+
+	public EnergyStorage(int capacity, int maxReceive, int maxExtract)
+	{
+
+		this.capacity = capacity;
+		this.maxReceive = maxReceive;
+		this.maxExtract = maxExtract;
+	}
+
+	public EnergyStorage readFromNBT(NBTTagCompound nbt)
+	{
+
+		this.energy = nbt.getInteger("Energy");
+		return this;
+	}
+
+	public NBTTagCompound writeToNBT(NBTTagCompound nbt)
+	{
+
+		nbt.setInteger("Energy", energy);
+		return nbt;
+	}
+
+	public void setCapacity(int capacity)
+	{
+
+		this.capacity = capacity;
+
+		if (energy > capacity)
+		{
+			energy = capacity;
+		}
+	}
+
+	public void setMaxTransfer(int maxTransfer)
+	{
+
+		setMaxReceive(maxTransfer);
+		setMaxExtract(maxTransfer);
+	}
+
+	public void setMaxReceive(int maxReceive)
+	{
+
+		this.maxReceive = maxReceive;
+	}
+
+	public void setMaxExtract(int maxExtract)
+	{
+
+		this.maxExtract = maxExtract;
+	}
+
+	public int getMaxReceive()
+	{
+
+		return maxReceive;
+	}
+
+	public int getMaxExtract()
+	{
+
+		return maxExtract;
+	}
+
+	/**
+	 * This function is included to allow for server -> client sync. Do not call this externally to
+	 * the containing Tile Entity, as not all IEnergyHandlers are
+	 * guaranteed to have it.
+	 * 
+	 * @param energy
+	 */
+	public void setEnergyStored(int energy)
+	{
+
+		this.energy = energy;
+
+		if (this.energy > capacity)
+		{
+			this.energy = capacity;
+		}
+		else if (this.energy < 0)
+		{
+			this.energy = 0;
+		}
+	}
+
+	/**
+	 * This function is included to allow the containing tile to directly and efficiently modify the
+	 * energy contained in the EnergyStorage. Do not rely on this
+	 * externally, as not all IEnergyHandlers are guaranteed to have it.
+	 * 
+	 * @param energy
+	 */
+	public void modifyEnergyStored(int energy)
+	{
+
+		this.energy += energy;
+
+		if (this.energy > capacity)
+		{
+			this.energy = capacity;
+		}
+		else if (this.energy < 0)
+		{
+			this.energy = 0;
+		}
+	}
+
+	public int receiveEnergy(int maxReceive, boolean doAction)
+	{
+
+		int energyReceived = Math.min(capacity - energy, Math.min(this.maxReceive, maxReceive));
+
+		if (doAction)
+		{
+			energy += energyReceived;
+		}
+		return energyReceived;
+	}
+
+	public int extractEnergy(int maxExtract, boolean doAction)
+	{
+		int energyExtracted = Math.min(energy, Math.min(this.maxExtract, maxExtract));
+
+		if (doAction)
+		{
+			energy -= energyExtracted;
+		}
+		return energyExtracted;
+	}
+
+	public boolean checkReceive(int receive)
+	{
+		return this.receiveEnergy(receive, false) >= receive;
+	}
+
+	public boolean checkExtract(int extract)
+	{
+		return this.extractEnergy(extract, false) >= extract;
+	}
+
+	public boolean isFull()
+	{
+		return this.getEnergyStored() >= this.getMaxEnergyStored();
+	}
+
+	public int getEnergyStored()
+	{
+		return energy;
+	}
+
+	public int getMaxEnergyStored()
+	{
+		return capacity;
+	}
+
+}
