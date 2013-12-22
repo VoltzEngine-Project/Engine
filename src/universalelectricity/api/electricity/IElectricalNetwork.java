151cc22faeb5f0962047ed234c75e1437f21bcde
Robert <rseifert.phone@gmail.com>
2013-12-22 05:39:12 -0500

    Removed produce(Volt,amp) from IElectricalNetwork
    Going to use the EnergyNetwork version as i can check if production is
    an instanceof IVoltage and go from there

--- src/universalelectricity/api/electricity/IElectricalNetwork.java
+++ src/universalelectricity/api/electricity/IElectricalNetwork.java
@@ -12,12 +12,4 @@ public interface IElectricalNetwork extends IEnergyNetwork
 
     /** Gets the Amps the network is outputting */
     public long getAmperage();
-
-    /** Produces power to the energy network with no care if energy is actually fully used
-     * 
-     * @param source - machine that is producing the energy
-     * @param voltage - voltage the energy is emitted at
-     * @param amps - amps the energy is emitted at
-     * @return amount of energy that was accepted by the network */
-    public long produce(Object source, long voltage, long amps, boolean doProduce);
 }
