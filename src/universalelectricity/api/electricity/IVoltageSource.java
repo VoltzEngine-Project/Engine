7386f1461eca30ceeea17e5f5a0c1b510b867631
Robert <rseifert.phone@gmail.com>
2013-12-22 07:51:49 -0500

    Added IVoltageSource method

---
+++ src/universalelectricity/api/electricity/IVoltageSource.java
@@ -0,0 +1,16 @@
+package universalelectricity.api.electricity;
+
+import net.minecraftforge.common.ForgeDirection;
+
+/** Applied to electrical machines that are designed to act as sources of power in an electrical
+ * network. Mainly used to calculate the over all voltage of a network correctly.
+ * 
+ * @author DarkGuardsman */
+public interface IVoltageSource extends IVoltage
+{
+    /** Can this machine emit voltage on the given side. Should
+     * 
+     * @param side - side that the voltage will be emitted on
+     * @return true if at any given time this machine can output voltage on the side */
+    public boolean canEmitVoltage(ForgeDirection side);
+}
