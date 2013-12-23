1827e215a5c7f0d803e5deb467f2b902439c01ac
Calclavia <admin@calclavia.com>
2013-12-23 15:13:06 +0800

    Minor formatting change

--- src/universalelectricity/api/electricity/IElectricalNetwork.java
+++ src/universalelectricity/api/electricity/IElectricalNetwork.java
@@ -3,12 +3,14 @@ package universalelectricity.api.electricity;
 import universalelectricity.api.energy.IEnergyNetwork;
 
 /**
- * Extended version of the energy network that implements Voltage and Amperage to understand energy
+ * Extended version of the energy network that properly implements amperage and voltage.
  * 
  * @author DarkGuardsman
  */
 public interface IElectricalNetwork extends IEnergyNetwork
 {
-	/** Gets Voltage that the network is at */
+	/**
+	 * Gets the current voltage of the network at this point.
+	 */
 	public long getVoltage();
 }
