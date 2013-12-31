c26937ca337d8cf3b96f2b647be1a005c6c00d34
Calclavia <admin@calclavia.com>
2013-12-31 15:53:54 +0800

    Fixed position not being translated.

--- src/main/java/calclavia/lib/event/ChunkModifiedEvent.java
+++ src/main/java/calclavia/lib/event/ChunkModifiedEvent.java
@@ -20,12 +20,12 @@ public abstract class ChunkModifiedEvent extends ChunkEvent
 	{
 		public final int x, y, z, blockID, blockMetadata;
 
-		public ChunkSetBlockEvent(Chunk chunk, int x, int y, int z, int blockID, int blockMetadata)
+		public ChunkSetBlockEvent(Chunk chunk, int chunkX, int y, int chunkZ, int blockID, int blockMetadata)
 		{
 			super(chunk);
-			this.x = x;
+			this.x = (chunk.xPosition << 4) + chunkX;
 			this.y = y;
-			this.z = z;
+			this.z = (chunk.zPosition << 4) + chunkZ;
 			this.blockID = blockID;
 			this.blockMetadata = blockMetadata;
 		}
