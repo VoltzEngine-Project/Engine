c0d32e2507b3e5efc74dfede53e6c15743fa286e
DarkGuardsman <rseifert.phone@gmail.com>
2014-01-01 21:03:47 -0500

    Fixed a concurrent modification error in NetworkTickHandler

--- src/main/java/universalelectricity/core/net/NetworkTickHandler.java
+++ src/main/java/universalelectricity/core/net/NetworkTickHandler.java
@@ -8,68 +8,69 @@ import universalelectricity.api.net.INetwork;
 import cpw.mods.fml.common.ITickHandler;
 import cpw.mods.fml.common.TickType;
 
-/**
- * A ticker to update all networks. Register your custom network here to have it ticked by Universal
+/** A ticker to update all networks. Register your custom network here to have it ticked by Universal
  * Electricity.
  * 
- * @author Calclavia
- * 
- */
+ * @author Calclavia */
 public class NetworkTickHandler implements ITickHandler
 {
-	public static final NetworkTickHandler INSTANCE = new NetworkTickHandler();
+    public static final NetworkTickHandler INSTANCE = new NetworkTickHandler();
 
-	private final LinkedHashSet<INetwork> toAddNetworks = new LinkedHashSet<INetwork>();
-	private final LinkedHashSet<INetwork> networks = new LinkedHashSet<INetwork>();
+    private final LinkedHashSet<INetwork> toAddNetworks = new LinkedHashSet<INetwork>();
+    private final LinkedHashSet<INetwork> networks = new LinkedHashSet<INetwork>();
 
-	public static void addNetwork(INetwork network)
-	{
-		if (!INSTANCE.networks.contains(network))
-		{
-			INSTANCE.toAddNetworks.add(network);
-		}
-	}
+    public static void addNetwork(INetwork network)
+    {
+        synchronized (INSTANCE.toAddNetworks)
+        {
+            if (!INSTANCE.networks.contains(network))
+            {
+                INSTANCE.toAddNetworks.add(network);
+            }
+        }
+    }
 
-	@Override
-	public void tickStart(EnumSet<TickType> type, Object... tickData)
-	{
+    @Override
+    public void tickStart(EnumSet<TickType> type, Object... tickData)
+    {
 
-	}
+    }
 
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData)
-	{
-		this.networks.addAll(this.toAddNetworks);
-		this.toAddNetworks.clear();
+    @Override
+    public void tickEnd(EnumSet<TickType> type, Object... tickData)
+    {
 
-		Iterator<INetwork> it = this.networks.iterator();
+        this.networks.addAll(this.toAddNetworks);
+        this.toAddNetworks.clear();
 
-		while (it.hasNext())
-		{
-			INetwork network = it.next();
+        Iterator<INetwork> it = this.networks.iterator();
 
-			if (network.canUpdate())
-			{
-				network.update();
-			}
+        while (it.hasNext())
+        {
+            INetwork network = it.next();
 
-			if (!network.continueUpdate())
-			{
-				it.remove();
-			}
-		}
-	}
+            if (network.canUpdate())
+            {
+                network.update();
+            }
 
-	@Override
-	public EnumSet<TickType> ticks()
-	{
-		return EnumSet.of(TickType.SERVER);
-	}
+            if (!network.continueUpdate())
+            {
+                it.remove();
+            }
+        }
+    }
 
-	@Override
-	public String getLabel()
-	{
-		return "Universal Electricity Grid Ticker";
-	}
+    @Override
+    public EnumSet<TickType> ticks()
+    {
+        return EnumSet.of(TickType.SERVER);
+    }
+
+    @Override
+    public String getLabel()
+    {
+        return "Universal Electricity Grid Ticker";
+    }
 
 }
