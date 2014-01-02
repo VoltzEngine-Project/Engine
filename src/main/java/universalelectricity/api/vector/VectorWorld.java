6459f412a9075182bdbe52a2a5b0aa407a506787
Calclavia <admin@calclavia.com>
2014-01-02 20:03:09 +0800

    Added equals() to VectorWorld

--- src/main/java/universalelectricity/api/vector/VectorWorld.java
+++ src/main/java/universalelectricity/api/vector/VectorWorld.java
@@ -12,95 +12,107 @@ import net.minecraftforge.common.DimensionManager;
 
 public class VectorWorld extends Vector3
 {
-    public World world;
+	public World world;
 
-    public VectorWorld(World world, double x, double y, double z)
-    {
-        super(x, y, z);
-        this.world = world;
-    }
+	public VectorWorld(World world, double x, double y, double z)
+	{
+		super(x, y, z);
+		this.world = world;
+	}
 
-    public VectorWorld(NBTTagCompound nbt)
-    {
-        super(nbt);
-        this.world = DimensionManager.getWorld(nbt.getInteger("d"));
-    }
+	public VectorWorld(NBTTagCompound nbt)
+	{
+		super(nbt);
+		this.world = DimensionManager.getWorld(nbt.getInteger("d"));
+	}
 
-    public VectorWorld(Entity entity)
-    {
-        super(entity);
-        this.world = entity.worldObj;
-    }
+	public VectorWorld(Entity entity)
+	{
+		super(entity);
+		this.world = entity.worldObj;
+	}
 
-    public VectorWorld(TileEntity tile)
-    {
-        super(tile);
-        this.world = tile.worldObj;
-    }
+	public VectorWorld(TileEntity tile)
+	{
+		super(tile);
+		this.world = tile.worldObj;
+	}
 
-    public VectorWorld(World world, Vector3 v)
-    {
-        this.x = v.x;
-        this.y = v.y;
-        this.z = v.z;
-        this.world = world;
-    }
+	public VectorWorld(World world, Vector3 v)
+	{
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+		this.world = world;
+	}
 
-    @Override
-    public VectorWorld translate(double x, double y, double z)
-    {
-        this.x += x;
-        this.y += y;
-        this.z += z;
-        return this;
-    }
+	@Override
+	public VectorWorld translate(double x, double y, double z)
+	{
+		this.x += x;
+		this.y += y;
+		this.z += z;
+		return this;
+	}
 
-    @Override
-    public VectorWorld clone()
-    {
-        return new VectorWorld(world, x, y, z);
-    }
+	@Override
+	public VectorWorld clone()
+	{
+		return new VectorWorld(world, x, y, z);
+	}
 
-    @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound nbt)
-    {
-        super.writeToNBT(nbt);
-        nbt.setInteger("d", this.world.provider.dimensionId);
-        return nbt;
-    }
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o instanceof VectorWorld)
+		{
+			VectorWorld vector = (VectorWorld) o;
+			return this.world == vector.world && this.x == vector.x && this.y == vector.y && this.z == vector.z;
+		}
 
-    public int getBlockID()
-    {
-        return super.getBlockID(this.world);
-    }
+		return super.equals(o);
+	}
 
-    public int getBlockMetadata()
-    {
-        return super.getBlockMetadata(this.world);
-    }
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
+		nbt.setInteger("d", this.world.provider.dimensionId);
+		return nbt;
+	}
 
-    public TileEntity getTileEntity()
-    {
-        return super.getTileEntity(this.world);
-    }
+	public int getBlockID()
+	{
+		return super.getBlockID(this.world);
+	}
 
-    public boolean setBlock(int id, int metadata, int notify)
-    {
-        return super.setBlock(this.world, id, metadata, notify);
-    }
+	public int getBlockMetadata()
+	{
+		return super.getBlockMetadata(this.world);
+	}
 
-    public boolean setBlock(int id, int metadata)
-    {
-        return this.setBlock(id, metadata, 3);
-    }
+	public TileEntity getTileEntity()
+	{
+		return super.getTileEntity(this.world);
+	}
 
-    public boolean setBlock(int id)
-    {
-        return this.setBlock(id, 0);
-    }
-    
-    public List<Entity> getEntitiesWithin(Class<? extends Entity> par1Class)
-    {
-        return super.getEntitiesWithin(this.world, par1Class);
-    }
+	public boolean setBlock(int id, int metadata, int notify)
+	{
+		return super.setBlock(this.world, id, metadata, notify);
+	}
+
+	public boolean setBlock(int id, int metadata)
+	{
+		return this.setBlock(id, metadata, 3);
+	}
+
+	public boolean setBlock(int id)
+	{
+		return this.setBlock(id, 0);
+	}
+
+	public List<Entity> getEntitiesWithin(Class<? extends Entity> par1Class)
+	{
+		return super.getEntitiesWithin(this.world, par1Class);
+	}
 }
