038484302b0771ba4bf930f5b698d1a3cd35e85c
DarkGuardsman <rseifert.phone@gmail.com>
2014-01-08 18:55:42 -0500

    Minor addition

---
+++ src/main/java/calclavia/lib/prefab/item/ItemBlockHolder.java
@@ -0,0 +1,32 @@
+package calclavia.lib.prefab.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Simple itemBlock class for quick use with a block for controlling metadata
+ * 
+ * @author Darkguardsman
+ */
+public class ItemBlockHolder extends ItemBlock
+{
+	public ItemBlockHolder(int id)
+	{
+		super(id);
+		this.setMaxDamage(0);
+		this.setHasSubtypes(true);
+	}
+
+	@Override
+	public int getMetadata(int damage)
+	{
+		return damage;
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack itemStack)
+	{
+		return Block.blocksList[this.getBlockID()].getUnlocalizedName() + "." + itemStack.getItemDamage();
+	}
+}
