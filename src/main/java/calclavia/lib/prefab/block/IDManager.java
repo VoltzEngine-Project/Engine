f655b0230de384957f552a30c606e86c0b127708
Calclavia <admin@calclavia.com>
2014-01-10 20:18:04 +0800

    Created IDManager

---
+++ src/main/java/calclavia/lib/prefab/block/IDManager.java
@@ -0,0 +1,60 @@
+package calclavia.lib.prefab.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+/**
+ * Automatically gets the next avaliable ID for a mod.
+ * 
+ * @author Calclavia
+ * 
+ */
+public class IDManager
+{
+	/** Auto ID Management */
+	private final int blockIDBase;
+	private final int itemIDBase;
+
+	private int nextBlockID;
+	private int nextItemID;
+
+	public IDManager(int blockIDBase, int itemIDBase)
+	{
+		nextBlockID = this.blockIDBase = blockIDBase;
+		nextItemID = this.itemIDBase = itemIDBase;
+	}
+
+	public int getNextBlockID()
+	{
+		int id = nextBlockID;
+
+		while (id > 255 && id < (Block.blocksList.length - 1))
+		{
+			Block block = Block.blocksList[id];
+			if (block == null)
+			{
+				break;
+			}
+			id++;
+		}
+		nextBlockID = id + 1;
+		return id;
+	}
+
+	public int getNextItemID()
+	{
+		int id = nextItemID;
+		while (id > 255 && id < (Item.itemsList.length - 1))
+		{
+			Item item = Item.itemsList[id];
+			if (item == null)
+			{
+				break;
+			}
+			id++;
+		}
+		nextItemID = id + 1;
+		return id;
+	}
+
+}
