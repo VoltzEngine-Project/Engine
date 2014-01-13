812c8bd1f432751d412aeb37b53bb2a4f0325431
Calclavia <admin@calclavia.com>
2014-01-13 19:56:38 +0800

    Added render cube

--- src/main/java/calclavia/lib/render/RenderUtility.java
+++ src/main/java/calclavia/lib/render/RenderUtility.java
@@ -24,6 +24,7 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Icon;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.ForgeDirection;
 
@@ -33,6 +34,7 @@ import cpw.mods.fml.client.FMLClientHandler;
 
 public class RenderUtility
 {
+	public static RenderBlocks renderBlocks = new RenderBlocks();
 	public static final ResourceLocation PARTICLE_RESOURCE = new ResourceLocation("textures/particle/particles.png");
 
 	public static void setTerrainTexture()
@@ -291,6 +293,58 @@ public class RenderUtility
 			renderer.renderFaceXPos(block, 0.0D, 0.0D, 0.0D, renderer.getBlockIconFromSideAndMetadata(block, 5, metadata));
 			tessellator.draw();
 		}
+		GL11.glPopMatrix();
+	}
+
+	public static void renderCube(double x1, double y1, double z1, double x2, double y2, double z2, Block block)
+	{
+		renderCube(x1, y1, z1, x2, y2, z2, block, null);
+	}
+
+	public static void renderCube(double x1, double y1, double z1, double x2, double y2, double z2, Block block, Icon overrideTexture)
+	{
+		renderCube(x1, y1, z1, x2, y2, z2, block, overrideTexture, 0);
+	}
+
+	/**
+	 * Renders a cube with custom block boundaries.
+	 */
+	public static void renderCube(double x1, double y1, double z1, double x2, double y2, double z2, Block block, Icon overrideTexture, int meta)
+	{
+		GL11.glPushMatrix();
+		Tessellator t = Tessellator.instance;
+
+		GL11.glColor4f(1, 1, 1, 1);
+
+		renderBlocks.setRenderBounds(x1, y1, z1, x2, y2, z2);
+
+		t.startDrawingQuads();
+
+		Icon useTexture = overrideTexture != null ? overrideTexture : block.getIcon(0, meta);
+		t.setNormal(0.0F, -1.0F, 0.0F);
+		renderBlocks.renderFaceYNeg(block, 0, 0, 0, useTexture);
+
+		useTexture = overrideTexture != null ? overrideTexture : block.getIcon(1, meta);
+		t.setNormal(0.0F, 1.0F, 0.0F);
+		renderBlocks.renderFaceYPos(block, 0, 0, 0, useTexture);
+
+		useTexture = overrideTexture != null ? overrideTexture : block.getIcon(2, meta);
+		t.setNormal(0.0F, 0.0F, -1.0F);
+		renderBlocks.renderFaceZNeg(block, 0, 0, 0, useTexture);
+
+		useTexture = overrideTexture != null ? overrideTexture : block.getIcon(3, meta);
+		t.setNormal(0.0F, 0.0F, 1.0F);
+		renderBlocks.renderFaceZPos(block, 0, 0, 0, useTexture);
+
+		useTexture = overrideTexture != null ? overrideTexture : block.getIcon(4, meta);
+		t.setNormal(-1.0F, 0.0F, 0.0F);
+		renderBlocks.renderFaceXNeg(block, 0, 0, 0, useTexture);
+
+		useTexture = overrideTexture != null ? overrideTexture : block.getIcon(5, meta);
+		t.setNormal(1.0F, 0.0F, 0.0F);
+		renderBlocks.renderFaceXPos(block, 0, 0, 0, useTexture);
+		t.draw();
+
 		GL11.glPopMatrix();
 	}
 
