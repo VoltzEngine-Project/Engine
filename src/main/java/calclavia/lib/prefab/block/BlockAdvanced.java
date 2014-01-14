19d710e954bf297c9eeecdfe282f4e20e33203cb
Calclavia <admin@calclavia.com>
2014-01-14 19:31:45 +0800

    Fixed block advanced crash with TE item right click

--- src/main/java/calclavia/lib/prefab/block/BlockAdvanced.java
+++ src/main/java/calclavia/lib/prefab/block/BlockAdvanced.java
@@ -7,6 +7,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import buildcraft.api.tools.IToolWrench;
 
 /**
  * An advanced block class that is to be extended for wrenching capabilities.
@@ -75,22 +76,13 @@ public abstract class BlockAdvanced extends Block
 	 * 
 	 * @return True if it is a wrench.
 	 */
-	public boolean isUsableWrench(EntityPlayer entityPlayer, ItemStack itemStack, int x, int y, int z)
+	public boolean isUsableWrench(EntityPlayer player, ItemStack itemStack, int x, int y, int z)
 	{
-		if (entityPlayer != null && itemStack != null)
+		if (player != null && itemStack != null)
 		{
-			Class wrenchClass = itemStack.getItem().getClass();
-
-			/**
-			 * UE and Buildcraft
-			 */
-			try
+			if (itemStack.getItem() instanceof IToolWrench)
 			{
-				Method methodCanWrench = wrenchClass.getMethod("canWrench", EntityPlayer.class, Integer.TYPE, Integer.TYPE, Integer.TYPE);
-				return (Boolean) methodCanWrench.invoke(itemStack.getItem(), entityPlayer, x, y, z);
-			}
-			catch (Exception e)
-			{
+				return ((IToolWrench) itemStack.getItem()).canWrench(player, x, y, z);
 			}
 
 			/**
@@ -98,6 +90,8 @@ public abstract class BlockAdvanced extends Block
 			 */
 			try
 			{
+				Class wrenchClass = itemStack.getItem().getClass();
+
 				if (wrenchClass == Class.forName("ic2.core.item.tool.ItemToolWrench") || wrenchClass == Class.forName("ic2.core.item.tool.ItemToolWrenchElectric"))
 				{
 					return itemStack.getItemDamage() < itemStack.getMaxDamage();
@@ -116,23 +110,14 @@ public abstract class BlockAdvanced extends Block
 	 * 
 	 * @return True if damage was successfull.
 	 */
-	public boolean damageWrench(EntityPlayer entityPlayer, ItemStack itemStack, int x, int y, int z)
+	public boolean damageWrench(EntityPlayer player, ItemStack itemStack, int x, int y, int z)
 	{
-		if (this.isUsableWrench(entityPlayer, itemStack, x, y, z))
+		if (this.isUsableWrench(player, itemStack, x, y, z))
 		{
-			Class wrenchClass = itemStack.getItem().getClass();
-
-			/**
-			 * UE and Buildcraft
-			 */
-			try
+			if (itemStack.getItem() instanceof IToolWrench)
 			{
-				Method methodWrenchUsed = wrenchClass.getMethod("wrenchUsed", EntityPlayer.class, Integer.TYPE, Integer.TYPE, Integer.TYPE);
-				methodWrenchUsed.invoke(itemStack.getItem(), entityPlayer, x, y, z);
+				((IToolWrench) itemStack.getItem()).wrenchUsed(player, x, y, z);
 				return true;
-			}
-			catch (Exception e)
-			{
 			}
 
 			/**
@@ -140,10 +125,12 @@ public abstract class BlockAdvanced extends Block
 			 */
 			try
 			{
+				Class wrenchClass = itemStack.getItem().getClass();
+
 				if (wrenchClass == Class.forName("ic2.core.item.tool.ItemToolWrench") || wrenchClass == Class.forName("ic2.core.item.tool.ItemToolWrenchElectric"))
 				{
 					Method methodWrenchDamage = wrenchClass.getMethod("damage", ItemStack.class, Integer.TYPE, EntityPlayer.class);
-					methodWrenchDamage.invoke(itemStack.getItem(), itemStack, 1, entityPlayer);
+					methodWrenchDamage.invoke(itemStack.getItem(), itemStack, 1, player);
 					return true;
 				}
 			}
