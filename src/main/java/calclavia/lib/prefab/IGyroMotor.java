d3953023a21249b19e1a92355fa772c5ae978ccc
DarkGaurdsman <rseifert.phone@gmail.com>
2014-01-30 19:10:27 -0500

    Added servo handling from ICBM

---
+++ src/main/java/calclavia/lib/prefab/IGyroMotor.java
@@ -0,0 +1,13 @@
+package calclavia.lib.prefab;
+
+/** Applied to devices that have both a yaw and pitch rotation.
+ * 
+ * @author Darkguardsman */
+public interface IGyroMotor
+{
+    /** Gets the servo instance that deals with yaw axis */
+    IServo getYawServo();
+
+    /** Gets the servo instance that deals with pitch axis */
+    IServo getPitchServo();
+}
