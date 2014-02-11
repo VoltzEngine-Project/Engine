b7691c148fa1ae97d63cceedde6b50b474c8c8cd
DarkGuardsman <rseifert.phone@gmail.com>
2014-02-11 15:44:09 -0500

    Made Client Content Registry ignore class not found exceptions

--- src/main/java/calclavia/lib/content/ClientRegistryProxy.java
+++ src/main/java/calclavia/lib/content/ClientRegistryProxy.java
@@ -9,54 +9,60 @@ import cpw.mods.fml.client.registry.ClientRegistry;
 
 public class ClientRegistryProxy extends CommonRegistryProxy
 {
-	@Override
-	public void registerBlock(Block block, Class<? extends ItemBlock> itemClass, String name, String modID)
-	{
-		super.registerBlock(block, itemClass, name, modID);
+    @Override
+    public void registerBlock(Block block, Class<? extends ItemBlock> itemClass, String name, String modID)
+    {
+        super.registerBlock(block, itemClass, name, modID);
 
-		BlockInfo blockInfo = block.getClass().getAnnotation(BlockInfo.class);
+        BlockInfo blockInfo = block.getClass().getAnnotation(BlockInfo.class);
 
-		if (blockInfo != null)
-		{
-			for (int i = 0; i < blockInfo.renderer().length; i++)
-			{
-				try
-				{
-					Class<? extends TileEntity> tileClass = (Class<? extends TileEntity>) Class.forName(blockInfo.tileEntity()[i]);
-					Class<? extends TileEntitySpecialRenderer> rendererClass = (Class<? extends TileEntitySpecialRenderer>) Class.forName(blockInfo.renderer()[i]);
-					ClientRegistry.bindTileEntitySpecialRenderer(tileClass, rendererClass.newInstance());
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-					throw new RuntimeException("Failed to register block for: " + name);
-				}
-			}
-		}
-	}
+        if (blockInfo != null)
+        {
+            for (int i = 0; i < blockInfo.renderer().length; i++)
+            {
+                try
+                {
+                    Class<? extends TileEntity> tileClass = (Class<? extends TileEntity>) Class.forName(blockInfo.tileEntity()[i]);
+                    Class<? extends TileEntitySpecialRenderer> rendererClass = (Class<? extends TileEntitySpecialRenderer>) Class.forName(blockInfo.renderer()[i]);
+                    ClientRegistry.bindTileEntitySpecialRenderer(tileClass, rendererClass.newInstance());
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                    throw new RuntimeException("[ClientContentRegistry]Failed to register block for: " + name);
+                }
+            }
+        }
+    }
 
-	@Override
-	public void registerTileEntity(String name, Class<? extends TileEntity> tileClass)
-	{
-		super.registerTileEntity(name, tileClass);
+    @Override
+    public void registerTileEntity(String name, Class<? extends TileEntity> tileClass)
+    {
+        super.registerTileEntity(name, tileClass);
 
-		TileEntitySpecialRenderer tileRenderer = null;
+        TileEntitySpecialRenderer tileRenderer = null;
 
-		try
-		{
+        try
+        {
 
-			String rendererName = tileClass.getName().replaceFirst("Tile", "Render");
-			Class renderClass = Class.forName(rendererName);
-			tileRenderer = (TileEntitySpecialRenderer) renderClass.newInstance();
-		}
-		catch (Exception e)
-		{
-			Calclavia.LOGGER.severe("Failed to register TileEntity renderer for " + name);
-		}
+            String rendererName = tileClass.getName().replaceFirst("Tile", "Render");
+            Class renderClass = Class.forName(rendererName);
+            tileRenderer = (TileEntitySpecialRenderer) renderClass.newInstance();
+        }
+        catch (ClassNotFoundException e)
+        {
+            //From DarkGuardsman: Ignore these types of exceptions as this just means the block has no renderer for the tile given
+        }
+        catch (Exception e)
+        {
+            Calclavia.LOGGER.severe("[ClientContentRegistry]Failed to register TileEntity renderer for " + name);
+            e.printStackTrace();
+            
+        }
 
-		if (tileRenderer != null)
-		{
-			ClientRegistry.bindTileEntitySpecialRenderer(tileClass, tileRenderer);
-		}
-	}
+        if (tileRenderer != null)
+        {
+            ClientRegistry.bindTileEntitySpecialRenderer(tileClass, tileRenderer);
+        }
+    }
 }
