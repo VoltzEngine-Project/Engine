2a3127bbd9a0b6c98782b19d0f89fe579deb838c
tgame14 <tgameBusiness@gmail.com>
2014-02-23 11:37:48 +0200

    Modules now support late load phase registrations

--- src/main/java/calclavia/lib/modproxy/IIntegrationProxy.java
+++ src/main/java/calclavia/lib/modproxy/IIntegrationProxy.java
@@ -6,7 +6,15 @@ package calclavia.lib.modproxy;
  * @since 23/02/14
  * @author tgame14
  */
-public interface IProxy extends IMod
+public interface IIntegrationProxy
 {
+    //TODO: Add support for FML Events that may be needed for integration
+
+    public void preInit();
+
+    public void init();
+
+    public void postInit();
+
     public String modId();
 }
