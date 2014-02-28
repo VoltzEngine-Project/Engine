90b56237e7c0ac8ba8fda67c367636245425178f
Calclavia <admin@calclavia.com>
2014-02-28 21:28:28 +0800

    Tooltips on items now need to use "shift" to access

--- src/main/java/calclavia/lib/prefab/item/ItemBlockMetadata.java
+++ src/main/java/calclavia/lib/prefab/item/ItemBlockMetadata.java
@@ -2,9 +2,12 @@ package calclavia.lib.prefab.item;
 
 import java.util.List;
 
+import org.lwjgl.input.Keyboard;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import calclavia.lib.render.EnumColor;
 import calclavia.lib.utility.LanguageUtility;
 
 public class ItemBlockMetadata extends ItemBlock
@@ -31,13 +34,20 @@ public class ItemBlockMetadata extends ItemBlock
 	}
 
 	@Override
-	public void addInformation(ItemStack itemstack, EntityPlayer par2EntityPlayer, List info, boolean par4)
+	public void addInformation(ItemStack itemStack, EntityPlayer par2EntityPlayer, List list, boolean par4)
 	{
-		String tooltip = LanguageUtility.getLocal(getUnlocalizedName(itemstack) + ".tooltip");
+		String tooltip = LanguageUtility.getLocal(getUnlocalizedName(itemStack) + ".tooltip");
 
 		if (tooltip != null && tooltip.length() > 0)
 		{
-			info.addAll(LanguageUtility.splitStringPerWord(tooltip, 5));
+			if (!Keyboard.isKeyDown(Keyboard.KEY_LSHIFT))
+			{
+				list.add(LanguageUtility.getLocal("tooltip.noShift").replace("%0", EnumColor.AQUA.toString()).replace("%1", EnumColor.GREY.toString()));
+			}
+			else
+			{
+				list.addAll(LanguageUtility.splitStringPerWord(tooltip, 5));
+			}
 		}
 	}
 }
