3163fba32ad1d95b00d1b068ffa7d9d8594b7f4f
Robert Seifert <rseifert.phone@gmail.com>
2014-03-04 00:29:59 -0500

    Fixed creative debug block not saving IO maps

--- src/main/java/calclavia/components/creative/TileInfiniteEnergy.java
+++ src/main/java/calclavia/components/creative/TileInfiniteEnergy.java
@@ -4,31 +4,35 @@ import net.minecraftforge.common.ForgeDirection;
 import universalelectricity.api.energy.EnergyStorageHandler;
 import calclavia.lib.prefab.tile.TileElectrical;
 
+/** Creative mode way to quickly debug machines that use or produce electrical energy
+ * 
+ * @author DarkGuardsman */
 public class TileInfiniteEnergy extends TileElectrical
 {
-	public TileInfiniteEnergy()
-	{
-		this.energy = new EnergyStorageHandler(Long.MAX_VALUE);
-		this.energy.setMaxExtract(Long.MAX_VALUE);
-	}
+    public TileInfiniteEnergy()
+    {
+        this.saveIOMap = true;
+        this.energy = new EnergyStorageHandler(Long.MAX_VALUE);
+        this.energy.setMaxExtract(Long.MAX_VALUE);
+    }
 
-	@Override
-	public void updateEntity()
-	{
-		super.updateEntity();
-		this.energy.setEnergy(Long.MAX_VALUE);
-		this.produce();
-	}
+    @Override
+    public void updateEntity()
+    {
+        super.updateEntity();
+        this.energy.setEnergy(Long.MAX_VALUE);
+        this.produce();
+    }
 
-	@Override
-	public long onReceiveEnergy(ForgeDirection from, long receive, boolean doReceive)
-	{
-		return receive;
-	}
+    @Override
+    public long onReceiveEnergy(ForgeDirection from, long receive, boolean doReceive)
+    {
+        return receive;
+    }
 
-	@Override
-	public long onExtractEnergy(ForgeDirection from, long request, boolean doExtract)
-	{
-		return request;
-	}
+    @Override
+    public long onExtractEnergy(ForgeDirection from, long request, boolean doExtract)
+    {
+        return request;
+    }
 }
