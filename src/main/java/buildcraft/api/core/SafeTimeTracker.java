79b98f47ac2b21d0052735ee596b34033d8ce86e
Calclavia <admin@calclavia.com>
2014-03-10 02:14:18 +0800

    Updated to final BuildCraft 1.6 API

--- src/main/java/buildcraft/api/core/SafeTimeTracker.java
+++ src/main/java/buildcraft/api/core/SafeTimeTracker.java
@@ -7,81 +7,35 @@
  */
 package buildcraft.api.core;
 
-import net.minecraft.world.World;
+public class SafeTimeTracker {
 
-public class SafeTimeTracker
-{
-
-	private long lastMark = Long.MIN_VALUE;
+	public static long worldTime;
+	private long lastMark = worldTime;
 	private long duration = -1;
-	private long randomRange = 0;
-	private long lastRandomDelay = 0;
-	private long internalDelay = 1;
-	
-	/**
-	 * @deprecated should use constructors with parameters instead
-	 */
-	 
-	public SafeTimeTracker () {
-		
-	}
-	
-	public SafeTimeTracker (long delay) {
-		internalDelay = delay;
-	}
-	
-	/**
-	 * In many situations, it is a bad idea to have all objects of the same
-	 * kind to be waiting for the exact same amount of time, as that can lead
-	 * to some situation where they're all synchronized and got to work all
-	 * at the same time. When created with a random range, the mark that is set
-	 * when reaching the expect delay will be added with a random number
-	 * between [0, range[, meaning that the event will take between 0 and range
-	 * more tick to run.
-	 */
-	public SafeTimeTracker (long delay, long random) {
-		internalDelay = delay;
-		randomRange = random;
-	}
-	
 	/**
 	 * Return true if a given delay has passed since last time marked was called
 	 * successfully.
-	 * @deprecated should use the constructor with a delay instead, and call
-	 * this function without a parameter
 	 */
-	public boolean markTimeIfDelay(World world, long delay)
-	{
-		if (world == null)
-			return false;
-
-		long currentTime = world.getTotalWorldTime();
-
-		if (currentTime < lastMark)
-		{
-			lastMark = currentTime;
-			return false;
-		}
-		else if (lastMark + delay + lastRandomDelay <= currentTime)
-		{
-			duration = currentTime - lastMark;
-			lastMark = currentTime;
-			lastRandomDelay = (int) (Math.random() * randomRage);
-			
+	public boolean markTimeIfDelay(long delay) {
+		long timePassed = worldTime - lastMark;
+		if (timePassed >= delay) {
+			duration = timePassed;
+			lastMark = worldTime;
 			return true;
 		}
-		else
+		if (worldTime < lastMark) {
+			lastMark = worldTime;
 			return false;
+		}
+		return false;
 
 	}
 
-	public long durationOfLastDelay()
-	{
+	public long durationOfLastDelay() {
 		return duration > 0 ? duration : 0;
 	}
 
-	public void markTime(World world)
-	{
-		lastMark = world.getTotalWorldTime();
+	public void markTime() {
+		lastMark = worldTime;
 	}
 }
--- src/main/java/buildcraft/api/power/PowerHandler.java
+++ src/main/java/buildcraft/api/power/PowerHandler.java
@@ -7,40 +7,18 @@
  */
 package buildcraft.api.power;
 
+import buildcraft.api.core.SafeTimeTracker;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.ForgeDirection;
-import buildcraft.api.core.SafeTimeTracker;
 
-/**
- * The PowerHandler is similar to FluidTank in that it holds your power and
- * allows standardized interaction between machines.
- * 
- * To receive power to your machine you needs create an instance of PowerHandler
- * and implement IPowerReceptor on the TileEntity.
- * 
- * If you plan emit power, you need only implement IPowerEmitter. You do not
- * need a PowerHandler. Engines have a PowerHandler because they can also
- * receive power from other Engines.
- * 
- * See TileRefinery for a simple example of a power using machine.
- * 
- * @see IPowerReceptor
- * @see IPowerEmitter
- * 
- * @author CovertJaguar <http://www.railcraft.info/>
- */
-public final class PowerHandler
-{
+public final class PowerHandler {
 
-	public static enum Type
-	{
+	public static enum Type {
 
 		ENGINE, GATE, MACHINE, PIPE, STORAGE;
 
-		public boolean canReceiveFromPipes()
-		{
-			switch (this)
-			{
+		public boolean canReceiveFromPipes() {
+			switch (this) {
 				case MACHINE:
 				case STORAGE:
 					return true;
@@ -49,10 +27,8 @@ public final class PowerHandler
 			}
 		}
 
-		public boolean eatsEngineExcess()
-		{
-			switch (this)
-			{
+		public boolean eatsEngineExcess() {
+			switch (this) {
 				case MACHINE:
 				case STORAGE:
 					return true;
@@ -62,34 +38,19 @@ public final class PowerHandler
 		}
 	}
 
-	/**
-	 * Extend this class to create custom Perdition algorithms (its not final).
-	 * 
-	 * NOTE: It is not possible to create a Zero perdition algorithm.
-	 */
-	public static class PerditionCalculator
-	{
+	public static class PerditionCalculator {
 
 		public static final float DEFAULT_POWERLOSS = 1F;
 		public static final float MIN_POWERLOSS = 0.01F;
 		private final float powerLoss;
 
-		public PerditionCalculator()
-		{
+		public PerditionCalculator() {
 			powerLoss = DEFAULT_POWERLOSS;
 		}
 
-		/**
-		 * Simple constructor for simple Perdition per tick.
-		 * 
-		 * @param powerLoss power loss per tick
-		 */
-		public PerditionCalculator(float powerLoss)
-		{
+		public PerditionCalculator(float powerLoss) {
 			if (powerLoss < MIN_POWERLOSS)
-			{
 				powerLoss = MIN_POWERLOSS;
-			}
 			this.powerLoss = powerLoss;
 		}
 
@@ -97,37 +58,19 @@ public final class PowerHandler
 		 * Apply the perdition algorithm to the current stored energy. This
 		 * function can only be called once per tick, but it might not be called
 		 * every tick. It is triggered by any manipulation of the stored energy.
-		 * 
+		 *
 		 * @param powerHandler the PowerHandler requesting the perdition update
 		 * @param current the current stored energy
 		 * @param ticksPassed ticks since the last time this function was called
 		 * @return
 		 */
-		public float applyPerdition(PowerHandler powerHandler, float current, long ticksPassed)
-		{
-			// float prev = current;
+		public float applyPerdition(PowerHandler powerHandler, float current, long ticksPassed) {
 			current -= powerLoss * ticksPassed;
 			if (current < 0)
-			{
 				current = 0;
-			}
-			// powerHandler.totalLostPower += prev - current;
 			return current;
 		}
-
-		/**
-		 * Taxes a flat rate on all incoming power.
-		 * 
-		 * Defaults to 0% tax rate.
-		 * 
-		 * @return percent of input to tax
-		 */
-		public float getTaxPercent()
-		{
-			return 0;
-		}
 	}
-
 	public static final PerditionCalculator DEFAULT_PERDITION = new PerditionCalculator();
 	private float minEnergyReceived;
 	private float maxEnergyReceived;
@@ -143,53 +86,40 @@ public final class PowerHandler
 	private final PowerReceiver receiver;
 	private final Type type;
 
-	// Debug
-	// private double totalLostPower = 0;
-	// private double totalReceivedPower = 0;
-	// private double totalUsedPower = 0;
-	// private long startTime = -1;
-
-	public PowerHandler(IPowerReceptor receptor, Type type)
-	{
+	public PowerHandler(IPowerReceptor receptor, Type type) {
 		this.receptor = receptor;
 		this.type = type;
 		this.receiver = new PowerReceiver();
 		this.perdition = DEFAULT_PERDITION;
 	}
 
-	public PowerReceiver getPowerReceiver()
-	{
+	public PowerReceiver getPowerReceiver() {
 		return receiver;
 	}
 
-	public float getMinEnergyReceived()
-	{
+	public float getMinEnergyReceived() {
 		return minEnergyReceived;
 	}
 
-	public float getMaxEnergyReceived()
-	{
+	public float getMaxEnergyReceived() {
 		return maxEnergyReceived;
 	}
 
-	public float getMaxEnergyStored()
-	{
+	public float getMaxEnergyStored() {
 		return maxEnergyStored;
 	}
 
-	public float getActivationEnergy()
-	{
+	public float getActivationEnergy() {
 		return activationEnergy;
 	}
 
-	public float getEnergyStored()
-	{
+	public float getEnergyStored() {
 		return energyStored;
 	}
 
 	/**
 	 * Setup your PowerHandler's settings.
-	 * 
+	 *
 	 * @param minEnergyReceived This is the minimum about of power that will be
 	 * accepted by the PowerHandler. This should generally be greater than the
 	 * activationEnergy if you plan to use the doWork() callback. Anything
@@ -205,32 +135,17 @@ public final class PowerHandler
 	 * store. Values tend to range between 100 and 5000. With 1000 and 1500
 	 * being common.
 	 */
-	public void configure(float minEnergyReceived, float maxEnergyReceived, float activationEnergy, float maxStoredEnergy)
-	{
+	public void configure(float minEnergyReceived, float maxEnergyReceived, float activationEnergy, float maxStoredEnergy) {
 		if (minEnergyReceived > maxEnergyReceived)
-		{
 			maxEnergyReceived = minEnergyReceived;
-		}
 		this.minEnergyReceived = minEnergyReceived;
 		this.maxEnergyReceived = maxEnergyReceived;
 		this.maxEnergyStored = maxStoredEnergy;
 		this.activationEnergy = activationEnergy;
 	}
 
-	/**
-	 * Allows you define perdition in terms of loss/ticks.
-	 * 
-	 * This function is mostly for legacy implementations. See
-	 * PerditionCalculator for more complex perdition formulas.
-	 * 
-	 * @param powerLoss
-	 * @param powerLossRegularity
-	 * @see PerditionCalculator
-	 */
-	public void configurePowerPerdition(int powerLoss, int powerLossRegularity)
-	{
-		if (powerLoss == 0 || powerLossRegularity == 0)
-		{
+	public void configurePowerPerdition(int powerLoss, int powerLossRegularity) {
+		if (powerLoss == 0 || powerLossRegularity == 0) {
 			perdition = new PerditionCalculator(0);
 			return;
 		}
@@ -240,57 +155,40 @@ public final class PowerHandler
 	/**
 	 * Allows you to define a new PerditionCalculator class to handler perdition
 	 * calculations.
-	 * 
+	 *
 	 * For example if you want exponentially increasing loss based on amount
 	 * stored.
-	 * 
+	 *
 	 * @param perdition
 	 */
-	public void setPerdition(PerditionCalculator perdition)
-	{
+	public void setPerdition(PerditionCalculator perdition) {
 		if (perdition == null)
 			perdition = DEFAULT_PERDITION;
 		this.perdition = perdition;
 	}
 
-	public PerditionCalculator getPerdition()
-	{
-		if (perdition == null)
-			return DEFAULT_PERDITION;
+	public PerditionCalculator getPerdition() {
 		return perdition;
 	}
 
 	/**
 	 * Ticks the power handler. You should call this if you can, but its not
 	 * required.
-	 * 
+	 *
 	 * If you don't call it, the possibility exists for some weirdness with the
 	 * perdition algorithm and work callback as its possible they will not be
 	 * called on every tick they otherwise would be. You should be able to
 	 * design around this though if you are aware of the limitations.
 	 */
-	public void update()
-	{
-		// if (startTime == -1)
-		// startTime = receptor.getWorld().getTotalWorldTime();
-		// else {
-		// long duration = receptor.getWorld().getTotalWorldTime() - startTime;
-		// System.out.printf("Power Stats: %s - Stored: %.2f Gained: %.2f - %.2f/t Lost: %.2f - %.2f/t Used: %.2f - %.2f/t%n",
-		// receptor.getClass().getSimpleName(), energyStored, totalReceivedPower, totalReceivedPower
-		// / duration, totalLostPower, totalLostPower / duration, totalUsedPower, totalUsedPower /
-		// duration);
-		// }
-
+	public void update() {
 		applyPerdition();
 		applyWork();
 		validateEnergy();
 	}
 
-	private void applyPerdition()
-	{
-		if (perditionTracker.markTimeIfDelay(receptor.getWorld(), 1) && energyStored > 0)
-		{
-			float newEnergy = getPerdition().applyPerdition(this, energyStored, perditionTracker.durationOfLastDelay());
+	private void applyPerdition() {
+		if (perditionTracker.markTimeIfDelay(1) && energyStored > 0) {
+			float newEnergy = perdition.applyPerdition(this, energyStored, perditionTracker.durationOfLastDelay());
 			if (newEnergy == 0 || newEnergy < energyStored)
 				energyStored = newEnergy;
 			else
@@ -299,30 +197,19 @@ public final class PowerHandler
 		}
 	}
 
-	private void applyWork()
-	{
+	private void applyWork() {
 		if (energyStored >= activationEnergy)
-		{
-			if (doWorkTracker.markTimeIfDelay(receptor.getWorld(), 1))
-			{
+			if (doWorkTracker.markTimeIfDelay(1))
 				receptor.doWork(this);
-			}
-		}
 	}
 
-	private void updateSources(ForgeDirection source)
-	{
-		if (sourcesTracker.markTimeIfDelay(receptor.getWorld(), 1))
-		{
-			for (int i = 0; i < 6; ++i)
-			{
+	private void updateSources(ForgeDirection source) {
+		if (sourcesTracker.markTimeIfDelay(1))
+			for (int i = 0; i < 6; ++i) {
 				powerSources[i] -= sourcesTracker.durationOfLastDelay();
 				if (powerSources[i] < 0)
-				{
 					powerSources[i] = 0;
-				}
 			}
-		}
 
 		if (source != null)
 			powerSources[source.ordinal()] = 10;
@@ -331,180 +218,133 @@ public final class PowerHandler
 	/**
 	 * Extract energy from the PowerHandler. You must call this even if doWork()
 	 * triggers.
-	 * 
+	 *
 	 * @param min
 	 * @param max
 	 * @param doUse
 	 * @return amount used
 	 */
-	public float useEnergy(float min, float max, boolean doUse)
-	{
+	public float useEnergy(float min, float max, boolean doUse) {
 		applyPerdition();
 
 		float result = 0;
 
 		if (energyStored >= min)
-		{
-			if (energyStored <= max)
-			{
+			if (energyStored <= max) {
 				result = energyStored;
 				if (doUse)
-				{
 					energyStored = 0;
-				}
-			}
-			else
-			{
+			} else {
 				result = max;
 				if (doUse)
-				{
 					energyStored -= max;
-				}
 			}
-		}
 
 		validateEnergy();
-
-		// if (doUse)
-		// totalUsedPower += result;
 
 		return result;
 	}
 
-	public void readFromNBT(NBTTagCompound data)
-	{
+	public void readFromNBT(NBTTagCompound data) {
 		readFromNBT(data, "powerProvider");
 	}
 
-	public void readFromNBT(NBTTagCompound data, String tag)
-	{
+	public void readFromNBT(NBTTagCompound data, String tag) {
 		NBTTagCompound nbt = data.getCompoundTag(tag);
 		energyStored = nbt.getFloat("storedEnergy");
 	}
 
-	public void writeToNBT(NBTTagCompound data)
-	{
+	public void writeToNBT(NBTTagCompound data) {
 		writeToNBT(data, "powerProvider");
 	}
 
-	public void writeToNBT(NBTTagCompound data, String tag)
-	{
+	public void writeToNBT(NBTTagCompound data, String tag) {
 		NBTTagCompound nbt = new NBTTagCompound();
 		nbt.setFloat("storedEnergy", energyStored);
 		data.setCompoundTag(tag, nbt);
 	}
 
-	public final class PowerReceiver
-	{
+	public final class PowerReceiver {
 
-		private PowerReceiver()
-		{
+		private PowerReceiver() {
 		}
 
-		public float getMinEnergyReceived()
-		{
+		public float getMinEnergyReceived() {
 			return minEnergyReceived;
 		}
 
-		public float getMaxEnergyReceived()
-		{
+		public float getMaxEnergyReceived() {
 			return maxEnergyReceived;
 		}
 
-		public float getMaxEnergyStored()
-		{
+		public float getMaxEnergyStored() {
 			return maxEnergyStored;
 		}
 
-		public float getActivationEnergy()
-		{
+		public float getActivationEnergy() {
 			return activationEnergy;
 		}
 
-		public float getEnergyStored()
-		{
+		public float getEnergyStored() {
 			return energyStored;
 		}
 
-		public Type getType()
-		{
+		public Type getType() {
 			return type;
 		}
 
-		public void update()
-		{
+		public void update() {
 			PowerHandler.this.update();
 		}
 
 		/**
 		 * The amount of power that this PowerHandler currently needs.
-		 * 
+		 *
 		 * @return
 		 */
-		public float powerRequest()
-		{
-			update();
+		public float powerRequest() {
 			return Math.min(maxEnergyReceived, maxEnergyStored - energyStored);
 		}
 
 		/**
 		 * Add power to the PowerReceiver from an external source.
-		 * 
-		 * IPowerEmitters are responsible for calling this themselves.
-		 * 
+		 *
 		 * @param quantity
 		 * @param from
 		 * @return the amount of power used
 		 */
-		public float receiveEnergy(Type source, final float quantity, ForgeDirection from)
-		{
+		public float receiveEnergy(Type source, final float quantity, ForgeDirection from) {
 			float used = quantity;
 			if (source == Type.ENGINE)
-			{
 				if (used < minEnergyReceived)
-				{
 					return 0;
-				}
 				else if (used > maxEnergyReceived)
-				{
 					used = maxEnergyReceived;
-				}
-			}
 
 			updateSources(from);
-
-			used -= used * getPerdition().getTaxPercent();
 
 			used = addEnergy(used);
 
 			applyWork();
 
 			if (source == Type.ENGINE && type.eatsEngineExcess())
-			{
-				used = Math.min(quantity, maxEnergyReceived);
-			}
-
-			// totalReceivedPower += used;
+				return Math.min(quantity, maxEnergyReceived);
 
 			return used;
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @return the amount the power changed by
 	 */
-	public float addEnergy(float quantity)
-	{
+	public float addEnergy(float quantity) {
 		energyStored += quantity;
 
-		if (energyStored > maxEnergyStored)
-		{
+		if (energyStored > maxEnergyStored) {
 			quantity -= energyStored - maxEnergyStored;
 			energyStored = maxEnergyStored;
-		}
-		else if (energyStored < 0)
-		{
+		} else if (energyStored < 0) {
 			quantity -= energyStored;
 			energyStored = 0;
 		}
@@ -514,26 +354,19 @@ public final class PowerHandler
 		return quantity;
 	}
 
-	public void setEnergy(float quantity)
-	{
+	public void setEnergy(float quantity) {
 		this.energyStored = quantity;
 		validateEnergy();
 	}
 
-	public boolean isPowerSource(ForgeDirection from)
-	{
+	public boolean isPowerSource(ForgeDirection from) {
 		return powerSources[from.ordinal()] != 0;
 	}
 
-	private void validateEnergy()
-	{
+	private void validateEnergy() {
 		if (energyStored < 0)
-		{
 			energyStored = 0;
-		}
 		if (energyStored > maxEnergyStored)
-		{
 			energyStored = maxEnergyStored;
-		}
 	}
 }
