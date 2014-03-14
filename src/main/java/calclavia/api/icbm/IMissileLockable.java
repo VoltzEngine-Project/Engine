8cb5f2982fcfa1c0424e91c31baf388dac692385
Robert Seifert <rseifert.phone@gmail.com>
2014-03-14 15:20:36 -0400

    Deprecated IMissileLockable in favor of ITarget

--- src/main/java/calclavia/api/icbm/IMissileLockable.java
+++ src/main/java/calclavia/api/icbm/IMissileLockable.java
@@ -5,6 +5,7 @@ import universalelectricity.api.vector.Vector3;
 /** Implement this to your entity if you want antiballistic missiles to be able to lock onto it.
  * 
  * @author Calclavia */
+@Deprecated //Replaced by ITarget
 public interface IMissileLockable
 {
     /** Can this entity be locked on by a missile?
