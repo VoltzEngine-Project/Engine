378d0c58ea245f15deaa81716e7c68cdbe9d6a03
Calclavia <admin@calclavia.com>
2014-03-15 22:46:09 +0800

    Added ModelCube prefab

--- src/main/java/calclavia/api/atomicscience/IFissileMaterial.java
+++
@@ -1,8 +0,0 @@
-package calclavia.api.atomicscience;
-
-import net.minecraft.item.ItemStack;
-
-public interface IFissileMaterial
-{
-	public void onFissile(ItemStack itemStack, IReactor reactor);
-}
