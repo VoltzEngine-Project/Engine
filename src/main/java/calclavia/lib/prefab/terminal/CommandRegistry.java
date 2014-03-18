98172aa6a66ffd366999c3fe3975a6a95a319b4c
Robert Seifert <rseifert.phone@gmail.com>
2014-03-18 16:58:32 -0400

    Change how terminal commands are processed.

--- src/main/java/calclavia/lib/prefab/terminal/CommandRegistry.java
+++ src/main/java/calclavia/lib/prefab/terminal/CommandRegistry.java
@@ -8,66 +8,68 @@ import calclavia.lib.access.GroupRegistry;
 
 public class CommandRegistry
 {
-	public static final List<ITerminalCommand> COMMANDS = new ArrayList<ITerminalCommand>();
+    public static final List<ITerminalCommand> COMMANDS = new ArrayList<ITerminalCommand>();
 
-	/**
-	 * @param prefix - what the command starts with for example /time
-	 * @param cmd - Cmd instance that will execute the command
-	 */
-	public static void register(ITerminalCommand cmd, String group)
-	{
-		if (!COMMANDS.contains(cmd))
-		{
-			COMMANDS.add(cmd);
-			if (group != null)
-			{
-				if (GroupRegistry.groupDefaultNodes.containsKey(group))
-				{
-					List<String> stra = new ArrayList<String>();
-					stra.add(cmd.getCommandName());
-				}
-			}
-		}
-	}
+    /** @param prefix - what the command starts with for example /time
+     * @param cmd - Cmd instance that will execute the command */
+    public static void register(ITerminalCommand cmd, String group)
+    {
+        if (!COMMANDS.contains(cmd))
+        {
+            COMMANDS.add(cmd);
+            if (group != null)
+            {
+                if (GroupRegistry.groupDefaultNodes.containsKey(group))
+                {
+                    List<String> stra = new ArrayList<String>();
+                    stra.add(cmd.getCommandName());
+                }
+            }
+        }
+    }
 
-	/**
-	 * When a player uses a command in any CMD machine it pass threw here first
-	 * 
-	 * @param terminal - The terminal, can be cast to TileEntity.
-	 */
-	public static boolean onCommand(EntityPlayer player, ITerminal terminal, String cmd)
-	{
-		if (cmd != null && cmd != "")
-		{
-			String[] args = cmd.split(" ");
+    /** When a player uses a command in any CMD machine it pass threw here first
+     * 
+     * @param terminal - The terminal, can be cast to TileEntity. */
+    public static List<String> onCommand(EntityPlayer player, ITerminal terminal, String cmd)
+    {
+        List<String> output_list = new ArrayList<String>();
+        if (cmd != null && cmd != "")
+        {
+            System.out.println("onCommand: " + cmd);
+            String[] args = cmd.split(" ");
 
-			if (args[0] != null)
-			{
-				for (ITerminalCommand command : COMMANDS)
-				{
-					if (command.getCommandName().equalsIgnoreCase(args[0]))
-					{
-						if (command.canSupport(terminal) && command.getNode(args) != null)
-						{
-							if (!terminal.canUse(command.getNode(args), player))
-							{
-								terminal.addToConsole("Access Denied.");
-								return false;
-							}
-							else
-							{
-								if (command.called(player, terminal, args))
-								{
-									return true;
-								}
-							}
-						}
-					}
-				}
-			}
-
-			terminal.addToConsole("Unknown Command.");
-		}
-		return false;
-	}
+            if (args[0] != null)
+            {
+                for (ITerminalCommand command : COMMANDS)
+                {
+                    if (command.getCommandName().equalsIgnoreCase(args[0]))
+                    {
+                        if (command.canSupport(terminal) && command.getNode(args) != null)
+                        {
+                            if (!terminal.canUse(command.getNode(args), player))
+                            {
+                                output_list.add("Access Denied");
+                                terminal.addToConsole(output_list);
+                                return output_list;
+                            }
+                            else
+                            {
+                                List<String> out = command.called(player, terminal, args);
+                                if(out != null && !out.isEmpty())
+                                {
+                                    output_list.addAll(out);
+                                    terminal.addToConsole(output_list);
+                                    return output_list;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            output_list.add("Unkown Command.");
+        }
+        terminal.addToConsole(output_list);
+        return output_list;
+    }
 }
