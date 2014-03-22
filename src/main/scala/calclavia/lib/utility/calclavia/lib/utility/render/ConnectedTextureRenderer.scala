8c282e0a9081f4ef029c7a062a57d3653f31b749
Calclavia <admin@calclavia.com>
2014-03-22 21:16:36 +0800

    Added ConnectedTextureRenderer prefab

---
+++ src/main/scala/calclavia/lib/utility/calclavia/lib/utility/render/ConnectedTextureRenderer.scala
@@ -0,0 +1,50 @@
+package calclavia.lib.render
+
+import org.lwjgl.opengl.GL11
+import calclavia.lib.utility.render.RenderBlockUtility
+import net.minecraftforge.common.ForgeDirection
+import universalelectricity.api.vector.Vector3
+import calclavia.lib.utility.WorldUtility
+import net.minecraft.client.renderer.texture.TextureMap
+import calclavia.lib.content.module.TileRender
+import net.minecraft.item.ItemStack
+import net.minecraft.client.renderer.RenderBlocks
+import net.minecraft.tileentity.TileEntity
+
+/**
+ * A generic TileEntity connected texture renderer.
+ * Created by Calclavia on 3/22/2014.
+ */
+class ConnectedTextureRenderer(tile: TileEntity, edgeTexture: String) extends TileRender
+{
+  override def renderItem(itemStack: ItemStack): Boolean =
+  {
+    GL11.glTranslated(0.5, 0.5, 0.5)
+    RenderBlockUtility.tessellateBlockWithConnectedTextures(itemStack.getItemDamage, tile.getBlockType, null, RenderUtility.getIcon(edgeTexture))
+    return true
+  }
+
+  override def renderStatic(renderer: RenderBlocks, position: Vector3): Boolean =
+  {
+    val world = tile.getWorldObj
+    val x = tile.xCoord
+    val y = tile.yCoord
+    val z = tile.zCoord
+    var sideMap: Byte = 0
+
+    for (dir <- ForgeDirection.VALID_DIRECTIONS)
+    {
+      val check = new Vector3(tile).translate(dir)
+      val checkTile = check.getTileEntity(world)
+
+      if (checkTile != null && checkTile.getClass == tile.getClass && check.getBlockMetadata(world) == tile.getBlockMetadata)
+      {
+        sideMap = WorldUtility.setEnableSide(sideMap, dir, true)
+      }
+    }
+
+    RenderBlockUtility.tessellateBlockWithConnectedTextures(sideMap, world, x, y, z, tile.getBlockType, null, RenderUtility.getIcon(edgeTexture))
+    RenderUtility.bind(TextureMap.locationBlocksTexture)
+    return true
+  }
+}
