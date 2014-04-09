1bc51fd2b2ad4be4dded8b3235ab9deb0d26c2cd
tgame14 <tgameBusiness@gmail.com>
2014-04-09 19:40:56 +0300

    Stub to force rebuild

--- src/main/scala/calclavia/components/CalclaviaLoader.java
+++ src/main/scala/calclavia/components/CalclaviaLoader.java
@@ -101,6 +101,7 @@ public class CalclaviaLoader
 	public static final String DOMAIN = "calclavia";
 	public static final String PREFIX = DOMAIN + ":";
 	public static final String[] LANGUAGES_SUPPORTED = new String[] { "en_US", "zh_CN", "es_ES", "it_IT", "nl_NL", "de_DE", "tr_TR", "ru_RU" };
+	
 	/**
 	 * Auto-incrementing configuration IDs. Use this to make sure no config ID is the same.
 	 */
