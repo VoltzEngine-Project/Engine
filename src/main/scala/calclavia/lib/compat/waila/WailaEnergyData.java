9898bc505f46c1e40e48f079bfd7db3685307ae0
Robert S <rseifert.phone@gmail.com>
2014-04-13 01:35:33 -0400

    Added waila support for IEnergyContainers

--- src/main/scala/calclavia/lib/compat/waila/WailaEnergyData.java
+++ src/main/scala/calclavia/lib/compat/waila/WailaEnergyData.java
@@ -11,45 +11,47 @@ import net.minecraftforge.common.ForgeDirection;
 
 import java.util.List;
 
+import universalelectricity.api.energy.IEnergyContainer;
 import universalelectricity.api.energy.UnitDisplay;
 import universalelectricity.api.energy.UnitDisplay.Unit;
 
-/**
- * Waila support for Electrical tiles
- * @author tgame14
- */
+/** Waila support for Electrical tiles
+ * 
+ * @author tgame14 */
 public class WailaEnergyData implements IWailaDataProvider
 {
-	@Override
-	public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config)
-	{
-		return null;
-	}
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        TileEntity tile = accessor.getTileEntity();
+        if (tile instanceof TileElectrical)
+        {
+            TileElectrical te = (TileElectrical) tile;
+            currenttip.add(LanguageUtility.getLocal("info.energy.waila") + " " + UnitDisplay.getDisplayShort(te.getEnergyHandler().getEnergy(), Unit.JOULES) + " / " + UnitDisplay.getDisplayShort(te.getEnergyHandler().getEnergyCapacity(), Unit.JOULES));
+        }
+        else if (tile instanceof IEnergyContainer)
+        {
+            IEnergyContainer te = (IEnergyContainer) tile;
+            currenttip.add(LanguageUtility.getLocal("info.energy.waila") + " " + UnitDisplay.getDisplayShort(te.getEnergy(accessor.getSide()), Unit.JOULES) + " / " + UnitDisplay.getDisplayShort(te.getEnergyCapacity(accessor.getSide()), Unit.JOULES));
+        }
+        return currenttip;
+    }
 
-	@Override
-	public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
-	{
-		return currenttip;
-	}
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return currenttip;
+    }
 
-	@Override
-	public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
-	{
-		TileEntity tile = accessor.getTileEntity();
-		if (!(tile instanceof TileElectrical))
-		{
-			return currenttip;
-		}
-		TileElectrical te = (TileElectrical) tile;
-		currenttip.add(LanguageUtility.getLocal("info.energylevel.waila") + " " + UnitDisplay.getDisplayShort(te.getEnergyHandler().getEnergy(), Unit.JOULES));
-		currenttip.add(LanguageUtility.getLocal("info.energycapacity.waila") + " " + UnitDisplay.getDisplayShort(te.getEnergyHandler().getEnergyCapacity(), Unit.JOULES));
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return null;
+    }
 
-		return currenttip;
-	}
-
-	@Override
-	public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
-	{
-		return currenttip;
-	}
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+    {
+        return currenttip;
+    }
 }
