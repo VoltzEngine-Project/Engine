d0fee84d4bb817837a07da62c4df28a471edf6a6
Maxwolf Goodliffe <devs@madsciencemod.com>
2014-05-01 04:39:30 -0700

    Added getTemperature() to IReactor interface.

--- src/main/scala/calclavia/api/atomicscience/IReactor.java
+++ src/main/scala/calclavia/api/atomicscience/IReactor.java
@@ -6,6 +6,8 @@ import net.minecraftforge.fluids.IFluidHandler;
 public interface IReactor extends IFluidHandler
 {
 	public void heat(long energy);
+	
+	public float getTemperature();
 
 	public boolean isOverToxic();
 
