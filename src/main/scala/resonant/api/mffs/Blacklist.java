826ad72886800eb8abb0de62cefc254cd31a0528
Calclavia <admin@calclavia.com>
2014-06-29 15:19:12 -0400

    Updated Blacklist to use blocks

--- src/main/scala/resonant/api/mffs/Blacklist.java
+++ src/main/scala/resonant/api/mffs/Blacklist.java
@@ -1,15 +1,17 @@
 package resonant.api.mffs;
 
+import net.minecraft.block.Block;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Blacklist
 {
-    /** Adds blocks to this black list if you do not wish them to be moved by the following:
-     * 
-     * The interger is the block ID. */
-    public static final Set<Integer> stabilizationBlacklist = new HashSet<Integer>();
-    public static final Set<Integer> disintegrationBlacklist = new HashSet<Integer>();
-    public static final Set<Integer> forceManipulationBlacklist = new HashSet<Integer>();
+	/**
+	 * Adds blocks to this black list if you do not wish them to be interacted by the following:
+	 */
+	public static final Set<Block> stabilizationBlacklist = new HashSet();
+	public static final Set<Block> disintegrationBlacklist = new HashSet();
+	public static final Set<Block> forceManipulationBlacklist = new HashSet();
 
 }
