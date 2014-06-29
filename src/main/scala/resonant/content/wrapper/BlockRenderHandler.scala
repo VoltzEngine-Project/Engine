19235463ad16fe9abbf7ac27124fbdba04e935d0
Calclavia <admin@calclavia.com>
2014-06-28 23:05:56 -0400

    Fixed BlockRenderHandler not seeking tile instance first

--- src/main/scala/resonant/content/wrapper/BlockRenderHandler.scala
+++ src/main/scala/resonant/content/wrapper/BlockRenderHandler.scala
@@ -7,9 +7,10 @@ import net.minecraft.item.ItemStack
 import net.minecraft.world.IBlockAccess
 import org.lwjgl.opengl.GL11._
 import org.lwjgl.opengl.GL12._
+import resonant.content.spatial.block.SpatialBlock
 import universalelectricity.core.transform.vector.Vector3
 
-object BlockRenderHandler  extends ISimpleBlockRenderingHandler
+object BlockRenderHandler extends ISimpleBlockRenderingHandler
 {
   val ID = RenderingRegistry.getNextAvailableRenderId()
 
@@ -31,17 +32,34 @@ object BlockRenderHandler  extends ISimpleBlockRenderingHandler
 
   def renderWorldBlock(access: IBlockAccess, x: Int, y: Int, z: Int, block: Block, modelId: Int, renderer: RenderBlocks): Boolean =
   {
-    if (block.isInstanceOf[BlockDummy])
+    var renderer: SpatialBlock = _
+
+    /**
+     * Try TileEntity rendering
+     */
+    val tile = access.getTileEntity(x, y, z)
+
+    if (tile.isInstanceOf[SpatialBlock])
+    {
+      val spatial = tile.asInstanceOf[SpatialBlock]
+      renderer = spatial.tile()
+    }
+
+    /**
+     * Try Block rendering
+     */
+    if (renderer == null && block.isInstanceOf[BlockDummy])
     {
       val dummy = block.asInstanceOf[BlockDummy]
       dummy.inject(access, x, y, z)
-      val tile = dummy.getTile(access, x, y, z)
-
-      if (tile != null)
-      {
-        return tile.renderStatic(renderer, new Vector3(x, y, z), 0);
-      }
+      renderer = dummy.getTile(access, x, y, z)
     }
+
+    if (renderer != null)
+    {
+      return renderer.renderStatic(renderer, new Vector3(x, y, z), 0);
+    }
+
     return false
   }
 
