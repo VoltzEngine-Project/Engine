8266a83faedac497387384544fb4335e38ddd48b
Calclavia <admin@calclavia.com>
2014-07-05 15:56:51 -0400

    Changed PacketTile to use VarArgs

--- src/main/scala/resonant/api/mffs/modules/IModuleAcceptor.java
+++ src/main/scala/resonant/api/mffs/modules/IModuleAcceptor.java
@@ -6,6 +6,9 @@ import net.minecraft.item.ItemStack;
 
 public interface IModuleAcceptor
 {
+	/**
+	 * Gets the ItemStack of a specific module type. This ItemStack is constructed and NOT a reference to the actual stacks within the block.
+	 */
     public ItemStack getModule(IModule module);
 
     public int getModuleCount(IModule module, int... slots);
--- src/main/scala/resonant/api/mffs/security/IBiometricIdentifier.java
+++ src/main/scala/resonant/api/mffs/security/IBiometricIdentifier.java
@@ -2,12 +2,13 @@ package resonant.api.mffs.security;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.item.ItemStack;
+import resonant.api.blocks.IBlockFrequency;
 import resonant.lib.access.Permission;
 
 /**
  * Applied to Biometric Identifiers (extends TileEntity).
  */
-public interface IBiometricIdentifier
+public interface IBiometricIdentifier extends IBlockFrequency
 {
 	/**
 	 * Is this specific permission granted to this specific user profile?
