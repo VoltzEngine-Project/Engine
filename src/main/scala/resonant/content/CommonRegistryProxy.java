bdeb4cb664919fcc995598350524fe46b2424cfb
Robert S <rseifert.phone@gmail.com>
2014-07-28 10:19:55 -0400

    Added auto detection for @Synced when using the ModManager system

--- src/main/scala/resonant/content/CommonRegistryProxy.java
+++ src/main/scala/resonant/content/CommonRegistryProxy.java
@@ -2,12 +2,33 @@ package resonant.content;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.tileentity.TileEntity;
+import resonant.lib.network.Synced;
+import resonant.lib.network.discriminator.PacketAnnotationManager;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class CommonRegistryProxy
 {
 	public void registerTileEntity(String name, Class<? extends TileEntity> clazz)
 	{
 		GameRegistry.registerTileEntityWithAlternatives(clazz, name, "CL" + name);
+        for (Field field : clazz.getDeclaredFields())
+        {
+            if(field.isAnnotationPresent(Synced.class))
+            {
+                PacketAnnotationManager.INSTANCE.register(clazz);
+                break;
+            }
+        }
+        for (Method m : clazz.getDeclaredMethods())
+        {
+            if (m.isAnnotationPresent(Synced.SyncedInput.class) || m.isAnnotationPresent(Synced.SyncedOutput.class))
+            {
+                PacketAnnotationManager.INSTANCE.register(clazz);
+                break;
+            }
+        }
 	}
 
 	public void registerDummyRenderer(Class<? extends TileEntity> clazz)
