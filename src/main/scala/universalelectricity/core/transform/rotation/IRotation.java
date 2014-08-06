9564e7c40d2f5b3b30f878d8a7e43c7cdd24730e
Robert S <rseifert.phone@gmail.com>
2014-08-06 05:30:18 -0400

    Re-added IRotation as its designed to be applied to entities

---
+++ src/main/scala/universalelectricity/core/transform/rotation/IRotation.java
@@ -0,0 +1,15 @@
+package universalelectricity.core.transform.rotation;
+
+/**
+ * Simple interface to define that an object has rotation
+ *
+ * @author DarkGuardsman
+ */
+public interface IRotation
+{
+	double yaw();
+
+	double pitch();
+
+	double roll();
+}
