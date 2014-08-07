d787b2933f36e3d4e5179f9d19a66a4aee1ccb16
Robert S <rseifert.phone@gmail.com>
2014-08-07 03:07:34 -0400

    Corrected TileEletricInventory not extends TileInventory

--- src/main/scala/resonant/content/prefab/java/TileElectricInventory.scala
+++ src/main/scala/resonant/content/prefab/java/TileElectricInventory.scala
@@ -1,12 +1,12 @@
 package resonant.lib.content.prefab.java
 
 import net.minecraft.block.material.Material
-import resonant.lib.content.prefab.TInventory
+import resonant.lib.content.prefab.TElectric
 
 /**
  * @author Calclavia
  */
-abstract class TileElectricInventory(material: Material) extends TileElectric(material : Material) with TInventory
+abstract class TileElectricInventory(material: Material) extends TileInventory(material: Material) with TElectric
 {
 
 }
