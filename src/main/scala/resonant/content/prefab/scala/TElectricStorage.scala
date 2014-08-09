b5850b989a32ad21387c3867833f14a77f603a96
Zerotheliger <zerotheliger098@gmail.com>
2014-08-08 17:33:10 -0700

    Update TElectricStorage.scala

--- src/main/scala/resonant/content/prefab/scala/TElectricStorage.scala
+++ src/main/scala/resonant/content/prefab/scala/TElectricStorage.scala
@@ -10,7 +10,7 @@ import universalelectricity.api.core.grid.electric.IEnergyContainer
  */
 trait TElectricStorage extends TElectric with IEnergyContainer
 {
-  var energy: EnergyStorage = _
+  override var energy: EnergyStorage = _
 
   /**
    * Sets the amount of energy this unit stored.
