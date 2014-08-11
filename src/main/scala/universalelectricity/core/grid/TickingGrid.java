4c241b1856b273a4b4e316110040d3e4a5f14bce
Robert S <rseifert.phone@gmail.com>
2014-08-11 19:18:11 -0400

    Turned TickingGrid to java to help clear out issues using scala from java

---
+++ src/main/scala/universalelectricity/core/grid/TickingGrid.java
@@ -0,0 +1,31 @@
+package universalelectricity.core.grid;
+
+import scala.reflect.ClassTag;
+import universalelectricity.api.core.grid.IUpdate;
+
+/**
+ * Created by robert on 8/11/2014.
+ */
+public class TickingGrid<N extends Node<N>> extends NodeGrid<N> implements IUpdate {
+
+    public TickingGrid(ClassTag tag)
+    {
+        super(tag);
+        UpdateTicker.addUpdater(this);
+    }
+
+    @Override
+    public void update(double deltaTime)
+    {
+        for(N node : this.getNodes())
+        {
+            node.update(deltaTime);
+        }
+    }
+
+    @Override
+    public boolean canUpdate() {return getNodes().size() > 0; }
+
+    @Override
+    public boolean continueUpdate() { return canUpdate();}
+}
