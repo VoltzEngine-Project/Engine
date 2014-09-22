0a77684290bc8940d9fa92fffab04b0c679890cd
Calclavia <admin@calclavia.com>
2014-09-22 22:43:42 +0800

    Minor energy node changes

--- src/main/scala/universalelectricity/core/grid/node/NodeEnergy.java
+++ src/main/scala/universalelectricity/core/grid/node/NodeEnergy.java
@@ -13,160 +13,170 @@ import java.util.Map;
 
 /**
  * Basic node designed to store energy
+ * <p/>
+ * TODO: Energy storage should later be handled by the TileEntity itself. Most blocks will be components of a circuit and do not store energy but instead consume it.
+ *
  * @author Darkguardsman
  */
 public class NodeEnergy extends NodeConnector implements IEnergyNode, IUpdate, ISave
 {
-    protected EnergyStorage buffer = null;
-    protected boolean shareEnergy = false;
+	@Deprecated
+	protected EnergyStorage buffer = null;
+	protected boolean shareEnergy = false;
 
-    public NodeEnergy(INodeProvider parent)
-    {
-        this(parent, 100);
-    }
+	public NodeEnergy(INodeProvider parent)
+	{
+		this(parent, 100);
+	}
 
-    public NodeEnergy(INodeProvider parent, double cap)
-    {
-        this(parent, cap, cap);
-    }
+	public NodeEnergy(INodeProvider parent, double cap)
+	{
+		this(parent, cap, cap);
+	}
 
-    public NodeEnergy(INodeProvider parent, double cap, double tran)
-    {
-        this(parent, cap, tran, tran);
-    }
+	public NodeEnergy(INodeProvider parent, double cap, double tran)
+	{
+		this(parent, cap, tran, tran);
+	}
 
-    public NodeEnergy(INodeProvider parent, double cap, double in, double out)
-    {
-        super(parent);
-        buffer = new EnergyStorage(cap, in, out);
-    }
+	public NodeEnergy(INodeProvider parent, double cap, double in, double out)
+	{
+		super(parent);
+		buffer = new EnergyStorage(cap, in, out);
+	}
 
+	@Override
+	public void update(double deltaTime)
+	{
+		if (canShareEnergy())
+		{
+			shareEnergy();
+		}
+	}
 
-    @Override
-    public void update(double deltaTime)
-    {
-        if(canShareEnergy())
-            shareEnergy();
-    }
+	public EnergyStorage buffer()
+	{
+		return buffer;
+	}
 
-    public EnergyStorage buffer()
-    {
-        return buffer;
-    }
+	@Override
+	public double addEnergy(ForgeDirection from, double energy, boolean doAdd)
+	{
+		if (canConnect(from))
+		{
+			return buffer.receiveEnergy(energy, doAdd);
+		}
+		return 0;
+	}
 
-    @Override
-    public double addEnergy(ForgeDirection from, double wattage, boolean doAdd)
-    {
-        if(canConnect(from.getOpposite()))
-        {
-            return buffer.receiveEnergy(wattage, doAdd);
-        }
-        return 0;
-    }
+	@Override
+	public double removeEnergy(ForgeDirection from, double energy, boolean doRemove)
+	{
+		if (canConnect(from) && canShareEnergy())
+		{
+			return buffer.extractEnergy(energy, doRemove);
+		}
+		return 0;
+	}
 
-    @Override
-    public double removeEnergy(ForgeDirection from, double wattage, boolean doRemove)
-    {
-        if(canConnect(from.getOpposite()) && canShareEnergy())
-        {
-            return buffer.extractEnergy(wattage, doRemove);
-        }
-        return 0;
-    }
+	@Override
+	public double getEnergy(ForgeDirection from)
+	{
+		if (canConnect(from.getOpposite()))
+		{
+			return buffer.getEnergy();
+		}
+		return -1;
+	}
 
-    @Override
-    public double getEnergy(ForgeDirection from)
-    {
-        if(canConnect(from.getOpposite()))
-        {
-            return buffer.getEnergy();
-        }
-        return -1;
-    }
+	@Override
+	public double getEnergyCapacity(ForgeDirection from)
+	{
+		if (canConnect(from.getOpposite()))
+		{
+			return buffer.getEnergyCapacity();
+		}
+		return -1;
+	}
 
-    @Override
-    public double getEnergyCapacity(ForgeDirection from)
-    {
-        if(canConnect(from.getOpposite()))
-        {
-            return buffer.getEnergyCapacity();
-        }
-        return -1;
-    }
+	/**
+	 * Can this node share energy
+	 */
+	public boolean canShareEnergy()
+	{
+		return shareEnergy;
+	}
 
-    /** Can this node share energy */
-    public boolean canShareEnergy()
-    {
-        return shareEnergy;
-    }
+	/**
+	 * Sets the node to share energy with connected nodes
+	 *
+	 * @param share - true will share energy
+	 */
+	public void setCanShareEnergy(boolean share)
+	{
+		this.shareEnergy = share;
+	}
 
-    /**
-     * Sets the node to share energy with connected nodes
-     * @param share - true will share energy
-     */
-    public void setCanShareEnergy(boolean share)
-    {
-        this.shareEnergy = share;
-    }
+	/**
+	 * called to share energy with all connected nodes
+	 */
+	public void shareEnergy()
+	{
+		if (getEnergy(ForgeDirection.UNKNOWN) > 0)
+		{
+			int handlers = connections.size();
+			for (Map.Entry<Object, ForgeDirection> entry : connections.entrySet())
+			{
+				double energyToGive = Math.min(buffer.maxExtract(), (getEnergy(entry.getValue()) / handlers) + (getEnergy(ForgeDirection.UNKNOWN) % handlers));
+				if (Compatibility.isHandler(entry.getKey(), entry.getValue()))
+				{
+					if (Compatibility.canConnect(entry.getKey(), entry.getValue(), this))
+					{
+						buffer.extractEnergy(Compatibility.fill(entry.getKey(), entry.getValue(), buffer.extractEnergy(energyToGive, false), true), true);
+					}
+				}
+				handlers--;
+			}
+		}
+	}
 
-    /** called to share energy with all connected nodes */
-    public void shareEnergy()
-    {
-        if(getEnergy(ForgeDirection.UNKNOWN) > 0)
-        {
-            int handlers = connections.size();
-            for (Map.Entry<Object, ForgeDirection> entry : connections.entrySet())
-            {
-                double energyToGive = Math.min(buffer.maxExtract(), (getEnergy(entry.getValue()) / handlers) + (getEnergy(ForgeDirection.UNKNOWN) % handlers));
-                if (Compatibility.isHandler(entry.getKey(), entry.getValue()))
-                {
-                    if (Compatibility.canConnect(entry.getKey(), entry.getValue(), this))
-                    {
-                        	buffer.extractEnergy(Compatibility.fill(entry.getKey(), entry.getValue(), buffer.extractEnergy(energyToGive, false), true), true);
-                    }
-                }
-                handlers--;
-            }
-        }
-    }
+	@Override
+	public boolean canConnect(ForgeDirection direction, Object object)
+	{
+		if (object != null)
+		{
+			return canConnect(direction) && Compatibility.isHandler(object, direction) && Compatibility.canConnect(object, direction, this);
+		}
+		return false;
+	}
 
-    @Override
-    public boolean canConnect(ForgeDirection direction, Object object)
-    {
-        if(object != null)
-        {
-            return canConnect(direction) && Compatibility.isHandler(object, direction) && Compatibility.canConnect(object, direction, this);
-        }
-        return false;
-    }
+	@Override
+	public boolean canUpdate()
+	{
+		return canShareEnergy() && getEnergy(ForgeDirection.UNKNOWN) > 0;
+	}
 
-    @Override
-    public boolean canUpdate()
-    {
-        return canShareEnergy() && getEnergy(ForgeDirection.UNKNOWN) > 0;
-    }
+	@Override
+	public boolean continueUpdate()
+	{
+		return true;
+	}
 
-    @Override
-    public boolean continueUpdate()
-    {
-        return true;
-    }
+	@Override
+	public void load(NBTTagCompound nbt)
+	{
+		if (nbt.hasKey("energy"))
+		{
+			NBTTagCompound energy = nbt.getCompoundTag("energy");
+			buffer.readFromNBT(energy);
+		}
+	}
 
-    @Override
-    public void load(NBTTagCompound nbt)
-    {
-        if(nbt.hasKey("energy"))
-        {
-            NBTTagCompound energy = nbt.getCompoundTag("energy");
-            buffer.readFromNBT(energy);
-        }
-    }
-
-    @Override
-    public void save(NBTTagCompound nbt)
-    {
-        NBTTagCompound energy = new NBTTagCompound();
-        buffer.writeToNBT(energy);
-        nbt.setTag("energy", energy);
-    }
+	@Override
+	public void save(NBTTagCompound nbt)
+	{
+		NBTTagCompound energy = new NBTTagCompound();
+		buffer.writeToNBT(energy);
+		nbt.setTag("energy", energy);
+	}
 }
