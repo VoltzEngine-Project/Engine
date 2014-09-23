021d66060f0c4163c877520559ce013fdb220023
Robert S <rseifert.phone@gmail.com>
2014-09-23 09:25:17 -0400

    Abstracted RI's turbine world gen and added it to RE

---
+++ src/main/scala/resonant/lib/schematic/SchematicPlate.scala
@@ -0,0 +1,42 @@
+package resonant.lib.schematic
+
+import java.util.HashMap
+
+import net.minecraft.block.Block
+import net.minecraftforge.common.util.ForgeDirection
+import resonant.lib.`type`.Pair
+import universalelectricity.core.transform.vector.Vector3
+
+/**
+ * Creates a flat box shaped world gen
+ *
+ * @param name - unlocalized name for translation
+ * @param block - block to build with
+ */
+class SchematicPlate(name : String, block : Block) extends Schematic {
+
+  def getName: String =
+  {
+    return name
+  }
+
+  def getStructure(dir: ForgeDirection, size: Int): HashMap[Vector3, Pair[Block, Integer]] =
+  {
+    val returnMap: HashMap[Vector3, Pair[Block, Integer]] = new HashMap[Vector3, Pair[Block, Integer]]
+
+    for (x : Int  <- size to -size)
+    {
+      for (y : Int  <- size to -size)
+      {
+        for (z : Int <- size to -size)
+        {
+          if ((dir.offsetX != 0 && x == 0) || (dir.offsetY != 0 && y == 0) || (dir.offsetZ != 0 && z == 0))
+          {
+            returnMap.put(new Vector3(x, y, z), new Pair[Block, Integer](block, dir.ordinal))
+          }
+        }
+      }
+    }
+    return returnMap
+  }
+}
