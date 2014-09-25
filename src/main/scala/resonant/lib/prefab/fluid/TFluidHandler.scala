01f77c870df7936bffb353e2eaf2d77e1c963f7d
Robert S <rseifert.phone@gmail.com>
2014-09-25 19:31:25 -0400

    Added Trait for IFluidTank & improvied TFluidHandler which both implement TTankProvider

--- src/main/scala/resonant/lib/prefab/fluid/TFluidHandler.scala
+++ src/main/scala/resonant/lib/prefab/fluid/TFluidHandler.scala
@@ -7,26 +7,46 @@ import net.minecraftforge.fluids._
  * Prefab Trait for IFluidHandler
  * @author Calclavia
  */
-trait TFluidHandler extends IFluidHandler
+trait TFluidHandler extends IFluidHandler with TTankProvider
 {
-  var fluidTank: FluidTank = new FluidTank(FluidContainerRegistry.BUCKET_VOLUME)
-
-  override def fill(from: ForgeDirection, resource: FluidStack, doFill: Boolean): Int = fluidTank.fill(resource, doFill)
+  override def fill(from: ForgeDirection, resource: FluidStack, doFill: Boolean): Int =
+  {
+    if (getPrimaryTank != null)
+      return getPrimaryTank.fill(resource, doFill)
+    else
+      return 0
+  }
 
   override def drain(from: ForgeDirection, resource: FluidStack, doDrain: Boolean): FluidStack =
   {
-    if (resource != null && resource.isFluidEqual(fluidTank.getFluid))
+    if (getPrimaryTank != null && resource != null && resource.isFluidEqual(getPrimaryTank.getFluid))
     {
       return drain(from, resource.amount, doDrain)
     }
     return null
   }
 
-  override def drain(from: ForgeDirection, maxDrain: Int, doDrain: Boolean): FluidStack = fluidTank.drain(maxDrain, doDrain)
+  override def drain(from: ForgeDirection, maxDrain: Int, doDrain: Boolean): FluidStack =
+  {
+    if (getPrimaryTank != null)
+      return getPrimaryTank.drain(maxDrain, doDrain)
+    else
+      return null
+  }
 
   override def canFill(from: ForgeDirection, fluid: Fluid): Boolean = true
 
   override def canDrain(from: ForgeDirection, fluid: Fluid): Boolean = true
 
-  override def getTankInfo(from: ForgeDirection): Array[FluidTankInfo] = Array[FluidTankInfo](fluidTank.getInfo)
+  override def getTankInfo(from: ForgeDirection): Array[FluidTankInfo] =
+  {
+    if (getPrimaryTank != null)
+    {
+      return Array[FluidTankInfo](getPrimaryTank.getInfo)
+    }
+    else
+    {
+      return Array[FluidTankInfo]()
+    }
+  }
 }
