8bf91b41e0248425cf6543150501c686266d364e
Robert S <rseifert.phone@gmail.com>
2014-10-09 22:39:00 -0400

    Added a way to disable anything extending CustomPotion.class

--- src/main/scala/resonant/lib/prefab/potion/CustomPotion.java
+++ src/main/scala/resonant/lib/prefab/potion/CustomPotion.java
@@ -1,44 +1,109 @@
 package resonant.lib.prefab.potion;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import resonant.engine.References;
 
 public abstract class CustomPotion extends Potion
 {
-	/**
-	 * Creates a new type of potion
-	 *
-	 * @param id          - The ID of this potion. Make it greater than 20.
-	 * @param isBadEffect - Is this potion a good potion or a bad one?
-	 * @param color       - The color of this potion.
-	 * @param name        - The name of this potion.
-	 */
-	public CustomPotion(int id, boolean isBadEffect, int color, String name)
-	{
-		super(getPotionID(name, id), isBadEffect, color);
-		this.setPotionName("potion." + name);
-		Potion.potionTypes[this.getId()] = this;
-	}
+    public boolean enable = true;
+    public boolean disableCreative = true;
+    public boolean disablePeaceful = true;
 
-	public static int getPotionID(String name, int id)
-	{
-		References.CONFIGURATION.load();
-		int finalID = References.CONFIGURATION.get("Potion ID", name + " ID", id).getInt(id);
-		References.CONFIGURATION.save();
-		return finalID;
-	}
+    /**
+     * Creates a new type of potion
+     *
+     * @param id          - The ID of this potion. Make it greater than 20.
+     * @param isBadEffect - Is this potion a good potion or a bad one?
+     * @param color       - The color of this potion.
+     * @param name        - The name of this potion.
+     */
+    public CustomPotion(int id, boolean isBadEffect, int color, String name)
+    {
+        super(getPotionID(name, id), isBadEffect, color);
+        this.setPotionName("potion." + name);
+        Potion.potionTypes[this.getId()] = this;
+    }
 
-	@Override
-	public Potion setIconIndex(int par1, int par2)
-	{
-		super.setIconIndex(par1, par2);
-		return this;
-	}
+    public static int getPotionID(String name, int id)
+    {
+        References.CONFIGURATION.load();
+        int finalID = References.CONFIGURATION.get("Potion ID", name + " ID", id).getInt(id);
+        References.CONFIGURATION.save();
+        return finalID;
+    }
 
-	@Override
-	protected Potion setEffectiveness(double par1)
-	{
-		super.setEffectiveness(par1);
-		return this;
-	}
+    @Override
+    public void performEffect(EntityLivingBase ent, int amplifier)
+    {
+       if(shouldRemoveEffect(ent, amplifier))
+       {
+           removeEffect(ent);
+           return;
+       }
+    }
+
+    /**
+     * Called to check if the effect should be remove before its timer runs out
+     * @param ent - entity with the effect
+     * @param amplifier - level of the effect
+     * @return true to remove
+     */
+    public boolean shouldRemoveEffect(EntityLivingBase ent, int amplifier)
+    {
+        // if disabled, dead, or has no hp left(basically dead)
+        if (!enable || ent.isDead || ent.getHealth() <= 0)
+        {
+            return true;
+        }
+
+        //Remove any potion effect off of entities by default to avoid issues
+        if (disableCreative && ent instanceof EntityPlayer && ((EntityPlayer) ent).capabilities.isCreativeMode)
+        {
+            return true;
+        }
+
+        //Disable mal effects with peaceful on
+        if (disablePeaceful && ent.worldObj.difficultySetting.getDifficultyId() == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Removes the effect of the entity
+     *
+     * @param entity - entity that may have the effect
+     */
+    public void removeEffect(EntityLivingBase entity)
+    {
+        if (entity.isPotionActive(getId()))
+        {
+            entity.removePotionEffect(getId());
+        }
+    }
+
+
+    @Override
+    public CustomPotion setIconIndex(int par1, int par2)
+    {
+        super.setIconIndex(par1, par2);
+        return this;
+    }
+
+    @Override
+    protected CustomPotion setEffectiveness(double par1)
+    {
+        super.setEffectiveness(par1);
+        return this;
+    }
+
+    @Override
+    public CustomPotion setPotionName(String name)
+    {
+        super.setPotionName(name);
+        return this;
+    }
 }
