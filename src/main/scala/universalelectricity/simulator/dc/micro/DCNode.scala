b93f782dfcb78a90fadac8853ca36977aea62759
Calclavia <admin@calclavia.com>
2014-10-12 17:02:34 +0800

    Some work on circuit charge pushing

--- src/main/scala/universalelectricity/simulator/dc/micro/DCNode.scala
+++ src/main/scala/universalelectricity/simulator/dc/micro/DCNode.scala
@@ -1,5 +1,6 @@
 package universalelectricity.simulator.dc.micro
 
+import net.minecraftforge.common.util.ForgeDirection
 import universalelectricity.api.core.grid.INodeProvider
 import universalelectricity.core.grid.node.NodeConnector
 
@@ -16,7 +17,11 @@ import scala.collection.convert.wrapAll._
 class DCNode(parent: INodeProvider) extends NodeConnector(parent)
 {
   private var _current = 0D
+
+  private var prevCharge = 0D
   private var _charge = 0D
+  var chargeCapacity = 10000
+
   private var _resistance = Double.MinPositiveValue
 
   private var chargeBuffer = 0D
@@ -28,6 +33,10 @@ class DCNode(parent: INodeProvider) extends NodeConnector(parent)
   def voltage = current * resistance
 
   def charge = _charge
+
+  def charge(terminal: ForgeDirection) = charge
+
+  def charge_=(charge: Double) = _charge = Math.min(_charge + charge, chargeCapacity)
 
   //Gets the resistance of this component
   def resistance = _resistance
@@ -41,41 +50,57 @@ class DCNode(parent: INodeProvider) extends NodeConnector(parent)
 
   def update(deltaTime: Float)
   {
-    val chargeBefore = _charge
+    //Calculate current based on the change of charges over time
+    _current = Math.abs(charge - prevCharge) / deltaTime
 
-    _charge = Math.max(charge, chargeBuffer)
-    //TODO: Should we set this to zero?
-    chargeBuffer = 0
+    prevCharge = charge
+
+    if (chargeBuffer > 0)
+    {
+      push(chargeBuffer)
+      //TODO: Should we set this to zero?
+      chargeBuffer = 0
+    }
+  }
+
+  /**
+   * Pushes charges in this DC Component
+   * @param chargeAmount
+   */
+  private def push(chargeAmount: Double, exclude: DCNode*)
+  {
+    val excluded = exclude :+ this
+
+    val allComponents = connections.keys.map(_.asInstanceOf[DCNode]).toSet
+    //We only want to consider components that have less charge.
+    val components = allComponents.filter(c => charge > c.charge && !excluded.contains(c))
 
     /**
      * Push charge for every component like a "snake".
      * Charge always wants to flow to places with the least resistance.
-     * Charge always wants to flow to places with less charge. Electrons repell each other.
+     * Charge always wants to flow to places with less charge. Electrons repel each other.
      */
-    val components = connections.keys.map(_.asInstanceOf[DCNode])
+    val totalDeltaCharge = components.map(c => charge - c.charge(connections.get(c))).foldLeft(0D)(_ + _)
+    val totalResistance = components.map(_.resistance).foldLeft(0D)(_ + _)
 
-    //We only want to push to components that have less charge.
-    val comp = components.filter(_charge > _._charge)
-
-    val totalDeltaCharge = comp.map(_charge - _._charge).reduceLeft(_ + _)
-    val totalResistance = comp.map(_.resistance).reduceLeft(_ + _)
-
-    comp.foreach(c =>
+    components.foreach(c =>
     {
-      val chargePerct = (_charge - c._charge) / totalDeltaCharge
+      val diff = charge - c.charge(connections.get(c))
+      val chargePerct = diff / totalDeltaCharge
       val resisPerct = c.resistance / totalResistance
-      val transfer = _charge * (chargePerct + (1 - resisPerct)) / 2
 
-      c._charge += transfer
-      _charge -= transfer
+      //TODO: Consider resistance
+      val transfer = Math.min(charge * chargePerct /*(chargePerct + (1 - resisPerct)) / 2*/ , diff)
+
+      c.push(charge, excluded: _*)
+      charge -= transfer
     })
 
-    //Calculate current based on the change of charges over time
-    _current = Math.abs(chargeBefore - _charge) / deltaTime
+    charge += chargeAmount
   }
 
   /**
-   * Charges this DC Component
+   * Pushes charges in this DC Component
    * @param charge The amount of charge in coulombs
    */
   def charge(charge: Double)
@@ -83,5 +108,5 @@ class DCNode(parent: INodeProvider) extends NodeConnector(parent)
     chargeBuffer += charge
   }
 
-  override def toString = "DCNode [" + charge + "C " + current + "A " + voltage + "V]"
+  override def toString = "DCNode [" + charge.toInt + "C " + current.toInt + "A " + voltage.toInt + "V]"
 }
