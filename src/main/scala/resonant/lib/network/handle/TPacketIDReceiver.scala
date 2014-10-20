afe8a4013dde176f1c838eef0f0a5d7ae587deed
Robert S <rseifert.phone@gmail.com>
2014-10-20 13:11:42 -0400

    Changed TPacektIDReceiver to extend TPacketReceiver allowing both to share the same helper methods

--- src/main/scala/resonant/lib/network/handle/TPacketIDReceiver.scala
+++ src/main/scala/resonant/lib/network/handle/TPacketIDReceiver.scala
@@ -3,80 +3,37 @@ package resonant.lib.network.handle
 import cpw.mods.fml.common.network.ByteBufUtils
 import io.netty.buffer.ByteBuf
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.tileentity.TileEntity
-import resonant.engine.ResonantEngine
-import resonant.lib.network.discriminator.{PacketTile, PacketType}
-import resonant.lib.network.netty.AbstractPacket
+import resonant.lib.network.discriminator.PacketType
 import universalelectricity.api.core.grid.ISave
-import universalelectricity.core.transform.vector.{IVectorWorld, TVectorWorld}
 
-/** Trait to be applied to scala classes to implement basic packet handling.
-  * Handles desc packet by default using packet ID 0
-  *
-  * Created by robert(Darkguardsman) on 9/25/2014.
-  */
-trait TPacketIDReceiver extends IPacketIDReceiver with TVectorWorld
+/**
+ * Mixin prefab designed to implement basic packet handling & helper methods
+ *
+ * Handles NBTTagCompound packets for methods TileEntity.readFromNBT and ISave.load
+ * with packet ID 0.
+ *
+ * Created on 8/6/2014.
+ * @author robert(Darkguardsman)
+ */
+trait TPacketIDReceiver extends TPacketReceiver with IPacketIDReceiver
 {
-  override def read(buf: ByteBuf, id: Int, player: EntityPlayer, `type`: PacketType): Boolean =
-  {
-    if(id == 0)
+    override def read(buf: ByteBuf, id: Int, player: EntityPlayer, `type`: PacketType): Boolean =
     {
-      if(this.isInstanceOf[TileEntity])
-      {
-        this.asInstanceOf[TileEntity].readFromNBT(ByteBufUtils.readTag(buf))
-        return true
-      }
-      else if(this.isInstanceOf[ISave])
-      {
-        this.asInstanceOf[ISave].load(ByteBufUtils.readTag(buf))
-        return true
-      }
+        if (id == 0)
+        {
+            if (this.isInstanceOf[TileEntity])
+            {
+                this.asInstanceOf[TileEntity].readFromNBT(ByteBufUtils.readTag(buf))
+                return true
+            }
+            else if (this.isInstanceOf[ISave])
+            {
+                this.asInstanceOf[ISave].load(ByteBufUtils.readTag(buf))
+                return true
+            }
+        }
+        return false
     }
-    return false
-  }
-
-
-  /** Gets the description packet */
-  def getDescPacket : AbstractPacket =
-  {
-    var nbt : NBTTagCompound = null
-
-    if(this.isInstanceOf[TileEntity])
-    {
-      nbt = new NBTTagCompound
-      this.asInstanceOf[TileEntity].writeToNBT(nbt)
-      return new PacketTile(x.asInstanceOf[Int], y.asInstanceOf[Int], z.asInstanceOf[Int], Array[Any](0, nbt))
-    }
-    else if(this.isInstanceOf[ISave])
-    {
-      nbt = new NBTTagCompound
-      this.asInstanceOf[ISave].save(nbt)
-      return new PacketTile(x.asInstanceOf[Int], y.asInstanceOf[Int], z.asInstanceOf[Int],  Array[Any](0, nbt))
-    }
-    return null
-  }
-
-  /** Sends the desc packet to all players around this tile */
-  def sendDescPacket
-  {
-    sendPacket(getDescPacket)
-  }
-
-  /** Sends the packet to all players around this tile
-    * @param packet - packet to send */
-  def sendPacket(packet: AbstractPacket)
-  {
-    sendPacket(packet, 64)
-  }
-
-  /** Sends the packet to all players around this tile
-    * @param packet - packet to send
-    * @param distance - distance in blocks to search for players
-    */
-  def sendPacket(packet: AbstractPacket, distance : Double)
-  {
-    ResonantEngine.instance.packetHandler.sendToAllAround(packet, this.asInstanceOf[IVectorWorld], distance)
-  }
 
 }
--- src/main/scala/resonant/lib/network/handle/TPacketReceiver.scala
+++ src/main/scala/resonant/lib/network/handle/TPacketReceiver.scala
@@ -1,13 +1,76 @@
 package resonant.lib.network.handle
 
+import cpw.mods.fml.common.network.ByteBufUtils
 import io.netty.buffer.ByteBuf
 import net.minecraft.entity.player.EntityPlayer
-import resonant.lib.network.discriminator.PacketType
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.tileentity.TileEntity
+import resonant.engine.ResonantEngine
+import resonant.lib.network.discriminator.{PacketTile, PacketType}
+import resonant.lib.network.netty.AbstractPacket
+import universalelectricity.api.core.grid.ISave
+import universalelectricity.core.transform.vector.{IVectorWorld, TVectorWorld}
 
 /**
- * Created by robert on 8/6/2014.
+ * Mixin prefab designed to implement basic packet handling & helper methods
+ * Created on 8/6/2014.
+ * @author robert(Darkguardsman)
  */
-trait TPacketReceiver extends IPacketReceiver
+trait TPacketReceiver extends IPacketReceiver with TVectorWorld
 {
-  override def read(buf: ByteBuf, player: EntityPlayer, packet: PacketType)
+    override def read(buf: ByteBuf, player: EntityPlayer, packet: PacketType)
+    {
+        if(this.isInstanceOf[TileEntity])
+        {
+            this.asInstanceOf[TileEntity].readFromNBT(ByteBufUtils.readTag(buf))
+            return true
+        }
+        else if(this.isInstanceOf[ISave])
+        {
+            this.asInstanceOf[ISave].load(ByteBufUtils.readTag(buf))
+            return true
+        }
+    }
+
+    /** Gets the description packet */
+    def getDescPacket : AbstractPacket =
+    {
+        var nbt : NBTTagCompound = null
+
+        if(this.isInstanceOf[TileEntity])
+        {
+            nbt = new NBTTagCompound
+            this.asInstanceOf[TileEntity].writeToNBT(nbt)
+            return new PacketTile(x.asInstanceOf[Int], y.asInstanceOf[Int], z.asInstanceOf[Int], Array[Any](0, nbt))
+        }
+        else if(this.isInstanceOf[ISave])
+        {
+            nbt = new NBTTagCompound
+            this.asInstanceOf[ISave].save(nbt)
+            return new PacketTile(x.asInstanceOf[Int], y.asInstanceOf[Int], z.asInstanceOf[Int],  Array[Any](0, nbt))
+        }
+        return null
+    }
+
+    /** Sends the desc packet to all players around this tile */
+    def sendDescPacket
+    {
+        sendPacket(getDescPacket)
+    }
+
+    /** Sends the packet to all players around this tile
+      * @param packet - packet to send */
+    def sendPacket(packet: AbstractPacket)
+    {
+        sendPacket(packet, 64)
+    }
+
+    /** Sends the packet to all players around this tile
+      * @param packet - packet to send
+      * @param distance - distance in blocks to search for players
+      */
+    def sendPacket(packet: AbstractPacket, distance : Double)
+    {
+        ResonantEngine.instance.packetHandler.sendToAllAround(packet, this.asInstanceOf[IVectorWorld], distance)
+    }
 }
