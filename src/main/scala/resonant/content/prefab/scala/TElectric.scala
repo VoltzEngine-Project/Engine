9313b81360f6df237daae5458a2efdeabbca44a6
Calclavia <admin@calclavia.com>
2014-10-25 21:47:26 +0800

    Deprecated some DCNode methods

--- src/main/scala/resonant/content/prefab/scala/TElectric.scala
+++ src/main/scala/resonant/content/prefab/scala/TElectric.scala
@@ -1,5 +1,6 @@
 package resonant.lib.content.prefab
 
+import net.minecraft.block.Block
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraftforge.common.util.ForgeDirection
@@ -19,17 +20,37 @@ trait TElectric extends TIO with INodeProvider with ISaveObj
 
   protected var dcNode = new DCNode(this)
 
+  override def onWorldJoin()
+  {
+    dcNode.reconstruct()
+  }
+
+  override def onNeighborChanged(block: Block)
+  {
+    dcNode.reconstruct()
+  }
+
+  override def onWorldSeparate()
+  {
+    dcNode.deconstruct()
+  }
+
+  @deprecated
   def setCapacity(value: Double)
   { energy.setCapacity(value) }
 
+  @deprecated
   def energy: EnergyStorage = electricNode.buffer
 
+  @deprecated
   def setMaxTransfer(maxTransfer: Double)
   { energy.setMaxTransfer(maxTransfer) }
 
+  @deprecated
   def setMaxReceive(maxReceive: Double)
   { energy.setMaxReceive(maxReceive) }
 
+  @deprecated
   def setMaxExtract(maxExtract: Double)
   { energy.setMaxExtract(maxExtract) }
 
@@ -42,6 +63,9 @@ trait TElectric extends TIO with INodeProvider with ISaveObj
   {
     if (classOf[IElectricNode].isAssignableFrom(nodeType))
       return electricNode
+
+    if (classOf[DCNode].isAssignableFrom(nodeType))
+      return dcNode
 
     return null
   }
