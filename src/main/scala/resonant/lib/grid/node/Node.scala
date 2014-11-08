5bf9f83cb83b7a782df603f7edaea0e8eddb4aaa
Robert S <rseifert.phone@gmail.com>
2014-11-08 04:33:17 -0500

    Implement TVectorWorld in place of IVectorWorld to reduce code usage

--- src/main/scala/resonant/lib/grid/node/Node.scala
+++ src/main/scala/resonant/lib/grid/node/Node.scala
@@ -1,59 +1,27 @@
 package resonant.lib.grid.node
 
-import net.minecraft.tileentity.TileEntity
-import net.minecraft.world.World
 import resonant.api.grid.{INode, INodeProvider}
-import resonant.lib.transform.vector.{IVectorWorld, VectorWorld}
+import resonant.lib.transform.vector.{TVectorWorld, VectorWorld}
 
 /**
  * A node is any single part of a grid.
  * @author Calclavia
  */
-abstract class Node(var parent: INodeProvider) extends INode with IVectorWorld
+abstract class Node(var parent: INodeProvider) extends INode with TVectorWorld
 {
-  def reconstruct()
+  override def reconstruct()
   {
   }
 
-  def deconstruct()
+  override def deconstruct()
   {
   }
 
-  def getParent: INodeProvider = parent
+  override def getParent: INodeProvider = parent
 
+  /** @deprecated use asVectorWorld instead */
+  @Deprecated
   def position: VectorWorld = new VectorWorld(this)
-
-  def world: World =
-  {
-    if (getParent.isInstanceOf[TileEntity])
-      return (getParent.asInstanceOf[TileEntity]).getWorldObj
-
-    return null
-  }
-
-  def z: Double =
-  {
-    if (getParent.isInstanceOf[TileEntity])
-      return (getParent.asInstanceOf[TileEntity]).zCoord
-
-    return 0
-  }
-
-  def x: Double =
-  {
-    if (getParent.isInstanceOf[TileEntity])
-      return (getParent.asInstanceOf[TileEntity]).xCoord
-
-    return 0
-  }
-
-  def y: Double =
-  {
-    if (getParent.isInstanceOf[TileEntity])
-      return (getParent.asInstanceOf[TileEntity]).yCoord
-
-    return 0
-  }
 
   override def toString: String = getClass.getSimpleName + "[" + hashCode + "]"
 }
