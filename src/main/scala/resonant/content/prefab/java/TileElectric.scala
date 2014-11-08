a04f24251ba14da54d5ed60a3ab1b40be8a24c42
Robert S <rseifert.phone@gmail.com>
2014-11-08 04:49:24 -0500

    Implemented TEnergyBuffer on TileElectric

--- src/main/scala/resonant/content/prefab/java/TileElectric.scala
+++ src/main/scala/resonant/content/prefab/java/TileElectric.scala
@@ -1,9 +1,12 @@
 package resonant.lib.content.prefab.java
 
 import net.minecraft.block.material.Material
+import resonant.api.electric.EnergyStorage
 import resonant.content.prefab.java.TileAdvanced
 import resonant.lib.content.prefab.TElectric
+import resonant.lib.prefab.TEnergyBuffer
 
-class TileElectric(material: Material) extends TileAdvanced(material: Material) with TElectric
+class TileElectric(material: Material) extends TileAdvanced(material: Material) with TElectric with TEnergyBuffer
 {
+  override def getEnergyStorage(): EnergyStorage = dcNode.getEnergyStorge()
 }
