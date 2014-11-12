eca06749af75c66ba6a8677ce7e18adbc0b4b555
Robert S <rseifert.phone@gmail.com>
2014-11-12 08:35:48 -0500

    Applied trait energy storage to tile electric inventory

--- src/main/scala/resonant/content/prefab/java/TileElectricInventory.scala
+++ src/main/scala/resonant/content/prefab/java/TileElectricInventory.scala
@@ -1,12 +1,14 @@
 package resonant.lib.content.prefab.java
 
 import net.minecraft.block.material.Material
+import resonant.api.electric.EnergyStorage
 import resonant.lib.content.prefab.TElectric
+import resonant.lib.prefab.TEnergyBuffer
 
 /**
  * @author Calclavia
  */
-abstract class TileElectricInventory(material: Material) extends TileInventory(material: Material) with TElectric
+abstract class TileElectricInventory(material: Material) extends TileInventory(material: Material) with TElectric with TEnergyBuffer
 {
-
+  override def getEnergyStorage() : EnergyStorage = dcNode.getEnergyStorage()
 }
