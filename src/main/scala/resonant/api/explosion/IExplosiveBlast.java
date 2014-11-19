5854b214f7f810802bab37b56fc682fa86cdfc0b
Robert S <rseifert.phone@gmail.com>
2014-11-18 21:58:32 -0500

    Changed IExplosiveBase methods to not include location data

--- src/main/scala/resonant/api/explosion/IExplosiveBlast.java
+++ src/main/scala/resonant/api/explosion/IExplosiveBlast.java
@@ -6,7 +6,8 @@ import resonant.lib.transform.vector.Vector3;
 import java.util.Collection;
 import java.util.List;
 
-/** Use to do the actions caused by the IExplosive instance.
+/** Used to do the actions caused by the IExplosive instance. Make sure to construct the
+ * blast with the location info needed.
  * If your blast is threaded avoid using synchronized as this could lock up the thread.
  *
  * Created on 11/18/2014.
@@ -16,35 +17,26 @@ public interface IExplosiveBlast
 {
     /**
      * Should the result of the blast be threaded to reduce impact on server preformance
-     * @param world
-     * @param x
-     * @param y
-     * @param z
      * @param trigger
      * @return true if it should be threaded, false if the explosion doesn't effect blocks or is very small
      */
-    public boolean shouldThreadExplosion(World world, double x, double y, double z, Trigger trigger);
+    public boolean shouldThreadExplosion(Trigger trigger);
 
     /**
      * Gets all blocks effected by the blast, during this call never edit anything in the world. Instead
      * just do basic logic on if the block should be removed, or effected. If the block is effected add it
      * to a list then return the list. This way if the blast is threaded, or a test run the locations can
      * be feed back into the doEffect method.
-     * @param world  - world to do actions in
-     * @param x
-     * @param y
-     * @param z
      * @param trigger - cause of the blast
      * @return
      */
-    public Collection<Vector3> getEffectedBlocks(World world, double x, double y, double z, Trigger trigger);
+    public Collection<Vector3> getEffectedBlocks(Trigger trigger);
 
     /** Called to actually effect blocks from the list return by getEffectedBlocks.
-     * @param world - world to do actions in
      * @param blocks - list of blocks to edit
      * @param trigger - cause of the blast
      */
-    public void doEffectBlocks(World world, Collection<Vector3> blocks, Trigger trigger);
+    public void doEffectBlocks(Collection<Vector3> blocks, Trigger trigger);
 
     /**
      * Called to effect other things than blocks like entities. This is normally called right after
