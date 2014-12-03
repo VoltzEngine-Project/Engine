b71ff8e31e7f214385723476230aa12bb16c8e1f
Calclavia <admin@calclavia.com>
2014-12-03 15:02:52 +0800

    Added implicit wrapper between ForgeDirection and EnumFacing

--- src/main/scala/resonant/lib/wrapper/ForgeDirectionWrapper.scala
+++ src/main/scala/resonant/lib/wrapper/ForgeDirectionWrapper.scala
@@ -1,5 +1,6 @@
 package resonant.lib.wrapper
 
+import net.minecraft.util.EnumFacing
 import net.minecraftforge.common.util.ForgeDirection
 import resonant.lib.transform.vector.Vector3
 
@@ -15,4 +16,7 @@ object ForgeDirectionWrapper
     def offset: Vector3 = new Vector3(underlying.offsetX, underlying.offsetY, underlying.offsetZ)
   }
 
+  implicit def ForgeDirectionEnumFacingWrapper(enumFacing : EnumFacing) : ForgeDirection= ForgeDirection.getOrientation(enumFacing.ordinal)
+
+  implicit def EnumFacingForgeDirectionWrapper(forgeDir : ForgeDirection) : EnumFacing = EnumFacing.getFront(forgeDir.ordinal)
 }
