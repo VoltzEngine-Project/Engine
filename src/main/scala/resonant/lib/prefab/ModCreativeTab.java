2b426a289fb70bf489f6d26c39e13ccdb4dabbb0
Robert S <rseifert.phone@gmail.com>
2014-12-05 00:10:02 -0500

    Added sorting options to creative tab prefab

--- src/main/scala/resonant/lib/prefab/ModCreativeTab.java
+++ src/main/scala/resonant/lib/prefab/ModCreativeTab.java
@@ -6,6 +6,10 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Prefab creative tab to either create a fast creative tab or reduce code
  * need to make a more complex tab
@@ -13,7 +17,8 @@ import net.minecraft.item.ItemStack;
  */
 public class ModCreativeTab extends CreativeTabs
 {
-    public ItemStack itemStack;
+    public ItemStack itemStack = null;
+    public Comparator itemSorter = null;
 
     public ModCreativeTab(String name)
     {
@@ -39,6 +44,17 @@ public class ModCreativeTab extends CreativeTabs
     }
 
     @Override
+    public void displayAllReleventItems(List list)
+    {
+        super.displayAllReleventItems(list);
+
+        if(itemSorter != null && !list.isEmpty())
+        {
+            Collections.sort(list, itemSorter);
+        }
+    }
+
+    @Override
     public ItemStack getIconItemStack()
     {
         if (itemStack == null)
@@ -56,5 +72,34 @@ public class ModCreativeTab extends CreativeTabs
             itemStack = new ItemStack(Items.iron_door);
         }
         return itemStack.getItem();
+    }
+
+    public static abstract class ItemSorter implements Comparator
+    {
+        @Override
+        public int compare(Object o1, Object o2)
+        {
+            if(o1 instanceof ItemStack && o2 instanceof ItemStack)
+            {
+                return compareItem((ItemStack) o1, (ItemStack) o2);
+            }
+            return -1;
+        }
+
+        public abstract int compareItem(ItemStack o1, ItemStack o2);
+    }
+
+    public static class NameSorter extends ItemSorter
+    {
+        @Override
+        public int compareItem(ItemStack a, ItemStack b)
+        {
+            return getLabel(a).compareToIgnoreCase(getLabel(b));
+        }
+
+        public String getLabel(ItemStack stack)
+        {
+            return stack.getDisplayName();
+        }
     }
 }
