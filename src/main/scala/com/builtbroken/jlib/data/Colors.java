f63e4647bdcaa40033d458ea7b940b2f84d21b48
Robert S <rseifert.phone@gmail.com>
2015-01-08 10:50:35 -0500

    Changed int[] rgb to color instance, Added toInt methods to Color enum

--- src/main/scala/com/builtbroken/jlib/data/Colors.java
+++ src/main/scala/com/builtbroken/jlib/data/Colors.java
@@ -3,26 +3,26 @@ package com.builtbroken.jlib.data;
 import java.awt.*;
 
 /**
- * Simple enum of pre-defined colors
+ * Simple enum of pre-defined colors to keep things simple and consistent
  */
 public enum Colors
 {
-	BLACK("\u00a70", "black", new int[] { 0, 0, 0 }),
-	DARK_BLUE("\u00a71", "darkBlue", new int[] { 0, 0, 170 }),
-	DARK_GREEN("\u00a72", "darkGreen", new int[] { 0, 170, 0 }),
-	DARK_AQUA("\u00a73", "darkAqua", new int[] { 0, 170, 170 }),
-	DARK_RED("\u00a74", "darkRed", new int[] { 170, 0, 0 }),
-	PURPLE("\u00a75", "purple", new int[] { 170, 0, 170 }),
-	ORANGE("\u00a76", "orange", new int[] { 255, 170, 0 }),
-	GREY("\u00a77", "grey", new int[] { 170, 170, 170 }),
-	DARK_GREY("\u00a78", "darkGrey", new int[] { 85, 85, 85 }),
-	INDIGO("\u00a79", "indigo", new int[] { 85, 85, 255 }),
-	BRIGHT_GREEN("\u00a7a", "brightGreen", new int[] { 85, 255, 85 }),
-	AQUA("\u00a7b", "aqua", new int[] { 85, 255, 255 }),
-	RED("\u00a7c", "red", new int[] { 255, 85, 85 }),
-	PINK("\u00a7d", "pink", new int[] { 255, 85, 255 }),
-	YELLOW("\u00a7e", "yellow", new int[] { 255, 255, 85 }),
-	WHITE("\u00a7f", "white", new int[] { 255, 255, 255 });
+	BLACK("\u00a70", Color.BLACK),
+	DARK_BLUE("\u00a71", new Color( 0, 0, 170 )),
+	DARK_GREEN("\u00a72", new Color( 0, 170, 0 )),
+	DARK_AQUA("\u00a73", new Color(  0, 170, 170 )),
+	DARK_RED("\u00a74", new Color(  170, 0, 0 )),
+	PURPLE("\u00a75", new Color(  170, 0, 170 )),
+	ORANGE("\u00a76", new Color(  255, 170, 0 )),
+	GREY("\u00a77", new Color(  170, 170, 170 )),
+	DARK_GREY("\u00a78", Color.DARK_GRAY),
+	INDIGO("\u00a79", new Color(  85, 85, 255 )),
+	BRIGHT_GREEN("\u00a7a", new Color(  85, 255, 85 )),
+	AQUA("\u00a7b", new Color(  85, 255, 255 )),
+	RED("\u00a7c", Color.RED),
+	PINK("\u00a7d", Color.PINK),
+	YELLOW("\u00a7e",Color.YELLOW),
+	WHITE("\u00a7f", Color.WHITE);
 
 	/**
 	 * The color code that will be displayed
@@ -30,34 +30,41 @@ public enum Colors
 	public final String code;
 
     //TODO replace with Color object
-	public final int[] rgbCode;
+	public final Color color;
 
-	/**
-	 * A friendly name of the color.
-	 */
-	public String unlocalizedName;
-
-	private Colors(String s, String n, int[] rgb)
+	private Colors(String s, Color color)
 	{
 		code = s;
-		unlocalizedName = n;
-		rgbCode = rgb;
+		this.color = color;
 	}
 
-    //TODO replace with rgb getters that call to a color object
-	public float getColor(int index)
-	{
-		return rgbCode[index] / 255F;
-	}
-
-	public Color toColor()
-	{
-		return new Color(rgbCode[0], rgbCode[1], rgbCode[2]);
-	}
+    public Color color()
+    {
+        return color;
+    }
 
 	@Override
 	public String toString()
 	{
 		return code;
 	}
+
+    public int toInt()
+    {
+        return getIntFromColor(color());
+    }
+
+    public static int getIntFromColor(Color color)
+    {
+        return getIntFromColor(color.getRed(), color.getGreen(), color.getBlue());
+    }
+
+    public static int getIntFromColor(int Red, int Green, int Blue)
+    {
+        Red = (Red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
+        Green = (Green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
+        Blue = Blue & 0x000000FF; //Mask out anything not blue.
+
+        return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+    }
 }
