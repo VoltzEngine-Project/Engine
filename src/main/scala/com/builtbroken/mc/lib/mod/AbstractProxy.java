4c4900ca30b05890568af373cb94d436e1cf8b4f
Robert S <rseifert.phone@gmail.com>
2015-01-08 08:27:32 -0500

    Added an interface to redirect Gui requests to the tile

--- src/main/scala/com/builtbroken/mc/lib/mod/AbstractProxy.java
+++ src/main/scala/com/builtbroken/mc/lib/mod/AbstractProxy.java
@@ -1,7 +1,10 @@
 package com.builtbroken.mc.lib.mod;
 
+import com.builtbroken.mc.api.tile.IGuiTile;
+import com.builtbroken.mc.prefab.tile.Tile;
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import com.builtbroken.mc.lib.mod.loadable.ILoadable;
 
@@ -28,12 +31,30 @@ public abstract class AbstractProxy implements IGuiHandler, ILoadable
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
 	{
-		return null;
+		return getClientGuiElement(ID, player, world.getTileEntity(x, y, z));
 	}
+
+    public Object getServerGuiElement(int ID, EntityPlayer player, TileEntity tile)
+    {
+        if(tile instanceof IGuiTile)
+        {
+            return ((IGuiTile) tile).getServerGuiElement(ID, player);
+        }
+        return null;
+    }
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
 	{
-		return null;
+		return getClientGuiElement(ID, player, world.getTileEntity(x, y, z));
 	}
+
+    public Object getClientGuiElement(int ID, EntityPlayer player, TileEntity tile)
+    {
+        if(tile instanceof IGuiTile)
+        {
+            return ((IGuiTile) tile).getClientGuiElement(ID, player);
+        }
+        return null;
+    }
 }
