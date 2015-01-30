ce549e80d56dd4213f775149f63a2339b71221bb
Robert S <rseifert.phone@gmail.com>
2015-01-30 08:30:30 -0500

    Fixed server side trying to register an ISimpleItemRenderer

--- src/main/scala/com/builtbroken/mc/core/registry/ClientRegistryProxy.java
+++ src/main/scala/com/builtbroken/mc/core/registry/ClientRegistryProxy.java
@@ -1,18 +1,32 @@
 package com.builtbroken.mc.core.registry;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
-import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.tileentity.TileEntity;
+import com.builtbroken.mc.api.items.ISimpleItemRenderer;
+import com.builtbroken.mc.lib.render.block.ItemRenderHandler;
 import com.builtbroken.mc.lib.render.block.RenderTileDummy;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 
 public class ClientRegistryProxy extends CommonRegistryProxy
 {
-	@Override
-	public void registerDummyRenderer(Class<? extends TileEntity> clazz)
-	{
-		if (!TileEntityRendererDispatcher.instance.mapSpecialRenderers.containsKey(clazz))
-		{
-			ClientRegistry.bindTileEntitySpecialRenderer(clazz, new RenderTileDummy());
-		}
-	}
+    @Override
+    public void registerTileEntity(String name, String prefix, Block block, TileEntity tile)
+    {
+        super.registerTileEntity(name, prefix, block, tile);
+        if (tile instanceof ISimpleItemRenderer)
+        {
+            ItemRenderHandler.register(new ItemStack(block).getItem(), (ISimpleItemRenderer) tile);
+        }
+    }
+
+    @Override
+    public void registerDummyRenderer(Class<? extends TileEntity> clazz)
+    {
+        if (!TileEntityRendererDispatcher.instance.mapSpecialRenderers.containsKey(clazz))
+        {
+            ClientRegistry.bindTileEntitySpecialRenderer(clazz, new RenderTileDummy());
+        }
+    }
 }
--- src/main/scala/com/builtbroken/mc/core/registry/CommonRegistryProxy.java
+++ src/main/scala/com/builtbroken/mc/core/registry/CommonRegistryProxy.java
@@ -1,13 +1,14 @@
 package com.builtbroken.mc.core.registry;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
 
 public class CommonRegistryProxy
 {
-    public void registerTileEntity(String name, String prefix, Class<? extends TileEntity> clazz)
+    public void registerTileEntity(String name, String prefix, Block block, TileEntity tile)
     {
-        GameRegistry.registerTileEntityWithAlternatives(clazz, prefix + name, name, clazz.getSimpleName(), prefix + clazz.getSimpleName());
+        GameRegistry.registerTileEntityWithAlternatives(tile.getClass(), prefix + name, name, tile.getClass().getSimpleName(), prefix + tile.getClass().getSimpleName());
     }
 
     public void registerDummyRenderer(Class<? extends TileEntity> clazz)
