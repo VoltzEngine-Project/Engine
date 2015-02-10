922ff6e3d80f5e9ce7d23b96dda975a066469966
Robert S <rseifert.phone@gmail.com>
2015-02-09 19:43:16 -0500

    Fixed for NPE when reflection fails

--- src/main/scala/com/builtbroken/mc/core/registry/ClientRegistryProxy.java
+++ src/main/scala/com/builtbroken/mc/core/registry/ClientRegistryProxy.java
@@ -1,6 +1,7 @@
 package com.builtbroken.mc.core.registry;
 
 import com.builtbroken.mc.api.items.ISimpleItemRenderer;
+import com.builtbroken.mc.core.Engine;
 import com.builtbroken.mc.core.References;
 import com.builtbroken.mc.core.registry.implement.IRegistryInit;
 import com.builtbroken.mc.lib.helper.ReflectionUtility;
@@ -23,7 +24,7 @@ public class ClientRegistryProxy extends CommonRegistryProxy
     public void registerBlock(ModManager manager, String name, String modPrefix, Block block, Class<? extends ItemBlock> itemBlock)
     {
         super.registerBlock(manager, name, modPrefix, block, itemBlock);
-        if(block.getUnlocalizedName() == null || block.getUnlocalizedName().contains("null"))
+        if (block.getUnlocalizedName() == null || block.getUnlocalizedName().contains("null"))
         {
             block.setBlockName(name);
         }
@@ -32,30 +33,41 @@ public class ClientRegistryProxy extends CommonRegistryProxy
         try
         {
             Field field = ReflectionUtility.getMCField(Block.class, "textureName");
-            if (field.get(block) == null)
+            if (field == null)
+            {
+                Engine.instance.logger().error(manager.name() + " Failed to access textureName field for block " + name);
+                block.setBlockTextureName(modPrefix + name);
+            }
+            else if (field.get(block) == null)
             {
                 block.setBlockTextureName(modPrefix + name);
             }
         }
         catch (IllegalAccessException e)
         {
-            References.LOGGER.error(manager.name() + " Failed to access textureName field for block " + name);
+            Engine.instance.logger().error(manager.name() + " Failed to access textureName field for block " + name);
+            block.setBlockTextureName(modPrefix + name);
         }
 
         //Sets creative tab
-        if(manager.defaultTab != null)
+        if (manager.defaultTab != null)
         {
             try
             {
                 Field field = ReflectionUtility.getMCField(Block.class, "displayOnCreativeTab");
-                if (field.get(block) == null)
+                if (field == null)
+                {
+                    References.LOGGER.error(manager.name() + " Failed to access creativeTab field for block " + name);
+                    block.setCreativeTab(manager.defaultTab);
+                }
+                else if (field.get(block) == null)
                 {
                     block.setCreativeTab(manager.defaultTab);
                 }
-            }
-            catch (IllegalAccessException e)
+            } catch (IllegalAccessException e)
             {
                 References.LOGGER.error(manager.name() + " Failed to access creativeTab field for block " + name);
+                block.setCreativeTab(manager.defaultTab);
             }
         }
     }
@@ -66,6 +78,7 @@ public class ClientRegistryProxy extends CommonRegistryProxy
         super.registerItem(manager, name, modPrefix, item);
         if (modPrefix != null)
         {
+            //TODO make forced item name optional
             item.setUnlocalizedName(modPrefix + name);
 
             if (ReflectionHelper.getPrivateValue(Item.class, item, "iconString", "field_111218_cA") == null)
@@ -103,7 +116,7 @@ public class ClientRegistryProxy extends CommonRegistryProxy
     public void onRegistry(Object object)
     {
         super.onRegistry(object);
-        if(object instanceof IRegistryInit)
+        if (object instanceof IRegistryInit)
         {
             ((IRegistryInit) object).onClientRegistered();
         }
