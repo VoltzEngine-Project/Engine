a734893549da2b1ce8a9f6091150eaf3c7c4e458
Robert S <rseifert.phone@gmail.com>
2015-02-10 04:08:19 -0500

    Added chat command to clear armor and inventory from a player

--- src/main/scala/com/builtbroken/mc/core/EngineCommand.java
+++ src/main/scala/com/builtbroken/mc/core/EngineCommand.java
@@ -1,6 +1,5 @@
 package com.builtbroken.mc.core;
 
-import com.builtbroken.mc.core.References;
 import com.builtbroken.mc.prefab.entity.EntityProjectile;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
@@ -9,6 +8,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 
@@ -37,8 +38,11 @@ public class EngineCommand extends CommandBase
         if (args == null || args.length == 0 || args[0].equalsIgnoreCase("help"))
         {
             sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " version"));
-            sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " remove help"));
-            sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " butcher"));
+            if (sender instanceof EntityPlayer)
+            {
+                sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " remove help"));
+                sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " butcher"));
+            }
             //sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " gridinfo"));
             //sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " gridpause"));
         }
@@ -46,79 +50,111 @@ public class EngineCommand extends CommandBase
         {
             sender.addChatMessage(new ChatComponentText("Version: " + References.VERSION + "  Build: " + References.BUILD_VERSION));
         }
-        else if (args[0].equalsIgnoreCase("remove"))
+        else if (sender instanceof EntityPlayer)
         {
-            if (args.length == 1 || args[1].equalsIgnoreCase("help"))
+            EntityPlayer entityPlayer = (EntityPlayer) sender;
+            if (args[0].equalsIgnoreCase("remove"))
             {
-                sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " remove projectiles <radius>"));
-            }
-            else if (sender instanceof EntityPlayer)
-            {
-                int radius = 100;
-                EntityPlayer entityPlayer = (EntityPlayer) sender;
-
-                if(args.length >= 2 && args[2] != null)
+                if (args.length == 1 || args[1].equalsIgnoreCase("help"))
                 {
-                    try
-                    {
-                        radius = Integer.parseInt(args[2]);
-                    }
-                    catch(NumberFormatException e)
-                    {
-                        sender.addChatMessage(new ChatComponentText("Radius needs to be an integer"));
-                    }
-                }
-                if (args[1].equalsIgnoreCase("projectiles"))
-                {
-                    int m = 0;
-                    for (Entity entity : getEntitiesWithin(entityPlayer, radius))
-                    {
-                        if (entity instanceof EntityProjectile)
-                        {
-                            m++;
-                            entity.setDead();
-                        }
-                    }
-
-                    sender.addChatMessage(new ChatComponentText("Removed " + m + " projectile entities within " + radius + " block radius."));
-                }
-                else if (args[1].equalsIgnoreCase("mobs"))
-                {
-                    butcher((EntityPlayer) sender, 100);
-                }
-                else if(args[1].equalsIgnoreCase("all"))
-                {
-                    int m = 0;
-                    for (Entity entity : getEntitiesWithin(entityPlayer, radius))
-                    {
-                        if (entity instanceof EntityLiving)
-                        {
-                            m++;
-                            entity.setDead();
-                        }
-                    }
-
-                    sender.addChatMessage(new ChatComponentText("Removed " + m + " living entities within " + radius + " block radius."));
+                    sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " remove projectiles <radius>"));
+                    sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " remove mobs <radius>"));
+                    sender.addChatMessage(new ChatComponentText("/" + getCommandName() + " remove all <radius>"));
                 }
                 else
                 {
-                    sender.addChatMessage(new ChatComponentText("Not sure what your trying to remove?"));
+                    int radius = 100;
+
+                    if (args.length >= 2 && args[2] != null)
+                    {
+                        try
+                        {
+                            radius = Integer.parseInt(args[2]);
+                        } catch (NumberFormatException e)
+                        {
+                            sender.addChatMessage(new ChatComponentText("Radius needs to be an integer"));
+                        }
+                    }
+                    if (args[1].equalsIgnoreCase("projectiles"))
+                    {
+                        int m = 0;
+                        for (Entity entity : getEntitiesWithin(entityPlayer, radius))
+                        {
+                            if (entity instanceof EntityProjectile)
+                            {
+                                m++;
+                                entity.setDead();
+                            }
+                        }
+
+                        sender.addChatMessage(new ChatComponentText("Removed " + m + " projectile entities within " + radius + " block radius."));
+                    }
+                    else if (args[1].equalsIgnoreCase("mobs"))
+                    {
+                        butcher(entityPlayer, 100);
+                    }
+                    else if (args[1].equalsIgnoreCase("all"))
+                    {
+                        int m = 0;
+                        for (Entity entity : getEntitiesWithin(entityPlayer, radius))
+                        {
+                            if (entity instanceof EntityLiving)
+                            {
+                                m++;
+                                entity.setDead();
+                            }
+                        }
+
+                        sender.addChatMessage(new ChatComponentText("Removed " + m + " living entities within " + radius + " block radius."));
+                    }
+                    else
+                    {
+                        sender.addChatMessage(new ChatComponentText("Not sure what you are trying to remove?"));
+                    }
+                }
+            }
+            else if (args[0].equalsIgnoreCase("butcher"))
+            {
+                butcher((EntityPlayer) sender, 100);
+            }
+            else if(args[0].equalsIgnoreCase("clearinv"))
+            {
+                EntityPlayer p;
+                if(args.length > 1)
+                    p = getPlayer(sender, args[1]);
+                else
+                    p = entityPlayer;
+
+                if(p != null)
+                {
+                    for (int slot = 0; slot < p.inventory.mainInventory.length; slot++)
+                    {
+                        p.inventory.mainInventory[slot] = null;
+                    }
+                    p.inventoryContainer.detectAndSendChanges();
+                }
+            }
+            else if(args[0].equalsIgnoreCase("cleararmor"))
+            {
+
+                EntityPlayer p;
+                if(args.length > 1)
+                    p = getPlayer(sender, args[1]);
+                else
+                    p = entityPlayer;
+
+                if(p != null)
+                {
+                    for (int slot = 0; slot < p.inventory.armorInventory.length; slot++)
+                    {
+                        p.inventory.armorInventory[slot] = null;
+                    }
+                    p.inventoryContainer.detectAndSendChanges();
                 }
             }
             else
             {
-                sender.addChatMessage(new ChatComponentText("Only a player can trigger remove commands at the moment"));
-            }
-        }
-        else if (args[0].equalsIgnoreCase("butcher"))
-        {
-            if (sender instanceof EntityPlayer)
-            {
-                butcher((EntityPlayer) sender, 100);
-            }
-            else
-            {
-                sender.addChatMessage(new ChatComponentText("Only a player can trigger this command"));
+                throw new WrongUsageException(this.getCommandUsage(sender));
             }
         }
         else
@@ -140,6 +176,27 @@ public class EngineCommand extends CommandBase
         }
 
         entityPlayer.addChatComponentMessage(new ChatComponentText("Removed " + m + " mobs entities within " + radius + " block radius."));
+    }
+
+    @Override
+    public List addTabCompletionOptions(ICommandSender sender, String[] args)
+    {
+        if(args != null && args.length > 0 && args[0] != null)
+        {
+            if (sender instanceof EntityPlayer)
+            {
+                if (args[0].equalsIgnoreCase("clearinv"))
+                {
+                    return args.length == 2 ? getListOfStringsMatchingLastWord(args, this.userNames()) : null;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected String[] userNames()
+    {
+        return MinecraftServer.getServer().getAllUsernames();
     }
 
     private List<Entity> getEntitiesWithin(EntityPlayer entityPlayer, double radius)
