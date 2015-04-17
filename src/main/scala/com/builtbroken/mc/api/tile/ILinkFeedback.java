0ff3ee56d0da4f7f324465cee1b2f4d9172c879a
Robert S <rseifert.phone@gmail.com>
2015-04-17 02:36:36 -0400

    Two more interfaces for linking machines

---
+++ src/main/scala/com/builtbroken/mc/api/tile/ILinkFeedback.java
@@ -0,0 +1,16 @@
+package com.builtbroken.mc.api.tile;
+
+import com.builtbroken.mc.lib.transform.vector.Location;
+
+/** Used to receive feed back when machines are linked to each other.
+ * Called normally in {@link com.builtbroken.mc.api.tile.ILinkable#link(com.builtbroken.mc.lib.transform.vector.Location, short)}
+ * Created by robert on 4/16/2015.
+ */
+public interface ILinkFeedback
+{
+    /**
+     * Called when this tile is linked to another tile
+     * @param location - location of the machine this one was linked to
+     */
+    public void onLinked(Location location);
+}
