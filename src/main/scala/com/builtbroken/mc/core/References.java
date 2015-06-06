5fa1191d6252a9fe8c94ce4c433a2ae05aed58d1
Robert Seifert <rseifert.phone@gmail.com>
2015-06-06 18:57:49 -0400

    Added build # to the version #

--- src/main/scala/com/builtbroken/mc/core/References.java
+++ src/main/scala/com/builtbroken/mc/core/References.java
@@ -22,8 +22,8 @@ public final class References
     public static final String MAJOR_VERSION = "@MAJOR@";
 	public static final String MINOR_VERSION = "@MINOR@";
 	public static final String REVISION_VERSION = "@REVIS@";
-	public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION;
 	public static final String BUILD_VERSION = "@BUILD@";
+	public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION + "." + BUILD_VERSION;
 
     public static final String DOMAIN = "voltzengine";
 	public static final String PREFIX = DOMAIN + ":";
