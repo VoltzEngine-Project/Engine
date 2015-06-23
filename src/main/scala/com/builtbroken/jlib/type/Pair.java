a0272ac740e126fa57c091022d4ef61c07fd9530
Robert Seifert <rseifert.phone@gmail.com>
2015-06-23 01:34:46 -0400

    Added toString() to Pair.class, Little cleanup in Pair.class

--- src/main/scala/com/builtbroken/jlib/type/Pair.java
+++ src/main/scala/com/builtbroken/jlib/type/Pair.java
@@ -1,64 +1,56 @@
 package com.builtbroken.jlib.type;
 
 /**
- * Container for two objects
+ * Container for two objects that need to be linked to each other
  *
  * @author Robert Seifert
  */
 public class Pair<L, R>
 {
-	private L left;
-	private R right;
+    private L left;
+    private R right;
 
-	public Pair(L left, R right)
-	{
-		this.left = left;
-		this.right = right;
-	}
+    public Pair(L left, R right)
+    {
+        this.left = left;
+        this.right = right;
+    }
 
-	public L left()
-	{
-		return left;
-	}
+    public L left()
+    {
+        return left;
+    }
 
-	public R right()
-	{
-		return right;
-	}
+    public R right()
+    {
+        return right;
+    }
 
-	public void setLeft(L l)
-	{
-		left = l;
-	}
+    public void setLeft(L l)
+    {
+        left = l;
+    }
 
-	public void setRight(R r)
-	{
-		right = r;
-	}
+    public void setRight(R r)
+    {
+        right = r;
+    }
 
-	@Override
-	public int hashCode()
-	{
-		if (left == null || right == null)
-		{
-			super.hashCode();
-		}
-		return left.hashCode() ^ right.hashCode();
-	}
+    @Override
+    public int hashCode()
+    {
+        return (left == null || right == null) ? super.hashCode() : left.hashCode() ^ right.hashCode();
+    }
 
-	@Override
-	public boolean equals(Object o)
-	{
-		if (o == null)
-		{
-			return false;
-		}
-		if (!(o instanceof Pair))
-		{
-			return false;
-		}
-		Pair pairo = (Pair) o;
-		return this.left.equals(pairo.left()) && this.right.equals(pairo.right());
-	}
+    @Override
+    public boolean equals(Object o)
+    {
+        return o instanceof Pair ? this.left.equals(((Pair) o).left()) && this.right.equals(((Pair) o).right()) : false;
+    }
 
+    @Override
+    public String toString()
+    {
+        return "Pair[l='" + left + "' r='" + right + "']";
+    }
 }
