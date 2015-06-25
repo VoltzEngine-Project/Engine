c073a1a42cc8f4fdeda97c8adf46b75c16f1100f
Robert Seifert <rseifert.phone@gmail.com>
2015-06-25 10:24:59 -0400

    Added some render icon methods to RenderItemOverlayUtility

--- src/main/scala/com/builtbroken/mc/api/items/I2DCastItem.java
+++ src/main/scala/com/builtbroken/mc/api/items/I2DCastItem.java
@@ -4,12 +4,10 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
-import net.minecraftforge.fluids.Fluid;
 
 /**
  * Created by Dark on 6/23/2015.
  */
-@SideOnly(Side.CLIENT)
 public interface I2DCastItem
 {
     /**
@@ -17,13 +15,6 @@ public interface I2DCastItem
      * @param stack - this item
      * @return
      */
+    @SideOnly(Side.CLIENT)
     IIcon getCastIcon(ItemStack stack);
-
-    /**
-     * The icon used to render the item being casted
-     * @param stack - this item
-     * @param fluid - fluid being casted
-     * @return
-     */
-    IIcon getCastedItemIcon(ItemStack stack, Fluid fluid);
 }
