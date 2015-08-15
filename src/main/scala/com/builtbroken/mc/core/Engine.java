932393723d6572d40fb0c44330aeb4efb3c26c19
Robert Seifert <rseifert.phone@gmail.com>
2015-08-15 00:50:27 -0400

    Changed Engine to no longer use AbstractMod to ensure better loading order

--- src/main/scala/com/builtbroken/mc/core/Engine.java
+++ src/main/scala/com/builtbroken/mc/core/Engine.java
@@ -21,12 +21,13 @@ import com.builtbroken.mc.core.network.netty.PacketManager;
 import com.builtbroken.mc.core.proxy.NEIProxy;
 import com.builtbroken.mc.core.registry.ModManager;
 import com.builtbroken.mc.lib.helper.PotionUtility;
-import com.builtbroken.mc.lib.mod.AbstractMod;
 import com.builtbroken.mc.lib.mod.AbstractProxy;
 import com.builtbroken.mc.lib.mod.compat.Mods;
 import com.builtbroken.mc.lib.mod.compat.oc.OCProxy;
+import com.builtbroken.mc.lib.mod.compat.rf.RFLoader;
 import com.builtbroken.mc.lib.mod.config.ConfigHandler;
 import com.builtbroken.mc.lib.mod.config.ConfigScanner;
+import com.builtbroken.mc.lib.mod.loadable.LoadableHandler;
 import com.builtbroken.mc.lib.world.edit.PlacementData;
 import com.builtbroken.mc.lib.world.explosive.ExplosiveRegistry;
 import com.builtbroken.mc.lib.world.heat.HeatedBlockRegistry;
@@ -37,6 +38,10 @@ import com.builtbroken.mc.prefab.recipe.fluid.MRHandlerFluidStack;
 import com.builtbroken.mc.prefab.recipe.item.MRHandlerItemStack;
 import com.builtbroken.mc.prefab.recipe.item.MRSmelterHandler;
 import com.builtbroken.mc.prefab.tile.item.ItemBlockMetadata;
+import com.builtbroken.mc.prefab.tile.multiblock.BlockMultiblock;
+import com.builtbroken.mc.prefab.tile.multiblock.TileMulti;
+import com.builtbroken.mc.prefab.tile.multiblock.types.TileMultiInv;
+import com.builtbroken.mc.prefab.tile.multiblock.types.TileMultiTank;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -48,6 +53,7 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.command.ICommandManager;
 import net.minecraft.command.ServerCommandManager;
@@ -60,6 +66,7 @@ import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.oredict.OreDictionary;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.util.Arrays;
@@ -71,7 +78,7 @@ import java.util.Arrays;
  */
 
 @Mod(modid = References.ID, name = References.NAME, version = References.VERSION, acceptableRemoteVersions = "*")
-public class Engine extends AbstractMod
+public class Engine
 {
     public static final ModManager contentRegistry = new ModManager().setPrefix(References.PREFIX).setTab(CreativeTabs.tabTools);
     public static final boolean runningAsDev = System.getProperty("development") != null && System.getProperty("development").equalsIgnoreCase("true");
@@ -85,8 +92,15 @@ public class Engine extends AbstractMod
     @Instance(References.ID)
     public static Engine instance;
 
+    protected LoadableHandler loader;
+    protected ModManager manager;
+    protected Logger logger;
+    private Configuration config;
+
     public static Block ore = null;
     public static Block heatedStone;
+    public static Block multiBlock;
+
     public static Item itemWrench;
     public static Item instaHole;
     public static Item itemSelectionTool;
@@ -94,6 +108,7 @@ public class Engine extends AbstractMod
 
     //Interal trigger booleans
     private static boolean oresRequested = false;
+    private static boolean multiBlockRequested = false;
     public static boolean heatedRockRequested = false;
 
     public final PacketManager packetHandler = new PacketManager(References.CHANNEL);
@@ -113,11 +128,6 @@ public class Engine extends AbstractMod
     public static double TO_RF_RATIO = 500;
     public static double TO_BC_RATIO = 50;
 
-    public Engine()
-    {
-        super(References.PREFIX, "ve/VoltzEngine");
-    }
-
     /**
      * Requests that all ores are generated
      * Must be called in pre-init
@@ -127,12 +137,25 @@ public class Engine extends AbstractMod
         oresRequested = true;
     }
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent evt)
+    public static void requestMultiBlock()
     {
-        super.preInit(evt);
+        multiBlockRequested = true;
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        NetworkRegistry.INSTANCE.registerGuiHandler(this, getProxy());
+        config = new Configuration(new File(event.getModConfigurationDirectory(), "bbm/ve/VoltzEngine.cfg"));
+        heatDataConfig = new Configuration(new File(event.getModConfigurationDirectory(), "bbm/ve/HeatMap.cfg"));
+        explosiveConfig = new Configuration(new File(event.getModConfigurationDirectory(), "bbm/ve/Explosives.cfg"));
+
+        config.load();
+        heatDataConfig.load();
+        explosiveConfig.load();
+
         References.LOGGER = logger;
-        ConfigScanner.instance().generateSets(evt.getAsmData());
+        ConfigScanner.instance().generateSets(event.getAsmData());
         ConfigHandler.sync(getConfig(), References.DOMAIN);
         NetworkRegistry.INSTANCE.registerGuiHandler(this, proxy);
 
@@ -142,15 +165,11 @@ public class Engine extends AbstractMod
         MinecraftForge.EVENT_BUS.register(SelectionHandler.INSTANCE);
         FMLCommonHandler.instance().bus().register(SelectionHandler.INSTANCE);
 
-        heatDataConfig = new Configuration(new File(evt.getModConfigurationDirectory(), "bbm/ve/HeatMap.cfg"));
-        explosiveConfig = new Configuration(new File(evt.getModConfigurationDirectory(), "bbm/ve/Explosives.cfg"));
 
         //Load heat configs
-        heatDataConfig.load();
         enabledHeatMap = heatDataConfig.getBoolean("EnabledHeatMap", Configuration.CATEGORY_GENERAL, true, "Heat map handles interaction of heat based energy and the world. Disable only if it causes issues or you want to reduce world file size. If disabled it can prevent machines from working.");
 
         //Load explosive configs
-        explosiveConfig.load();
         log_registering_explosives = explosiveConfig.getBoolean("EnableRegisterLogging", Configuration.CATEGORY_GENERAL, false, "Adds debug each time a mod registers an explosive handler. Should only be enabled to figure out which mod is overriding another mod's explosive");
 
         MachineRecipeType.ITEM_SMELTER.setHandler(new MRSmelterHandler());
@@ -161,6 +180,7 @@ public class Engine extends AbstractMod
         MachineRecipeType.FLUID_SMELTER.setHandler(new MRHandlerFluidStack(MachineRecipeType.FLUID_SMELTER.INTERNAL_NAME));
         MachineRecipeType.FLUID_CAST.setHandler(new MRHandlerCast());
 
+        loader.applyModule(getProxy());
         loader.applyModule(packetHandler);
         loader.applyModule(CrusherRecipeLoad.class);
         loader.applyModule(GrinderRecipeLoad.class);
@@ -169,6 +189,32 @@ public class Engine extends AbstractMod
         loader.applyModule(NEIProxy.class);
         loader.applyModule(GroupProfileHandler.GLOBAL);
         loader.applyModule(OCProxy.class, Mods.OC.isLoaded());
+
+        //Check if RF api exists
+        boolean shouldLoadRFHandler = true;
+        for (String s : new String[]{"IEnergyConnection", "IEnergyContainerItem", "IEnergyHandler", "IEnergyProvider", "IEnergyReceiver", "IEnergyStorage"})
+        {
+            try
+            {
+                Class clazz = Class.forName("cofh.api.energy." + s);
+                if (clazz == null)
+                {
+                    shouldLoadRFHandler = false;
+                    break;
+                }
+            } catch (ClassNotFoundException e)
+            {
+                shouldLoadRFHandler = false;
+                logger().error("Not loading RF support as we couldn't detect one of cofh's energy classes");
+                e.printStackTrace();
+                break;
+            }
+        }
+
+        if (shouldLoadRFHandler)
+        {
+            loader.applyModule(RFLoader.class);
+        }
 
         PotionUtility.resizePotionArray();
 
@@ -181,7 +227,7 @@ public class Engine extends AbstractMod
         ToolMode.REGISTRY.add(new ToolModeGeneral());
         ToolMode.REGISTRY.add(new ToolModeRotation());
 
-        if(runningAsDev)
+        if (runningAsDev)
             itemCraftingParts = contentRegistry.newItem(ItemParts.class);
         /**
          * Multiblock Handling
@@ -192,12 +238,13 @@ public class Engine extends AbstractMod
             itemWrench = getManager().newItem("ve.screwdriver", new ItemScrewdriver());
         if (getConfig().get("Content", "LoadSelectionTool", true).getBoolean(true))
             itemSelectionTool = getManager().newItem("ve.selectiontool", new ItemSelectionWand());
+
+        loader.preInit();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent evt)
     {
-        super.init(evt);
         Engine.metadata.modId = References.NAME;
         Engine.metadata.name = References.NAME;
         Engine.metadata.description = References.NAME + " is a content creation toolkit";
@@ -218,30 +265,36 @@ public class Engine extends AbstractMod
             Ores.registerSet(ore, getConfig());
         }
 
-        if(heatedRockRequested)
+        if (multiBlockRequested)
+        {
+            multiBlock = contentRegistry.newBlock("veMultiBlock", BlockMultiblock.class);
+            GameRegistry.registerTileEntity(TileMulti.class, "veTileMulti");
+            GameRegistry.registerTileEntity(TileMultiInv.class, "veTileMultiInv");
+            GameRegistry.registerTileEntity(TileMultiTank.class, "veTileMultiTank");
+        }
+
+        if (heatedRockRequested)
         {
             heatedStone = contentRegistry.newBlock("VEHeatedRock", BlockHeatedStone.class, ItemBlockMetadata.class);
             NEIProxy.hideItem(heatedStone);
-            if(enabledHeatMap)
+            if (enabledHeatMap)
             {
                 HeatedBlockRegistry.addNewHeatingConversion(Blocks.stone, new PlacementData(heatedStone, 15), 600);
                 HeatedBlockRegistry.addNewHeatingConversion(heatedStone, Blocks.lava, 1200);
             }
         }
+
+        loader.init();
+        getManager().fireInit();
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent evt)
     {
-        super.postInit(evt);
-
         //All blocks should be loaded before post init so we can init things that need to iterate over the block list
-        if(enabledHeatMap)
+        if (enabledHeatMap)
             HeatedBlockRegistry.init(heatDataConfig);
 
-        //Save configs as this is our last chance
-        heatDataConfig.save();
-        explosiveConfig.save();
 
         OreDictionary.registerOre("ingotGold", Items.gold_ingot);
         OreDictionary.registerOre("ingotIron", Items.iron_ingot);
@@ -255,13 +308,36 @@ public class Engine extends AbstractMod
         //MachineRecipes.instance.addRecipe(RecipeType.ITEM_GRINDER.name(), Blocks.cobblestone, Blocks.sand);
         //MachineRecipes.instance.addRecipe(RecipeType.ITEM_GRINDER.name(), Blocks.gravel, Blocks.sand);
         //MachineRecipes.instance.addRecipe(RecipeType.ITEM_GRINDER.name(), Blocks.glass, Blocks.sand);
+
+        loader.postInit();
+        getManager().firePostInit();
+
+        //Save configs as this is our last chance
+        heatDataConfig.save();
+        explosiveConfig.save();
+        getConfig().save();
     }
 
-    @Override
     public AbstractProxy getProxy()
     {
         return proxy;
     }
+
+    public Configuration getConfig()
+    {
+        return config;
+    }
+
+    public ModManager getManager()
+    {
+        return this.manager;
+    }
+
+    public Logger logger()
+    {
+        return this.logger;
+    }
+
 
     @EventHandler
     public void serverStarting(FMLServerStartingEvent event)
@@ -279,7 +355,7 @@ public class Engine extends AbstractMod
 
     public static boolean isPlayerOpped(EntityPlayer player)
     {
-        return player instanceof EntityPlayerMP && isPlayerOpped((EntityPlayerMP)player);
+        return player instanceof EntityPlayerMP && isPlayerOpped((EntityPlayerMP) player);
     }
 
     public static boolean isPlayerOpped(EntityPlayerMP player)
