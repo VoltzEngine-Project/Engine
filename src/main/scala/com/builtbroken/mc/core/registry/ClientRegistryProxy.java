db7afc28e9bda126e26a77960db0b5d6e070efef
Robert Seifert <rseifert.phone@gmail.com>
2015-08-29 13:43:08 -0400

    Fixed creative tab being overridden when registering items, Removed most reflection in client proxy for registry

--- src/main/scala/com/builtbroken/mc/core/registry/ClientRegistryProxy.java
+++ src/main/scala/com/builtbroken/mc/core/registry/ClientRegistryProxy.java
@@ -1,13 +1,10 @@
 package com.builtbroken.mc.core.registry;
 
 import com.builtbroken.mc.api.items.ISimpleItemRenderer;
-import com.builtbroken.mc.core.Engine;
 import com.builtbroken.mc.core.registry.implement.IRegistryInit;
-import com.builtbroken.mc.lib.helper.ReflectionUtility;
 import com.builtbroken.mc.lib.render.block.ItemRenderHandler;
 import com.builtbroken.mc.lib.render.block.RenderTileDummy;
 import cpw.mods.fml.client.registry.ClientRegistry;
-import cpw.mods.fml.relauncher.ReflectionHelper;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.Item;
@@ -15,45 +12,29 @@ import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 
-import java.lang.reflect.Field;
-
 public class ClientRegistryProxy extends CommonRegistryProxy
 {
     @Override
     public void registerBlock(ModManager manager, String name, String modPrefix, Block block, Class<? extends ItemBlock> itemBlock)
     {
         super.registerBlock(manager, name, modPrefix, block, itemBlock);
+
+        //Set block name if it is missing
         if (block.getUnlocalizedName() == null || block.getUnlocalizedName().contains("null"))
         {
-            block.setBlockName(name);
+            block.setBlockName((modPrefix != null ? modPrefix : "") + name);
         }
 
-        //Set texture name, reflection is used to prevent overriding the blocks existing name
-        try
+        //Set texture name if it is missing
+        if (modPrefix != null && (block.textureName == null || block.textureName.isEmpty()))
         {
-            Field field = ReflectionUtility.getMCField(Block.class, "textureName", "field_149768_d");
-            if (field == null)
-            {
-                Engine.instance.logger().error(manager.name() + " Failed to access textureName field for block " + name);
-                block.setBlockTextureName(modPrefix + name);
-            }
-            else if (field.get(block) == null)
-            {
-                block.setBlockTextureName(modPrefix + name);
-            }
-        } catch (IllegalAccessException e)
-        {
-            Engine.instance.logger().error(manager.name() + " Failed to access textureName field for block " + name);
             block.setBlockTextureName(modPrefix + name);
         }
 
-        //Sets creative tab
-        if (manager.defaultTab != null)
+        //Sets creative tab if it is missing
+        if (manager.defaultTab != null && block.getCreativeTabToDisplayOn() == null)
         {
-            if (ReflectionHelper.getPrivateValue(Block.class, block, "displayOnCreativeTab", "field_149772_a") == null)
-            {
-                block.setCreativeTab(manager.defaultTab);
-            }
+            block.setCreativeTab(manager.defaultTab);
         }
     }
 
@@ -63,18 +44,18 @@ public class ClientRegistryProxy extends CommonRegistryProxy
         super.registerItem(manager, name, modPrefix, item);
         if (modPrefix != null)
         {
-            if (ReflectionHelper.getPrivateValue(Item.class, item, "unlocalizedName", "field_77774_bZ") == null)
+            if (item.unlocalizedName == null || item.unlocalizedName.isEmpty())
             {
                 item.setUnlocalizedName(modPrefix + name);
             }
 
-            if (ReflectionHelper.getPrivateValue(Item.class, item, "iconString", "field_111218_cA") == null)
+            if (item.iconString == null || item.iconString.isEmpty())
             {
                 item.setTextureName(modPrefix + name);
             }
         }
 
-        if (manager.defaultTab != null)
+        if (manager.defaultTab != null && item.getCreativeTab() == null)
         {
             item.setCreativeTab(manager.defaultTab);
         }
