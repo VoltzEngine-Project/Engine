5708e7874538686b058e8ad73819ef750ee40689
Robert Seifert <rseifert.phone@gmail.com>
2015-09-17 05:48:03 -0400

    Added java docs to AbstractCommand

--- src/main/scala/com/builtbroken/mc/prefab/commands/AbstractCommand.java
+++ src/main/scala/com/builtbroken/mc/prefab/commands/AbstractCommand.java
@@ -10,12 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by robert on 2/10/2015.
+ * Prefab for creating Minecraft based commands on the existing system. Contains helper methods
+ * and checks to improve the speed of making new commands.
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * <p/>
+ * <p/>
+ * Created by Dark(DarkGuardsman, Robert) on 2/10/2015.
  */
 public class AbstractCommand extends CommandBase
 {
+    /** Name of the command */
     protected final String name;
 
+    /**
+     * Creats a new command instance, should only be one instance per command
+     *
+     * @param name - name of the command, used by the user to call the command
+     */
     public AbstractCommand(String name)
     {
         this.name = name;
@@ -45,11 +57,24 @@ public class AbstractCommand extends CommandBase
         }
     }
 
+    /**
+     * Checks to see if the command is a help command. Override if you want to change when help is displayed.
+     *
+     * @param args - arguments for the command
+     * @return true if is a help command
+     */
     public boolean isHelpCommand(String[] args)
     {
         return args == null || args.length == 0 || args[0] == null || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("?");
     }
 
+    /**
+     * Called to handle help command
+     *
+     * @param sender - user running the command
+     * @param args   - arguments for the command
+     * @return true if the help command was handled or false if it wasn't a help command
+     */
     public boolean handleHelp(ICommandSender sender, String[] args)
     {
         if (isHelpCommand(args))
@@ -71,16 +96,36 @@ public class AbstractCommand extends CommandBase
         return false;
     }
 
+    /**
+     * Called to handle a command run by a player in game
+     *
+     * @param player - user in game who ran the chat command
+     * @param args   - args for the command
+     * @return true if the command was consumed or was processed in some way
+     */
     public boolean handleEntityPlayerCommand(EntityPlayer player, String[] args)
     {
         return false;
     }
 
+    /**
+     * Called to handle a command run by something that is not a player
+     *
+     * @param sender - user running the command
+     * @param args   - args for the command
+     * @return true if the command was consumed or was processed in some way
+     */
     public boolean handleConsoleCommand(ICommandSender sender, String[] args)
     {
         return false;
     }
 
+    /**
+     * Added the help output to the player's chat
+     *
+     * @param sender - user running the command
+     * @param p      - page number to output
+     */
     protected void printHelp(ICommandSender sender, int p)
     {
         List<String> items = new ArrayList();
@@ -94,16 +139,34 @@ public class AbstractCommand extends CommandBase
         sender.addChatMessage(new ChatComponentText(""));
     }
 
+    /**
+     * Gets the prefix for this command
+     *
+     * @return prefix for the command
+     */
     public String getPrefix()
     {
         return "/" + getCommandName();
     }
 
+    /**
+     * Called to add entries to the help output list. Only return the name
+     * of the command and not it's parent prefix. As that will be applied
+     * for you automatically to save time.
+     *
+     * @param sender - user running the command
+     * @param items  - list to add entries too
+     */
     public void getHelpOutput(ICommandSender sender, List<String> items)
     {
         items.add("help");
     }
 
+    /**
+     * Gets all users only by username
+     *
+     * @return array of strings
+     */
     protected final String[] playersOnlineByUsername()
     {
         return MinecraftServer.getServer().getAllUsernames();
@@ -138,11 +201,24 @@ public class AbstractCommand extends CommandBase
         return s.trim();
     }
 
+    /**
+     * Removes the first entry from the array
+     *
+     * @param array - array to work on
+     * @return new array missing the first entry
+     */
     protected String[] removeFront(String[] array)
     {
         return removeFront(array, 1);
     }
 
+    /**
+     * Removes so many entries fromt he front of the array
+     *
+     * @param array - array to work on
+     * @param count - number of entries to remove
+     * @return new array
+     */
     protected String[] removeFront(String[] array, int count)
     {
         if (count <= 0)
