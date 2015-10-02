a0a4ace1132ba1094aa8777726ff73de5ecbe587
Robert Seifert <rseifert.phone@gmail.com>
2015-10-02 17:14:58 -0400

    Reworked RecipeShapedOre to extend ShapedOreRecipe

--- src/main/scala/com/builtbroken/mc/prefab/recipe/item/RecipeShapedOre.java
+++ src/main/scala/com/builtbroken/mc/prefab/recipe/item/RecipeShapedOre.java
@@ -1,31 +1,23 @@
 package com.builtbroken.mc.prefab.recipe.item;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import net.minecraft.block.Block;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
-public class RecipeShapedOre implements IRecipe
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class RecipeShapedOre extends ShapedOreRecipe
 {
-    //Added in for future ease of change, but hard coded for now.
     private static final int MAX_CRAFT_GRID_WIDTH = 3;
     private static final int MAX_CRAFT_GRID_HEIGHT = 3;
-
-    private ItemStack output = null;
-    private Object[] input = null;
-    private int width = 0;
-    private int height = 0;
-    private boolean mirrored = true;
+    int w = 0;
+    int h = 0;
 
     public RecipeShapedOre(Block result, Object... recipe) { this(new ItemStack(result), recipe); }
 
@@ -33,95 +25,24 @@ public class RecipeShapedOre implements IRecipe
 
     public RecipeShapedOre(ItemStack result, Object... recipe)
     {
-        output = result.copy();
-        process(recipe);
+        super(result, recipe);
+        getV();
     }
 
-    protected void process(Object... recipe)
+    private void getV()
     {
-        String shape = "";
-        int idx = 0;
-
-        if (recipe[idx] instanceof Boolean)
+        try
         {
-            mirrored = (Boolean) recipe[idx];
-            if (recipe[idx + 1] instanceof Object[])
-            {
-                recipe = (Object[]) recipe[idx + 1];
-            } else
-            {
-                idx = 1;
-            }
-        }
+            Field field = ShapedOreRecipe.class.getDeclaredFields()[4];
+            field.setAccessible(true);
+            w = field.getInt(this);
 
-        //Parse for recipe
-        if (recipe[idx] instanceof String[])
+            field = ShapedOreRecipe.class.getDeclaredFields()[5];
+            field.setAccessible(true);
+            h = field.getInt(this);
+        } catch (IllegalAccessException e)
         {
-            String[] parts = ((String[]) recipe[idx++]);
-
-            for (String s : parts)
-            {
-                width = s.length();
-                shape += s;
-            }
-
-            height = parts.length;
-        } else
-        {
-            while (recipe[idx] instanceof String)
-            {
-                String s = (String) recipe[idx++];
-                shape += s;
-                width = s.length();
-                height++;
-            }
-        }
-
-        //Parse for recipe items
-        if (width * height != shape.length())
-        {
-            String ret = "Invalid shaped ore recipe: ";
-            for (Object tmp : recipe)
-            {
-                ret += tmp + ", ";
-            }
-            ret += output;
-            throw new RuntimeException(ret);
-        }
-
-        HashMap<Character, Object> itemMap = new HashMap<Character, Object>();
-
-        for (; idx < recipe.length; idx += 2)
-        {
-            Character chr = (Character) recipe[idx];
-            Object in = recipe[idx + 1];
-            ItemStack stack = toItemStack(in);
-            if (stack == null)
-            {
-                if (in instanceof String && !OreDictionary.getOres((String) in).isEmpty())
-                {
-                    itemMap.put(chr, OreDictionary.getOres((String) in));
-                } else
-                {
-                    String ret = "Invalid shaped ore recipe: ";
-                    for (Object tmp : recipe)
-                    {
-                        ret += tmp + ", ";
-                    }
-                    ret += output;
-                    throw new RuntimeException(ret);
-                }
-            } else
-            {
-                itemMap.put(chr, stack);
-            }
-        }
-
-        input = new Object[width * height];
-        int x = 0;
-        for (char chr : shape.toCharArray())
-        {
-            input[x++] = itemMap.get(chr);
+            throw new RuntimeException(e);
         }
     }
 
@@ -130,30 +51,18 @@ public class RecipeShapedOre implements IRecipe
         if (in instanceof ItemStack)
         {
             return ((ItemStack) in).copy();
-        } else if (in instanceof Item)
+        }
+        else if (in instanceof Item)
         {
             return new ItemStack((Item) in);
-        } else if (in instanceof Block)
+        }
+        else if (in instanceof Block)
         {
             return new ItemStack((Block) in, 1, OreDictionary.WILDCARD_VALUE);
         }
         return null;
     }
 
-    /**
-     * Returns an Item that is the result of this recipe
-     */
-    @Override
-    public ItemStack getCraftingResult(InventoryCrafting var1) { return output.copy(); }
-
-    /**
-     * Returns the size of the recipe area
-     */
-    @Override
-    public int getRecipeSize() { return input.length; }
-
-    @Override
-    public ItemStack getRecipeOutput() { return output; }
 
     /**
      * Used to check if a recipe matches current crafting inventory
@@ -161,16 +70,11 @@ public class RecipeShapedOre implements IRecipe
     @Override
     public boolean matches(InventoryCrafting inv, World world)
     {
-        for (int x = 0; x <= MAX_CRAFT_GRID_WIDTH - width; x++)
+        for (int x = 0; x <= MAX_CRAFT_GRID_WIDTH - w; x++)
         {
-            for (int y = 0; y <= MAX_CRAFT_GRID_HEIGHT - height; ++y)
+            for (int y = 0; y <= MAX_CRAFT_GRID_HEIGHT - h; ++y)
             {
                 if (checkMatch(inv, x, y, false))
-                {
-                    return true;
-                }
-
-                if (mirrored && checkMatch(inv, x, y, true))
                 {
                     return true;
                 }
@@ -191,14 +95,15 @@ public class RecipeShapedOre implements IRecipe
                 int subY = y - startY;
                 Object target = null;
 
-                if (subX >= 0 && subY >= 0 && subX < width && subY < height)
+                if (subX >= 0 && subY >= 0 && subX < w && subY < h)
                 {
                     if (mirror)
                     {
-                        target = input[width - subX - 1 + subY * width];
-                    } else
+                        target = getInput()[w - subX - 1 + subY * w];
+                    }
+                    else
                     {
-                        target = input[subX + subY * width];
+                        target = getInput()[subX + subY * w];
                     }
                 }
 
@@ -210,7 +115,8 @@ public class RecipeShapedOre implements IRecipe
                     {
                         return false;
                     }
-                } else if (target instanceof ArrayList)
+                }
+                else if (target instanceof ArrayList)
                 {
                     boolean matched = false;
 
@@ -224,7 +130,8 @@ public class RecipeShapedOre implements IRecipe
                     {
                         return false;
                     }
-                } else if (target == null && slot != null)
+                }
+                else if (target == null && slot != null)
                 {
                     return false;
                 }
@@ -248,22 +155,5 @@ public class RecipeShapedOre implements IRecipe
             }
         }
         return false;
-    }
-
-    public RecipeShapedOre setMirrored(boolean mirror)
-    {
-        mirrored = mirror;
-        return this;
-    }
-
-    /**
-     * Returns the input for this recipe, any mod accessing this value should never
-     * manipulate the values in this array as it will effect the recipe itself.
-     *
-     * @return The recipes input vales.
-     */
-    public Object[] getInput()
-    {
-        return this.input;
     }
 }
--- src/main/scala/com/builtbroken/mc/prefab/recipe/item/sheetmetal/RecipeSheetMetal.java
+++ src/main/scala/com/builtbroken/mc/prefab/recipe/item/sheetmetal/RecipeSheetMetal.java
@@ -1,12 +1,13 @@
 package com.builtbroken.mc.prefab.recipe.item.sheetmetal;
 
+import com.builtbroken.mc.core.Engine;
 import com.builtbroken.mc.core.content.tool.ItemSheetMetalTools;
 import com.builtbroken.mc.prefab.recipe.item.RecipeShapedOre;
 import net.minecraft.item.ItemStack;
 
 /**
  * Recipe designed to better handle support for sheet metal tools
- * Created by Cow Pi on 8/26/2015.
+ * Created by DarkCow on 8/26/2015.
  */
 public class RecipeSheetMetal extends RecipeShapedOre
 {
@@ -18,13 +19,16 @@ public class RecipeSheetMetal extends RecipeShapedOre
     @Override
     protected ItemStack toItemStack(Object in)
     {
-        if (in instanceof String && in.equals("sheetMetalShears"))
+        if (Engine.itemSheetMetalTools != null)
         {
-            return ItemSheetMetalTools.getHammer();
-        }
-        else if (in instanceof String && in.equals("sheetMetalHammer"))
-        {
-            return ItemSheetMetalTools.getHammer();
+            if (in instanceof String && in.equals("sheetMetalShears"))
+            {
+                return ItemSheetMetalTools.getHammer();
+            }
+            else if (in instanceof String && in.equals("sheetMetalHammer"))
+            {
+                return ItemSheetMetalTools.getHammer();
+            }
         }
         return super.toItemStack(in);
     }
