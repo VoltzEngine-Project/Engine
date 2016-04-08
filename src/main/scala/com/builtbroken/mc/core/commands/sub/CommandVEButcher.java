f1f9c180d45a040cff2914b3e23a0372ecda9a83
Robert Seifert <rseifert.phone@gmail.com>
2016-04-08 06:37:33 -0400

    Added console handling for butcher and remove command, Allows for killing all entities in a world

--- src/main/scala/com/builtbroken/mc/core/commands/sub/CommandVEButcher.java
+++ src/main/scala/com/builtbroken/mc/core/commands/sub/CommandVEButcher.java
@@ -1,12 +1,19 @@
 package com.builtbroken.mc.core.commands.sub;
 
+import com.builtbroken.jlib.lang.StringHelpers;
 import com.builtbroken.mc.prefab.commands.SubCommand;
 import com.builtbroken.mc.prefab.entity.selector.EntitySelectors;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraftforge.common.DimensionManager;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -28,6 +35,74 @@ public class CommandVEButcher extends SubCommand
             entity.setDead();
         }
         entityPlayer.addChatComponentMessage(new ChatComponentText("Removed " + list.size() + " mobs entities within " + 100 + " block radius."));
+        return true;
+    }
+
+    @Override
+    public boolean handleConsoleCommand(ICommandSender sender, String[] args)
+    {
+        long time = System.nanoTime();
+        //TODO add ability to set location and range
+        int dim = 0;
+        if (args != null && args.length > 0)
+        {
+            if (args[0].startsWith("dim"))
+            {
+                try
+                {
+                    dim = Integer.parseInt(args[0].replace("dim", ""));
+                }
+                catch (NumberFormatException e)
+                {
+                    sender.addChatMessage(new ChatComponentText("Dim id needs to be an int"));
+                    return true;
+                }
+            }
+            else
+            {
+                sender.addChatMessage(new ChatComponentText("Right now only /ve butcher dim[#] is supported, ex /ve butcher dim0"));
+                return true;
+            }
+        }
+
+        WorldServer world = DimensionManager.getWorld(dim);
+        if (world != null)
+        {
+
+            int entitiesKilled = 0;
+            int chunksSearched = 0;
+            ChunkProviderServer provider = world.theChunkProviderServer;
+            for (Object object : provider.loadedChunks)
+            {
+                if (object instanceof Chunk)
+                {
+                    chunksSearched++;
+                    for (Object l : ((Chunk) object).entityLists)
+                    {
+                        if (l instanceof Collection)
+                        {
+                            for (Object e : (Collection) l)
+                            {
+                                if (e instanceof Entity && e instanceof IMob)
+                                {
+                                    if (((Entity) e).isEntityAlive())
+                                    {
+                                        ((Entity) e).setDead();
+                                        entitiesKilled++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            time = System.nanoTime() - time;
+            sender.addChatMessage(new ChatComponentText("Removed " + entitiesKilled + "mobs over " + chunksSearched + " chunks in " + StringHelpers.formatNanoTime(time)));
+        }
+        else
+        {
+            sender.addChatMessage(new ChatComponentText("World doesn't exist, this means it unloaded or the wrong id was provided."));
+        }
         return true;
     }
 
--- src/main/scala/com/builtbroken/mc/core/commands/sub/CommandVERemove.java
+++ src/main/scala/com/builtbroken/mc/core/commands/sub/CommandVERemove.java
@@ -1,5 +1,6 @@
 package com.builtbroken.mc.core.commands.sub;
 
+import com.builtbroken.jlib.lang.StringHelpers;
 import com.builtbroken.mc.prefab.commands.SubCommand;
 import com.builtbroken.mc.prefab.entity.selector.EntitySelector;
 import com.builtbroken.mc.prefab.entity.selector.EntitySelectors;
@@ -7,7 +8,12 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraftforge.common.DimensionManager;
 
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -20,38 +26,42 @@ public class CommandVERemove extends SubCommand
         super("remove");
     }
 
+    private EntitySelector getSelector(String name)
+    {
+        //Find out what entity selector to use
+        if (name.equalsIgnoreCase("projectiles"))
+        {
+            return EntitySelectors.PLAYER_SELECTOR.selector();
+        }
+        else if (name.equalsIgnoreCase("mobs"))
+        {
+            return EntitySelectors.MOB_SELECTOR.selector();
+        }
+        else if (name.equalsIgnoreCase("living"))
+        {
+            return EntitySelectors.LIVING_SELECTOR.selector();
+        }
+        else if (name.equalsIgnoreCase("items"))
+        {
+            return EntitySelectors.ITEM_SELECTOR.selector();
+        }
+        else if (name.equalsIgnoreCase("xp"))
+        {
+            return EntitySelectors.XP_SELECTOR.selector();
+        }
+        return null;
+    }
+
     @Override
     public boolean handleEntityPlayerCommand(EntityPlayer entityPlayer, String[] args)
     {
         if (args.length > 0)
         {
             int radius = 100;
-            EntitySelector selector = null;
-
-            //Find out what entity selector to use
-            if (args[0].equalsIgnoreCase("projectiles"))
+            EntitySelector selector = getSelector(args[0]);
+            if (selector == null)
             {
-                selector = EntitySelectors.PLAYER_SELECTOR.selector();
-            }
-            else if (args[0].equalsIgnoreCase("mobs"))
-            {
-                selector = EntitySelectors.MOB_SELECTOR.selector();
-            }
-            else if (args[0].equalsIgnoreCase("living"))
-            {
-                selector = EntitySelectors.LIVING_SELECTOR.selector();
-            }
-            else if (args[0].equalsIgnoreCase("items"))
-            {
-                selector = EntitySelectors.ITEM_SELECTOR.selector();
-            }
-            else if (args[0].equalsIgnoreCase("xp"))
-            {
-                selector = EntitySelectors.XP_SELECTOR.selector();
-            }
-            else
-            {
-                return false;
+                return false; //TODO return error
             }
 
             //Get radius from player
@@ -70,7 +80,8 @@ public class CommandVERemove extends SubCommand
                         entityPlayer.addChatMessage(new ChatComponentText("Radius needs to be positive"));
                         return true;
                     }
-                } catch (NumberFormatException e)
+                }
+                catch (NumberFormatException e)
                 {
                     entityPlayer.addChatMessage(new ChatComponentText("Radius needs to be an integer"));
                     return true;
@@ -93,6 +104,77 @@ public class CommandVERemove extends SubCommand
             return true;
         }
         return false;
+    }
+
+    @Override
+    public boolean handleConsoleCommand(ICommandSender sender, String[] args)
+    {
+        long time = System.nanoTime();
+        //TODO add ability to set location and range
+        int dim = 0;
+
+        EntitySelector selector = getSelector(args[0]);
+        if (selector == null)
+        {
+            return false; //TODO return error
+        }
+
+        if (args != null && args.length > 1)
+        {
+            if (args[1].startsWith("dim"))
+            {
+                try
+                {
+                    dim = Integer.parseInt(args[1].replace("dim", ""));
+                }
+                catch (NumberFormatException e)
+                {
+                    sender.addChatMessage(new ChatComponentText("Dim id needs to be an int"));
+                    return true;
+                }
+            }
+            else
+            {
+                sender.addChatMessage(new ChatComponentText("Right now only /ve butcher dim[#] is supported, ex /ve butcher dim0"));
+                return true;
+            }
+        }
+
+        WorldServer world = DimensionManager.getWorld(dim);
+        if (world != null)
+        {
+            int entitiesKilled = 0;
+            int chunksSearched = 0;
+            ChunkProviderServer provider = world.theChunkProviderServer;
+            for (Object object : provider.loadedChunks)
+            {
+                if (object instanceof Chunk)
+                {
+                    chunksSearched++;
+                    for (Object l : ((Chunk) object).entityLists)
+                    {
+                        if (l instanceof Collection)
+                        {
+                            for (Object e : (Collection) l)
+                            {
+                                if (e instanceof Entity && ((Entity) e).isEntityAlive() && selector.isEntityApplicable((Entity) e))
+                                {
+                                    ((Entity) e).setDead();
+                                    entitiesKilled++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            time = System.nanoTime() - time;
+            sender.addChatMessage(new ChatComponentText("Removed " + entitiesKilled + " entities over " + chunksSearched + " chunks in " + StringHelpers.formatNanoTime(time)));
+        }
+        else
+        {
+            sender.addChatMessage(new ChatComponentText("World doesn't exist, this means it unloaded or the wrong id was provided."));
+        }
+        return true;
     }
 
     @Override
