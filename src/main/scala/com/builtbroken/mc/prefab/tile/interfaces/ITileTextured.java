257c707c23dfca0dfbfc833f25dac946722576da
Robert Seifert <rseifert.phone@gmail.com>
2016-06-10 03:39:56 -0400

    Removed ITile interfaces to mod layer

--- src/main/scala/com/builtbroken/mc/prefab/tile/interfaces/ITileTextured.java
+++
@@ -1,9 +0,0 @@
-package com.builtbroken.mc.prefab.tile.interfaces;
-
-/** Applied to tiles that u
- * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
- * Created by Dark(DarkGuardsman, Robert) on 4/19/2016.
- */
-public interface ITileTextured
-{
-}
