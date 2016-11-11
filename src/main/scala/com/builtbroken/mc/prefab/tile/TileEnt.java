fffc9bc226fe383b20f55509fdee4ea4efad95ac
Robert Seifert <rseifert.phone@gmail.com>
2016-11-11 10:16:05 -0500

    Implemented packet validation on TileEnt class

--- src/main/scala/com/builtbroken/mc/prefab/tile/TileEnt.java
+++ src/main/scala/com/builtbroken/mc/prefab/tile/TileEnt.java
@@ -1,9 +1,11 @@
 package com.builtbroken.mc.prefab.tile;
 
+import com.builtbroken.mc.api.IWorldPosition;
 import com.builtbroken.mc.core.Engine;
 import com.builtbroken.mc.core.network.packet.AbstractPacket;
 import com.builtbroken.mc.core.network.packet.PacketTile;
 import com.builtbroken.mc.core.network.packet.PacketType;
+import com.builtbroken.mc.lib.transform.vector.Location;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -41,6 +43,18 @@ public class TileEnt extends Tile
         return null;
     }
 
+    /**
+     * Reads packet data
+     * <p>
+     * Will default description packets to {@link #readDescPacket(ByteBuf)} on the client side
+     * using ID {@link #DESCRIPTION_PACKET_ID}
+     *
+     * @param buf
+     * @param id
+     * @param player
+     * @param type
+     * @return true if the packet was read
+     */
     public boolean read(ByteBuf buf, int id, EntityPlayer player, PacketType type)
     {
         if (id == DESCRIPTION_PACKET_ID)
@@ -52,6 +66,21 @@ public class TileEnt extends Tile
             return true;
         }
         return false;
+    }
+
+    /**
+     * Checks if the packet is valid
+     * <p>
+     * Defaults to checking distance
+     *
+     * @param player
+     * @param receiveLocation
+     * @param packet
+     * @return
+     */
+    public boolean shouldReadPacket(EntityPlayer player, IWorldPosition receiveLocation, PacketType packet)
+    {
+        return new Location(player).distance(receiveLocation.x(), receiveLocation.y(), receiveLocation.z()) < 10 && packet instanceof PacketTile;
     }
 
     /**
