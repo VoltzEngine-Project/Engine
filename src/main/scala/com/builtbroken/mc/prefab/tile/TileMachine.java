289a7fbd2c8ae903a4f503d51bee884c81e99ace
Robert Seifert <rseifert.phone@gmail.com>
2016-11-14 01:07:15 -0500

    Improved facing direction handling in TileMachine

--- src/main/scala/com/builtbroken/mc/prefab/tile/TileMachine.java
+++ src/main/scala/com/builtbroken/mc/prefab/tile/TileMachine.java
@@ -9,12 +9,20 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.ForgeDirection;
 
 /**
- * Created by robert on 1/12/2015.
+ * Prefab for anything that acts as a machine block
+ * Created by Dark(DarkGuardsman, Robert) on 1/12/2015.
  */
 public class TileMachine extends TileEnt implements IRotation
 {
+    /** Direction the machine is facing, try to use {@link #getFacing()} and {@link #setFacing(ForgeDirection)} */
     protected ForgeDirection facing = ForgeDirection.NORTH;
 
+    /**
+     * Creates a new TileMachine instance
+     *
+     * @param name     - name of the tile
+     * @param material - material of the tile
+     */
     public TileMachine(String name, Material material)
     {
         super(name, material);
@@ -24,26 +32,46 @@ public class TileMachine extends TileEnt implements IRotation
     public void onPlaced(EntityLivingBase entityLiving, ItemStack itemStack)
     {
         super.onPlaced(entityLiving, itemStack);
-        this.facing = determineForgeDirection(entityLiving);
+        setRotationOnPlacement(entityLiving, itemStack);
+    }
+
+    /**
+     * Called to set the rotation when an entity places the tile
+     *
+     * @param entityLiving
+     * @param itemStack
+     */
+    protected void setRotationOnPlacement(EntityLivingBase entityLiving, ItemStack itemStack)
+    {
+        //TODO implement advanced placement for improved user controls
+        this.setFacing(determineForgeDirection(entityLiving));
     }
 
     @Override
     public ForgeDirection getDirection()
     {
-        if (facing == null)
-        {
-            facing = ForgeDirection.NORTH;
-        }
-        return facing;
+        return getFacing();
+    }
+
+    /**
+     * Called to see if the tile wants to
+     * store the facing direction as a meta
+     * value rather than as an NBT value
+     *
+     * @return true if yes
+     */
+    protected boolean useMetaForFacing()
+    {
+        return false;
     }
 
     @Override
     public void readFromNBT(NBTTagCompound nbt)
     {
         super.readFromNBT(nbt);
-        if (nbt.hasKey("facing"))
+        if (!useMetaForFacing())
         {
-            this.facing = ForgeDirection.getOrientation(nbt.getByte("facing"));
+            setFacing(ForgeDirection.getOrientation(nbt.getByte("facing")));
         }
     }
 
@@ -51,9 +79,9 @@ public class TileMachine extends TileEnt implements IRotation
     public void writeToNBT(NBTTagCompound nbt)
     {
         super.writeToNBT(nbt);
-        if (facing != null && facing != ForgeDirection.NORTH)
+        if (!useMetaForFacing() && getFacing() != null)
         {
-            nbt.setByte("facing", (byte) facing.ordinal());
+            nbt.setByte("facing", (byte) getFacing().ordinal());
         }
     }
 
@@ -61,13 +89,56 @@ public class TileMachine extends TileEnt implements IRotation
     public void readDescPacket(ByteBuf buf)
     {
         super.readDescPacket(buf);
-        facing = ForgeDirection.getOrientation(buf.readByte());
+        if (!useMetaForFacing())
+        {
+            setFacing(ForgeDirection.getOrientation(buf.readByte()));
+        }
     }
 
     @Override
     public void writeDescPacket(ByteBuf buf)
     {
         super.writeDescPacket(buf);
-        buf.writeByte((byte) getDirection().ordinal());
+        if (!useMetaForFacing())
+        {
+            buf.writeByte(getFacing() == null ? ForgeDirection.NORTH.ordinal() : getFacing().ordinal());
+        }
+    }
+
+    /**
+     * Gets the tile's facing direction
+     *
+     * @return direction
+     */
+    public ForgeDirection getFacing()
+    {
+        if (facing == null)
+        {
+            if (useMetaForFacing())
+            {
+                //Facing is used as an internal cache of the meta
+                facing = ForgeDirection.getOrientation(getMetadata());
+            }
+            else
+            {
+                facing = ForgeDirection.NORTH;
+            }
+        }
+        return facing;
+    }
+
+    /**
+     * Called to set the facing direction
+     *
+     * @param facing - direction to face
+     */
+    public void setFacing(ForgeDirection facing)
+    {
+        this.facing = facing;
+        if (useMetaForFacing())
+        {
+            //Facing is used as an internal cache of the meta
+            this.setMeta(facing.ordinal());
+        }
     }
 }
