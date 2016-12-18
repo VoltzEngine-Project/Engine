aad1b06ae431c988f63cc601ba3e7dfe2681b6ed
Robert Seifert <rseifert.phone@gmail.com>
2016-12-18 05:15:02 -0500

    Added a way to detect if shift is held

--- src/main/scala/com/builtbroken/mc/lib/mod/AbstractProxy.java
+++ src/main/scala/com/builtbroken/mc/lib/mod/AbstractProxy.java
@@ -3,84 +3,93 @@ package com.builtbroken.mc.lib.mod;
 import com.builtbroken.mc.api.tile.IGuiTile;
 import com.builtbroken.mc.lib.mod.loadable.ILoadable;
 import cpw.mods.fml.common.network.IGuiHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import org.lwjgl.input.Keyboard;
 
 /**
  * An abstract proxy that can be extended by any mod.
  */
 public abstract class AbstractProxy implements IGuiHandler, ILoadable
 {
-	public void preInit()
-	{
+    public void preInit()
+    {
 
-	}
+    }
 
-	public void init()
-	{
+    public void init()
+    {
 
-	}
+    }
 
-	public void postInit()
-	{
+    public void postInit()
+    {
 
-	}
+    }
 
-	@Override
-	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	{
-		if(ID == 10001)
-		{
-			return getServerGuiElement(y, player, world.getEntityByID(x));
-		}
-		return getServerGuiElement(ID, player, world.getTileEntity(x, y, z));
-	}
+    @Override
+    public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        if (ID == 10001)
+        {
+            return getServerGuiElement(y, player, world.getEntityByID(x));
+        }
+        return getServerGuiElement(ID, player, world.getTileEntity(x, y, z));
+    }
 
     public Object getServerGuiElement(int ID, EntityPlayer player, TileEntity tile)
     {
-        if(tile instanceof IGuiTile)
+        if (tile instanceof IGuiTile)
         {
             return ((IGuiTile) tile).getServerGuiElement(ID, player);
         }
         return null;
     }
 
-	public Object getServerGuiElement(int ID, EntityPlayer player, Entity entity)
-	{
-		if(entity instanceof IGuiTile)
-		{
-			return ((IGuiTile) entity).getServerGuiElement(ID, player);
-		}
-		return null;
-	}
+    public Object getServerGuiElement(int ID, EntityPlayer player, Entity entity)
+    {
+        if (entity instanceof IGuiTile)
+        {
+            return ((IGuiTile) entity).getServerGuiElement(ID, player);
+        }
+        return null;
+    }
 
-	@Override
-	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
-	{
-		if(ID == 10001)
-		{
-			return getClientGuiElement(y, player, world.getEntityByID(x));
-		}
-		return getClientGuiElement(ID, player, world.getTileEntity(x, y, z));
-	}
+    @Override
+    public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+    {
+        if (ID == 10001)
+        {
+            return getClientGuiElement(y, player, world.getEntityByID(x));
+        }
+        return getClientGuiElement(ID, player, world.getTileEntity(x, y, z));
+    }
 
     public Object getClientGuiElement(int ID, EntityPlayer player, TileEntity tile)
     {
-        if(tile instanceof IGuiTile)
+        if (tile instanceof IGuiTile)
         {
             return ((IGuiTile) tile).getClientGuiElement(ID, player);
         }
         return null;
     }
 
-	public Object getClientGuiElement(int ID, EntityPlayer player, Entity entity)
-	{
-		if(entity instanceof IGuiTile)
-		{
-			return ((IGuiTile) entity).getClientGuiElement(ID, player);
-		}
-		return null;
-	}
+    public Object getClientGuiElement(int ID, EntityPlayer player, Entity entity)
+    {
+        if (entity instanceof IGuiTile)
+        {
+            return ((IGuiTile) entity).getClientGuiElement(ID, player);
+        }
+        return null;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean isShiftHeld()
+    {
+        return Keyboard.isKeyDown(Keyboard.KEY_RSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_LSHIFT);
+    }
 }
