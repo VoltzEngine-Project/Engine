7d39997975699b3f6498a888cf3b06e3c6ac0f1a
Robert Seifert <rseifert.phone@gmail.com>
2017-01-01 02:43:32 -0500

    Fixed ItemStack having a size of zero when getting smelting recipes

--- src/main/scala/com/builtbroken/mc/prefab/recipe/item/MRSmelterHandler.java
+++ src/main/scala/com/builtbroken/mc/prefab/recipe/item/MRSmelterHandler.java
@@ -1,7 +1,6 @@
 package com.builtbroken.mc.prefab.recipe.item;
 
 import com.builtbroken.mc.api.recipe.MachineRecipeType;
-import com.builtbroken.mc.prefab.recipe.item.MRHandlerItemStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 
@@ -18,12 +17,21 @@ public class MRSmelterHandler extends MRHandlerItemStack
     @Override
     public ItemStack getRecipe(Object[] items, float extraChance, float failureChance)
     {
-        if(items != null)
+        if (items != null)
         {
             ItemStack result = super.getRecipe(items, extraChance, failureChance);
             if (result == null && items.length == 1 && items[0] instanceof ItemStack)
             {
-                return FurnaceRecipes.smelting().getSmeltingResult((ItemStack)items[0]);
+                result = FurnaceRecipes.smelting().getSmeltingResult((ItemStack) items[0]);
+            }
+            if (result != null)
+            {
+                ItemStack re = result.copy();
+                if (re.stackSize <= 0)
+                {
+                    re.stackSize = 1;
+                }
+                return re;
             }
             return result;
         }
