d7328e10b30acb5378b87817753c370ded876f90
Robert Seifert <rseifert.phone@gmail.com>
2017-03-30 20:36:26 -0400

    Fixed imports

--- src/test/scala/com/builtbroken/test/command/TestAbstractCommand.java
+++ src/test/scala/com/builtbroken/test/command/TestAbstractCommand.java
@@ -1,13 +1,13 @@
 package com.builtbroken.test.command;
 
-import com.builtbroken.mc.prefab.commands.AbstractCommand;
+import com.builtbroken.mc.core.commands.prefab.AbstractCommand;
 import com.builtbroken.mc.testing.junit.AbstractTest;
 import com.builtbroken.mc.testing.junit.VoltzTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * JUnit test for {@link com.builtbroken.mc.prefab.commands.AbstractCommand}
+ *
  *
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 9/17/2015.
