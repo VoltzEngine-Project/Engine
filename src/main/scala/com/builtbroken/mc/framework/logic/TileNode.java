c92b68fba6cc3cf2eeabf07a4edb18f3081001d8
Robert Seifert <rseifert.phone@gmail.com>
2017-08-28 19:00:04 -0400

    Implemented helpers in TileNode for dealing with GUI packets

--- src/main/scala/com/builtbroken/mc/framework/logic/TileNode.java
+++ src/main/scala/com/builtbroken/mc/framework/logic/TileNode.java
@@ -5,6 +5,7 @@ import com.builtbroken.jlib.data.vector.Pos3D;
 import com.builtbroken.mc.api.IWorldPosition;
 import com.builtbroken.mc.api.data.IPacket;
 import com.builtbroken.mc.api.tile.IPlayerUsing;
+import com.builtbroken.mc.api.tile.access.IGuiTile;
 import com.builtbroken.mc.api.tile.node.ITileNode;
 import com.builtbroken.mc.api.tile.node.ITileNodeHost;
 import com.builtbroken.mc.core.Engine;
@@ -15,6 +16,7 @@ import com.builtbroken.mc.framework.block.imp.IPlacementListener;
 import com.builtbroken.mc.framework.logic.imp.ITileDesc;
 import com.builtbroken.mc.imp.transform.vector.Location;
 import com.builtbroken.mc.imp.transform.vector.Pos;
+import com.builtbroken.mc.prefab.gui.ContainerDummy;
 import com.mojang.authlib.GameProfile;
 import cpw.mods.fml.common.FMLCommonHandler;
 import io.netty.buffer.ByteBuf;
@@ -27,15 +29,19 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 3/31/2017.
  */
-public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlacementListener
+public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlacementListener, IPlayerUsing
 {
     protected static int DESCRIPTION_PACKET_ID = -1;
+    protected static int GUI_PACKET_ID = -2;
 
     protected ITileNodeHost host;
 
@@ -44,10 +50,21 @@ public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlace
     protected String username;
     protected UUID owner;
 
+    private final List<EntityPlayer> guiUsers = new ArrayList();
+
     public TileNode(String id, String mod)
     {
         this.id = id;
         this.mod = mod;
+    }
+
+    @Override
+    public void update(long ticks)
+    {
+        if (isServer() && ticks % 3 == 0)
+        {
+            doUpdateGuiUsers();
+        }
     }
 
     @Override
@@ -60,6 +77,12 @@ public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlace
     public ITileNodeHost getHost()
     {
         return host;
+    }
+
+    @Override
+    public List<EntityPlayer> getPlayersUsing()
+    {
+        return guiUsers;
     }
 
     //=============================================
@@ -162,7 +185,15 @@ public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlace
         {
             if (isClient())
             {
-                readDescPacket(buf);
+                readDescPacket(player, buf);
+            }
+            return true;
+        }
+        else if (id == GUI_PACKET_ID)
+        {
+            if (isClient())
+            {
+                readGuiPacket(player, buf);
             }
             return true;
         }
@@ -195,6 +226,9 @@ public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlace
     /**
      * Called client side only to read
      * the data from a description packet
+     * <p>
+     * If you have a player instance use the player version
+     * {@link #readDescPacket(EntityPlayer, ByteBuf)}
      *
      * @param buf - what to read data from
      */
@@ -204,12 +238,59 @@ public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlace
     }
 
     /**
+     * Called client side only to read
+     * the data from a description packet
+     *
+     * @param player - player the packet is being read for
+     * @param buf    - what to read data from
+     */
+    public void readDescPacket(EntityPlayer player, ByteBuf buf)
+    {
+        readDescPacket(buf);
+    }
+
+    /**
      * Called server side to write the
      * data for a description packet
+     * <p>
+     * If you have a player instance use the player version
+     * {@link #writeDescPacket(EntityPlayer, ByteBuf)}
      *
      * @param buf - what to write data to
      */
     public void writeDescPacket(ByteBuf buf)
+    {
+
+    }
+
+    /**
+     * Called server side to write the
+     * data for a description packet
+     *
+     * @param player - player the packet is being created for
+     * @param buf    - what to write data to
+     */
+    public void writeDescPacket(EntityPlayer player, ByteBuf buf)
+    {
+        writeDescPacket(buf);
+    }
+
+    /**
+     * Called to write GUI data
+     *
+     * @param buf
+     */
+    protected void writeGuiPacket(EntityPlayer player, ByteBuf buf)
+    {
+
+    }
+
+    /**
+     * Called to read GUI data
+     *
+     * @param buf
+     */
+    protected void readGuiPacket(EntityPlayer player, ByteBuf buf)
     {
 
     }
@@ -302,21 +383,71 @@ public class TileNode implements ITileNode, IPacketIDReceiver, ITileDesc, IPlace
      */
     public void sendDescPacket()
     {
-        sendPacketToClient(getDescPacket());
+        if (isServer())
+        {
+            MinecraftServer server = MinecraftServer.getServer();
+            List<EntityPlayer> players = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+
+            for (EntityPlayer player : players)
+            {
+                if (player.worldObj == world().unwrap())
+                {
+                    IPacket packet = getPacketForData();
+                    packet.data().writeInt(DESCRIPTION_PACKET_ID);
+                    writeDescPacket(player, packet.data());
+                }
+            }
+        }
     }
 
     public void sendPacketToGuiUsers(IPacket packet)
     {
         if (isServer() && this instanceof IPlayerUsing && packet != null)
         {
-            for (EntityPlayer player : ((IPlayerUsing) this).getPlayersUsing())
+            Iterator<EntityPlayer> players = ((IPlayerUsing) this).getPlayersUsing().iterator();
+            while (players.hasNext())
             {
-                if (player instanceof EntityPlayerMP)
+                EntityPlayer player = players.next();
+                if (isValidToSendGUIPacket(player))
                 {
                     Engine.packetHandler.sendToPlayer(packet, (EntityPlayerMP) player);
                 }
+                else
+                {
+                    players.remove();
+                }
             }
         }
+    }
+
+    /**
+     * Called to update client's with a GUI open
+     */
+    public void doUpdateGuiUsers()
+    {
+        if (isServer() && this instanceof IGuiTile && this instanceof IPlayerUsing)
+        {
+            Iterator<EntityPlayer> players = ((IPlayerUsing) this).getPlayersUsing().iterator();
+            while (players.hasNext())
+            {
+                EntityPlayer player = players.next();
+                if (isValidToSendGUIPacket(player))
+                {
+                    IPacket packet = getHost().getPacketForData(GUI_PACKET_ID);
+                    writeGuiPacket(player, packet.data());
+                    Engine.packetHandler.sendToPlayer(packet, (EntityPlayerMP) player);
+                }
+                else
+                {
+                    players.remove();
+                }
+            }
+        }
+    }
+
+    protected boolean isValidToSendGUIPacket(EntityPlayer player)
+    {
+        return player instanceof EntityPlayerMP && player.openContainer instanceof ContainerDummy && ((ContainerDummy) player.openContainer).tile == this;
     }
 
     //==========================
