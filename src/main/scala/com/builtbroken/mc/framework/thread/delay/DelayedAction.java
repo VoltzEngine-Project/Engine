044a848d05914388e987e0fcd7aee41af3f95b02
Robert Seifert <rseifert.phone@gmail.com>
2017-09-29 00:31:34 -0400

    Started work on a delayed action handler
    This will allow triggering events after a delay in world ticks

---
+++ src/main/scala/com/builtbroken/mc/framework/thread/delay/DelayedAction.java
@@ -0,0 +1,60 @@
+package com.builtbroken.mc.framework.thread.delay;
+
+import net.minecraft.world.World;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 9/28/2017.
+ */
+public abstract class DelayedAction
+{
+    public final World world;
+
+    protected int ticksToWait = 0;
+
+    protected boolean isAlive = true;
+
+    public DelayedAction(World world, int ticks)
+    {
+        this.world = world;
+        this.ticksToWait = ticks;
+    }
+
+    /**
+     * Called to kill the action before it is
+     * finished.
+     */
+    public void kill()
+    {
+        isAlive = false;
+    }
+
+    /**
+     * Can the action be cleared before it is run.
+     * <p>
+     * Best to return true for non-import tasks that
+     * could be cleared to save CPU with no issues.
+     * Things like audio, graphics, damage, etc
+     *
+     * @return true if yes
+     */
+    public boolean canClear()
+    {
+        return false;
+    }
+
+    /**
+     * Called to queue the action for ticking
+     */
+    public void start()
+    {
+        DelayedActionHandler.add(this);
+    }
+
+    /**
+     * Called to trigger the action
+     *
+     * @return true if done
+     */
+    public abstract boolean trigger();
+}
---
+++ src/main/scala/com/builtbroken/mc/framework/thread/delay/DelayedActionHandler.java
@@ -0,0 +1,68 @@
+package com.builtbroken.mc.framework.thread.delay;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.world.World;
+
+import java.util.*;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 9/28/2017.
+ */
+public class DelayedActionHandler
+{
+    public static Map<Integer, List<DelayedAction>> worldToActions = new HashMap();
+
+    @SubscribeEvent
+    public void onWorldTick(TickEvent.WorldTickEvent event)
+    {
+        final World world = event.world;
+        final int dimID = world.provider.dimensionId;
+        if (worldToActions.containsKey(dimID))
+        {
+            List<DelayedAction> actions = worldToActions.get(dimID);
+            if (actions != null && actions.size() > 0)
+            {
+                Iterator<DelayedAction> it = actions.iterator();
+                while (it.hasNext())
+                {
+                    DelayedAction action = it.next();
+                    if (action.isAlive)
+                    {
+                        action.ticksToWait--;
+                        if (action.ticksToWait <= 0)
+                        {
+                            if(action.trigger())
+                            {
+                                it.remove();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        it.remove();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void add(DelayedAction delayedAction)
+    {
+        List<DelayedAction> list = null;
+        if (worldToActions.containsKey(delayedAction.world.provider.dimensionId))
+        {
+            list = worldToActions.get(delayedAction.world.provider.dimensionId);
+        }
+
+        if (list != null)
+        {
+            list = new ArrayList();
+        }
+
+        list.add(delayedAction);
+
+        worldToActions.put(delayedAction.world.provider.dimensionId, list);
+    }
+}
