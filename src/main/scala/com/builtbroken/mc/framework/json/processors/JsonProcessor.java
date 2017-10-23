1c8c508387746408376c707eb0866f3804c22568
Robert WC Seifert <rseifert.phone@gmail.com>
2017-10-23 17:22:31 -0400

    Added for loop support by default to JsonProcessor

--- src/main/scala/com/builtbroken/mc/framework/json/processors/JsonProcessor.java
+++ src/main/scala/com/builtbroken/mc/framework/json/processors/JsonProcessor.java
@@ -8,6 +8,7 @@ import com.builtbroken.mc.framework.json.imp.IJsonGenObject;
 import com.builtbroken.mc.framework.json.imp.IJsonProcessor;
 import com.builtbroken.mc.framework.json.loading.JsonProcessorInjectionMap;
 import com.builtbroken.mc.framework.json.struct.JsonConditional;
+import com.builtbroken.mc.framework.json.struct.JsonForLoop;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import org.apache.logging.log4j.LogManager;
@@ -43,30 +44,111 @@ public abstract class JsonProcessor<D extends IJsonGenObject> implements IJsonPr
         return key.equalsIgnoreCase(getJsonKey());
     }
 
+
     @Override
-    public boolean process(JsonElement element, List<IJsonGenObject> entries)
+    public boolean process(JsonElement element, List<IJsonGenObject> objects)
     {
-        D output = process(element);
-        if (output != null)
-        {
-            entries.add(output);
-        }
+        handle(null, element, objects);
         return true;
     }
 
-    public D process(JsonElement element)
+    /**
+     * Internal method for handling a new JSON entry. Mainly used to handle for-loops before
+     * processing the main data. Not all processors use this system e.g. Block and Item processor
+     * are intentionally set to not use for-loops to prevent item IDs from being consumed.
+     *
+     * @param out     - output object of the process, normally null however can be used to
+     *                note an item, entity, block, etc. This way the json doesn't need to specify
+     *                a target object to apply the data towards.
+     * @param element - data
+     * @param objects - list of processed objects from data
+     */
+    protected void handle(final Object out, JsonElement element, List<IJsonGenObject> objects)
+    {
+        //Handle for loops
+        if (element.isJsonObject())
+        {
+            JsonObject jsonObject = element.getAsJsonObject();
+            if (JsonForLoop.hasLoops(jsonObject))
+            {
+                JsonForLoop.handleLoops(jsonObject, o -> {
+                    D data = process(out, o);
+                    if (data != null)
+                    {
+                        objects.add(data);
+                    }
+                });
+                return;
+            }
+        }
+
+        //Default handling
+        D data = process(out, element);
+        if (data != null)
+        {
+            objects.add(data);
+        }
+    }
+
+    /**
+     * Called to process a json element into an object. Its assumed
+     * one element results in one data segement. If this is not the
+     * case use {@link #process(JsonElement, List)} instead
+     *
+     * @param out     - host of the data (item, block, entity, etc), can be null
+     *                if the json was not nested into a host object.
+     * @param element - data
+     * @return object
+     */
+    protected D process(final Object out, final JsonElement element)
+    {
+        return process(element);
+    }
+
+    /**
+     * Called to process a json element into an object.  Its assumed
+     * one element results in one data segement. If this is not the
+     * case use {@link #process(JsonElement, List)} instead.
+     * <p>
+     * As well its assumed the host of the data doesn't matter. If this
+     * is not the case use {@link #process(Object, JsonElement)} or another
+     * method that provides access to the host object.
+     *
+     * @param element - data
+     * @return object
+     */
+    protected D process(final JsonElement element)
     {
         return null;
     }
 
+    /**
+     * Called to process additional keys as annotation injection data
+     *
+     * @param objectToInject - object
+     * @param jsonData       - data
+     */
     protected void processAdditionalKeys(D objectToInject, JsonObject jsonData)
+    {
+        processAdditionalKeys(objectToInject, jsonData, null);
+    }
+
+    /**
+     * Called to process additional keys as annotation injection data
+     *
+     * @param objectToInject - object
+     * @param jsonData       - data
+     * @param keysToIgnore   - list of keys to ignore from processing
+     */
+    protected void processAdditionalKeys(D objectToInject, JsonObject jsonData, List<String> keysToIgnore)
     {
         try
         {
             //Call to process extra tags from file
             for (Map.Entry<String, JsonElement> entry : jsonData.entrySet())
             {
-                if (keyHandler.handle(objectToInject, entry.getKey().toLowerCase(), entry.getValue()))
+                if ((keysToIgnore == null || !keysToIgnore.contains(entry.getKey()))
+                        && keyHandler.handle(objectToInject, entry.getKey().toLowerCase(), entry.getValue()))
                 {
                     if (Engine.runningAsDev)
                     {
