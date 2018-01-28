3c176c9a9d27d7cfa73547d4336d3ba906b6140d
dsirius <dmodoomsirius@users.noreply.github.com>
2018-01-28 03:15:32 -0500

    disable tests for now

--- src/test/scala/com/builtbroken/test/core/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
- * Created by Dark(DarkGuardsman, Robert) on 10/12/2015.
- */
-package com.builtbroken.test.core;
