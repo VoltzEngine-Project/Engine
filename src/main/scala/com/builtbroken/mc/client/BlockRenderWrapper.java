e21a53567465d92a90b0d673493566a33f7ff578
Robert WC Seifert <rseifert.phone@gmail.com>
2018-05-02 22:18:56 -0400

    Fixed cubes sides being set to not render

--- src/main/scala/com/builtbroken/mc/client/BlockRenderWrapper.java
+++ src/main/scala/com/builtbroken/mc/client/BlockRenderWrapper.java
@@ -14,7 +14,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-/** Used to fake data for rendering blocks
+/**
+ * Used to fake data for rendering blocks
+ *
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 2/19/2017.
  */
@@ -207,7 +209,10 @@ public class BlockRenderWrapper extends Block
 
     public void clearRenderSides()
     {
-        renderSides = 0;
+        for (ForgeDirection direction : ForgeDirection.VALID_DIRECTIONS)
+        {
+            setRenderSide(direction, true);
+        }
     }
 
     public void setRenderSide(ForgeDirection direction, boolean can)
