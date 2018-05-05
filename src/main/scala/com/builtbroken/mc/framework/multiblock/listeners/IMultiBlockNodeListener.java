878933410bf2411424c7cb699ad465491637461e
Robert WC Seifert <rseifert.phone@gmail.com>
2018-05-05 03:36:36 -0400

    Moved node listener for multiblock

--- src/main/scala/com/builtbroken/mc/framework/multiblock/listeners/IMultiBlockNodeListener.java
+++ src/main/scala/com/builtbroken/mc/framework/multiblock/listeners/IMultiBlockNodeListener.java
@@ -1,6 +1,7 @@
-package com.builtbroken.mc.framework.multiblock;
+package com.builtbroken.mc.framework.multiblock.listeners;
 
 import com.builtbroken.mc.api.tile.multiblock.IMultiTile;
+import com.builtbroken.mc.api.tile.node.ITileNode;
 import com.builtbroken.mc.imp.transform.vector.Pos;
 import net.minecraft.entity.player.EntityPlayer;
 
@@ -10,15 +11,20 @@ import net.minecraft.entity.player.EntityPlayer;
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 5/3/2018.
  */
-public interface IMultiBlockNodeListener
+public interface IMultiBlockNodeListener extends ITileNode
 {
     /**
      * Called when multi-block is added
      *
-     * @param tileMulti - tile
-     * @param relativePos       - relative position
+     * @param tileMulti   - tile
+     * @param relativePos - relative position
      */
     void onMultiTileAdded(IMultiTile tileMulti, Pos relativePos);
 
     boolean onMultiTileActivated(IMultiTile tile, Pos relativePos, EntityPlayer player, int side, float xHit, float yHit, float zHit);
+
+    default String getMultiBlockLayoutKey()
+    {
+        return null;
+    }
 }
