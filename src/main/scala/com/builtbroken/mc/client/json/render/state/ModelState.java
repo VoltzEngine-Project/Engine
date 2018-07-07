bf3dcb7e8274896d58ccff0cb1838935dc79ee81
Robert WC Seifert <rseifert.phone@gmail.com>
2018-07-07 02:37:18 -0400

    Moved model state render steps into sub methods
    Cleanup before adding new features

--- src/main/scala/com/builtbroken/mc/client/json/render/state/ModelState.java
+++ src/main/scala/com/builtbroken/mc/client/json/render/state/ModelState.java
@@ -22,11 +22,25 @@ import java.util.List;
  */
 public class ModelState extends RenderStateTexture implements IModelState
 {
+    /** Model ID to render */
     public String modelID;
+
+    /** Parts of the model to render */
     public String[] parts;
+
+    /** Render offset of the model */
     public Pos offset;
+
+    /** Scale of the model */
     public Pos scale;
+
+    /** Point to translate to for rotation */
+    public Pos rotationPoint;
+
+    /** Rotation of the model */
     public EulerAngle rotation;
+
+    /** Order to rotate */
     public String[] rotationOrder = new String[]{"roll", "pitch", "yaw"};
 
     public boolean renderParent = false;
@@ -59,125 +73,144 @@ public class ModelState extends RenderStateTexture implements IModelState
             //Starts rendering by storing previous matrix
             GL11.glPushMatrix();
 
+            //Setup model scale, rotation, and translation
             if (!subRender)
             {
                 //TODO handle parent additions, in which parent and child data are combined
-                //Scales object by value
-                if (scale != null)
-                {
-                    GL11.glScaled(scale.x(), scale.y(), scale.z());
-                }
-                else if (parentState instanceof IModelState && ((IModelState) parentState).getScale() != null)
-                {
-                    GL11.glScaled(((IModelState) parentState).getScale().x(), ((IModelState) parentState).getScale().y(), ((IModelState) parentState).getScale().z());
-                }
-
-                if (combineRotations && _cachedRotation == null && parentState != null)
-                {
-                    double r = rotation != null ? rotation.roll() : 0;
-                    double p = rotation != null ? rotation.pitch() : 0;
-                    double y = rotation != null ? rotation.yaw() : 0;
-
-                    IRenderState state = getParent();
-                    while (state instanceof IModelState)
-                    {
-                        EulerAngle rot = ((IModelState) state).getRotation();
-                        if (rot != null)
-                        {
-                            r += rot.roll();
-                            p += rot.pitch();
-                            y += rot.yaw();
-                        }
-                        state = state.getParent();
-                    }
-
-                    _cachedRotation = new EulerAngle(y, p, r);
-                }
-
-                //Apply rotation, using render defined order
-                if (rotationOrder != null)
-                {
-                    for (String r : rotationOrder)
-                    {
-                        if (r != null)
-                        {
-                            //TODO convert string to int for faster compare
-                            if (r.equalsIgnoreCase("-roll"))
-                            {
-                                doRoll(-roll);
-                            }
-                            else if (r.equalsIgnoreCase("-pitch"))
-                            {
-                                doPitch(-pitch);
-                            }
-                            else if (r.equalsIgnoreCase("-yaw"))
-                            {
-                                doYaw(-yaw);
-                            }
-                            else if (r.equalsIgnoreCase("roll"))
-                            {
-                                doRoll(roll);
-                            }
-                            else if (r.equalsIgnoreCase("pitch"))
-                            {
-                                doPitch(pitch);
-                            }
-                            else if (r.equalsIgnoreCase("yaw"))
-                            {
-                                doYaw(yaw);
-                            }
-                        }
-                    }
-                }
-
-                //Moves the object
-                if (offset != null)
-                {
-                    GL11.glTranslated(offset.x(), offset.y(), offset.z());
-                }
-                else if (parentState instanceof IModelState && ((IModelState) parentState).getOffset() != null)
-                {
-                    GL11.glTranslated(((IModelState) parentState).getOffset().x(), ((IModelState) parentState).getOffset().y(), ((IModelState) parentState).getOffset().z());
-                }
+                scale();
+                rotate(yaw, pitch, roll);
+                translate();
             }
 
-            //Render parent
-            GL11.glPushMatrix();
-            if (parentState instanceof IModelState)
-            {
-                if (renderParent)
-                {
-                    ((IModelState) parentState).render(true);
-                }
-                else if (parts == null && parentState instanceof ModelState && ((ModelState) parentState).renderParent)
-                {
-                    if (((ModelState) parentState).parentState instanceof IModelState)
-                    {
-                        ((IModelState) ((ModelState) parentState).parentState).render(true);
-                    }
-                }
-            }
-            GL11.glPopMatrix();
-
-            //Binds texture
-            if (textureData != null)
-            {
-                FMLClientHandler.instance().getClient().renderEngine.bindTexture(textureData.getLocation());
-            }
-            else
-            {
-                //Backup texture bind, if no texture
-                FMLClientHandler.instance().getClient().renderEngine.bindTexture(SharedAssets.GREY_TEXTURE);
-            }
-
-            //Render model
-            modelData.render(renderOnlyParts(), getPartsToRender());
+            //Render model and parent model if needed
+            doRender(modelData, textureData);
 
             //Ends render by restoring previous matrix(rotation, position, etc)
             GL11.glPopMatrix();
             return true;
         }
         return false;
+    }
+
+    protected void scale()
+    {
+        if (scale != null)
+        {
+            GL11.glScaled(scale.x(), scale.y(), scale.z());
+        }
+        else if (parentState instanceof IModelState && ((IModelState) parentState).getScale() != null)
+        {
+            GL11.glScaled(((IModelState) parentState).getScale().x(), ((IModelState) parentState).getScale().y(), ((IModelState) parentState).getScale().z());
+        }
+    }
+
+    protected void rotate(float yaw, float pitch, float roll)
+    {
+        if (combineRotations && _cachedRotation == null && parentState != null)
+        {
+            double r = rotation != null ? rotation.roll() : 0;
+            double p = rotation != null ? rotation.pitch() : 0;
+            double y = rotation != null ? rotation.yaw() : 0;
+
+            IRenderState state = getParent();
+            while (state instanceof IModelState)
+            {
+                EulerAngle rot = ((IModelState) state).getRotation();
+                if (rot != null)
+                {
+                    r += rot.roll();
+                    p += rot.pitch();
+                    y += rot.yaw();
+                }
+                state = state.getParent();
+            }
+
+            _cachedRotation = new EulerAngle(y, p, r);
+        }
+
+        //Apply rotation, using render defined order
+        if (rotationOrder != null)
+        {
+            for (String r : rotationOrder)
+            {
+                if (r != null)
+                {
+                    //TODO convert string to int for faster compare
+                    if (r.equalsIgnoreCase("-roll"))
+                    {
+                        doRoll(-roll);
+                    }
+                    else if (r.equalsIgnoreCase("-pitch"))
+                    {
+                        doPitch(-pitch);
+                    }
+                    else if (r.equalsIgnoreCase("-yaw"))
+                    {
+                        doYaw(-yaw);
+                    }
+                    else if (r.equalsIgnoreCase("roll"))
+                    {
+                        doRoll(roll);
+                    }
+                    else if (r.equalsIgnoreCase("pitch"))
+                    {
+                        doPitch(pitch);
+                    }
+                    else if (r.equalsIgnoreCase("yaw"))
+                    {
+                        doYaw(yaw);
+                    }
+                }
+            }
+        }
+    }
+
+    protected void translate()
+    {
+        //Moves the object
+        if (offset != null)
+        {
+            GL11.glTranslated(offset.x(), offset.y(), offset.z());
+        }
+        else if (parentState instanceof IModelState && ((IModelState) parentState).getOffset() != null)
+        {
+            GL11.glTranslated(((IModelState) parentState).getOffset().x(), ((IModelState) parentState).getOffset().y(), ((IModelState) parentState).getOffset().z());
+        }
+    }
+
+    protected void doRender(ModelCustomData modelData, TextureData textureData)
+    {
+        //Render parent
+        GL11.glPushMatrix();
+        if (parentState instanceof IModelState)
+        {
+            if (renderParent)
+            {
+                ((IModelState) parentState).render(true);
+            }
+            else if (parts == null && parentState instanceof ModelState && ((ModelState) parentState).renderParent)
+            {
+                if (((ModelState) parentState).parentState instanceof IModelState)
+                {
+                    ((IModelState) ((ModelState) parentState).parentState).render(true);
+                }
+            }
+        }
+        GL11.glPopMatrix();
+
+        //Binds texture
+        if (textureData != null)
+        {
+            FMLClientHandler.instance().getClient().renderEngine.bindTexture(textureData.getLocation());
+        }
+        else
+        {
+            //Backup texture bind, if no texture
+            FMLClientHandler.instance().getClient().renderEngine.bindTexture(SharedAssets.GREY_TEXTURE);
+        }
+
+        //Render model
+        modelData.render(renderOnlyParts(), getPartsToRender());
     }
 
     private void doRoll(float extra)
