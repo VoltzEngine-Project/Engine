f409328ada5f092f0bad2f5899414da82dd5b837
Robert WC Seifert <rseifert.phone@gmail.com>
2018-07-10 05:43:57 -0400

    Added most listener keys from BlockBase to reference class

--- src/main/scala/com/builtbroken/mc/framework/block/imp/BlockListenerKeys.java
+++ src/main/scala/com/builtbroken/mc/framework/block/imp/BlockListenerKeys.java
@@ -9,9 +9,25 @@ public final class BlockListenerKeys
     public static final String MULTI_BLOCK_LAYOUT_LISTENER = "multiblocklayout";
 
     public static final String BLOCK_STACK = "blockStack";
+
     public static final String BREAK = "break";
     public static final String PLACEMENT = "placement";
 
+    public static final String HARDNESS = "hardness";
+    public static final String RESISTANCE = "resistance";
+    public static final String BOUNDS = "bounds";
+    public static final String LIGHT = "light";
+
+    public static final String CHANGE = "change";
+    public static final String TOOL = "tool";
+    public static final String RAIN = "rain";
+
+    public static final String ACTIVATION = "activation";
+    public static final String WRENCH = "wrench";
+
+    public static final String UPDATE = "update";
+
     public static final String TILE_RENDER = "tilerender";
     public static final String JSON_RENDER_STATE = "jsonrenderstate";
+    public static final String ICON = "icon";
 }
